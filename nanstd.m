ed5423882f16e6c9238de5f2827ba2f9faf71a2d
Paul Kienzle <pkienzle@users.sf.net>
2002-03-13 18:11:53 +0000

    Slightly cleaner handling of n==1 case remove unnecessary do_fortran_indexing
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@155
    hg:cbd6d64bf4ddb05a8730ae5bdae8c83f644f5128

--- nanstd.m
+++ nanstd.m
@@ -18,7 +18,6 @@
 ## nanstd is identical to the std function except that NaN values are
 ## ignored.  If all values are NaN, the std is returned as NaN. If there
 ## is only a single non-NaN value, the std is returned as 0. 
-## [Is this behaviour compatible?]
 ##
 ## See also: nanmin, nanmax, nansum, nanmedian, nanmean
 function v = nanstd (X, dim)
@@ -33,36 +32,25 @@ function v = nanstd (X, dim)
       endif
     endif
     if (dim == 2) X = X.'; endif
-    dfi = do_fortran_indexing;
-    wdz = warn_divide_by_zero;
-    unwind_protect
-      do_fortran_indexing = 1;
-      warn_divide_by_zero = 0;
 
-      ## determine the number of non-missing points in each data set
-      n = sum (!isnan(X));
-
-      ## replace missing data with zero and compute the mean
-      X(isnan(X)) = 0;
-      meanX = sum (X) ./ n;
-
-      ## subtract the mean from the data and compute the sum squared
-      v = sumsq (X - ones(size(X,1), 1) * meanX);
-
-      ## because the missing data was set to zero each missing data
-      ## point will contribute (-meanX)^2 to sumsq, so remove these
-      v = v - (meanX .^ 2) .* (size(X,1) - n);
-
-      ## compute the standard deviation from the corrected sumsq
-      v = sqrt ( v ./ (n - 1) );
-
-      ## set special values of std for n=0 and n=1
-      ## v(n == 0) = NaN;  # meanX = 0/0 -> NaN above, so not necessary
-      v(n == 1) = 0;
-    unwind_protect_cleanup
-      do_fortran_indexing = dfi;
-      warn_divide_by_zero = wdz;
-    end_unwind_protect
+    ## determine the number of non-missing points in each data set
+    n = sum (!isnan(X));
+    
+    ## replace missing data with zero and compute the mean
+    X(isnan(X)) = 0;
+    meanX = sum (X) ./ n;
+    
+    ## subtract the mean from the data and compute the sum squared
+    v = sumsq (X - ones(size(X,1), 1) * meanX);
+    
+    ## because the missing data was set to zero each missing data
+    ## point will contribute (-meanX)^2 to sumsq, so remove these
+    v = v - (meanX .^ 2) .* (size(X,1) - n);
+    
+    ## compute the standard deviation from the corrected sumsq using
+    ## max(n-1,1) in the denominator so that the std for a single point is 0
+    v = sqrt ( v ./ max(n - 1, 1) );
+    
     if (dim == 2) v = v.'; endif
   endif
 endfunction
