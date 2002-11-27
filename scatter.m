efc400a3c1dd6e37637478c5f597c0ff2b7a6686
Paul Kienzle <pkienzle@users.sf.net>
2002-11-27 08:40:11 +0000

    author/license updates
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@672
    hg:62a26db76c17031e6c8e2a3d7613360ab33c95b2

--- scatter.m
+++ scatter.m
@@ -1,35 +1,51 @@
-function scatter (varargin)
+## Copyright (C) 2002 Alberto Terruzzi
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  ## SCATTER Scatter plot.
-  ##   SCATTER(X,Y,S,C) displays colored circles at the locations specified
-  ##   by the vectors X and Y (which must be the same size).  The area of
-  ##   each marker is determined by the values in the vector S (surface area)
-  ##   and the colors of each marker are based on the values in C. S can be a
-  ##   scalar, in which case all the markers are drawn the same size, or a
-  ##   vector the same length as X and Y.
-  ##   When C is a vector the same length as X and Y, the values in C
-  ##   are used as the color of each circle.  
-  ##   RGB color not supported
-  ##
-  ##   usage:  scatter (...)
-  ##   scatter(X,Y) draws the markers in the default color.
-  ##   scatter(X,Y,S) draws the markers with size S.
-  ##   scatter(X,Y,S,C) draws the markers with size S and color C.
-  ##   scatter(...,M) uses the marker M instead of default one.
-  ##   scatter(...,"filled") fills the markers, this feature isn't supported.
-  ##
-  ##   Use PLOT for single color, single marker size scatter plots.
-  ##
-  ##   Example: A=[0:0.5:2*pi];
-  ##            B=[0:0.2:2.4]; B(1,4)=5; B(1,8)=-5;
-  ##            C=[0:1:6, 0:1:5];
-  ##            scatter (A,B,abs(B*0.2),C,"@","filled");
-  ##
-  ## Author: Alberto Terruzzi <t-albert@libero.it>
-  ## Version: 1.0
-  ## Created: 19 November 2002
-  ##
-  ## ToDo: filled markers, RGB support.
+## SCATTER Scatter plot.
+##   SCATTER(X,Y,S,C) displays colored circles at the locations specified
+##   by the vectors X and Y (which must be the same size).  The area of
+##   each marker is determined by the values in the vector S (surface area)
+##   and the colors of each marker are based on the values in C. S can be a
+##   scalar, in which case all the markers are drawn the same size, or a
+##   vector the same length as X and Y.
+##   When C is a vector the same length as X and Y, the values in C
+##   are used as the color of each circle.  
+##   RGB color not supported
+##
+##   usage:  scatter (...)
+##   scatter(X,Y) draws the markers in the default color.
+##   scatter(X,Y,S) draws the markers with size S.
+##   scatter(X,Y,S,C) draws the markers with size S and color C.
+##   scatter(...,M) uses the marker M instead of default one.
+##   scatter(...,"filled") fills the markers, this feature isn't supported.
+##
+##   Use PLOT for single color, single marker size scatter plots.
+##
+##   Example: A=[0:0.5:2*pi];
+##            B=[0:0.2:2.4]; B(1,4)=5; B(1,8)=-5;
+##            C=[0:1:6, 0:1:5];
+##            scatter (A,B,abs(B*0.2),C,"@","filled");
+
+## Author: Alberto Terruzzi <t-albert@libero.it>
+## Version: 1.0
+## Created: 19 November 2002
+##
+## ToDo: filled markers, RGB support.
+
+function scatter (varargin)
 
 error(nargchk(2,6,nargin))
 filled = 0; 
