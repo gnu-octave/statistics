96becf27d332ff7a56013982330a6d02e3e6f881
Paul Kienzle <pkienzle@users.sf.net>
2004-08-02 05:10:02 +0000

    Allow user selection of bins, so long as width is uniform
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@1637
    hg:cf17df235e56babde763fb19a23406b0d7b35187

--- histfit.m
+++ histfit.m
@@ -49,15 +49,17 @@ if nargin < 2
 endif
 
 [n,xbin]=hist(data,nbins);
+dx = xbin(2)-xbin(1);
+if any(abs(diff(xbin,2)) > 10*dx*eps)
+  error("histfit bins must be uniform width");
+endif
 
 mr = nanmean(data); ## Estimates the parameter, MU, of the normal distribution.
 sr = nanstd(data);  ## Estimates the parameter, SIGMA, of the normal distribution.
 x=(-3*sr+mr:0.1*sr:3*sr+mr)';## Evenly spaced samples of the expected data range.
 [xb,yb] = bar(xbin,n);
-rangex = max(xb(:)) - min(xb(:)); ## Finds the range of this data.
-binwidth = rangex/nbins;    ## Finds the width of each bin.
 y = normal_pdf(x,mr,sr);  
-y = row*(y*binwidth);   ## Normalization necessary to overplot the histogram.
+y = row*y*dx;   ## Normalization necessary to overplot the histogram.
 plot(xb,yb,";;b",x,y,";;r-");     ## Plots density line over histogram.
 
 endfunction
