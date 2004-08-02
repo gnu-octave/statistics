923399411dcf686759df8230f841d85256c5dce1
Paul Kienzle <pkienzle@users.sf.net>
2004-08-02 20:01:03 +0000

    Use estimated parameter form for the exponential/normal distributions.
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@1648
    hg:009318a9b469bedbc268e1b2b1c2af96bf316172

--- anderson_darling_test.m
+++ anderson_darling_test.m
@@ -1,9 +1,10 @@
-## [p,A] = anderson_darling_test(x,uniform|normal|exponential)
+## [q,A] = anderson_darling_test(x,uniform|normal|exponential)
 ##
 ## Test the hypothesis that x is selected from the given distribution
-## using the Anderson-Darling test.
+## using the Anderson-Darling test.  Returns a small q value if the data
+## are not distributed as expected.
 ##
-## The Anderson-Darling test statistic measures
+## The Anderson-Darling A statistic is calculated as follows:
 ##
 ##    A = -n - \sum_{i=1}^n (2i-1)/n log(z_i (1-z_{n-i+1}))
 ##
@@ -12,59 +13,93 @@
 ## Anderson-Darling statistic is sensitive to the tails of the 
 ## distribution.
 ##
-## uniform (0,1):   f(x, 'uniform')
-## normal:          f((x-m)/sqrt(v),'normal')
-## lognormal:       f(log (x - m)/sqrt(v),'normal')
-## exponential:     f(lambda*x, 'exponential')
-## weibull:         f((x/scale)^shape, 'exponential')
+## For 'normal' and 'exponential' distributions, estimate the 
+## distribution parameters from the data, convert the values 
+## to CDF values, and compare the result to tabluated critical 
+## values.  This includes an correction for small n which 
+## works well enough for n >= 8, but less so from smaller n.
 ##
-## We use Marsaglia's approximation to the Anderson-Darling CDF
-## to compute the p-values (see anderson_darling_cdf for details).
+## For 'uniform', assume the values are uniformly distributed
+## in (0,1), compute A and return the corresponding p value from
+## 1-anderson_darling_cdf(A,n).
+## 
+## If you are selecting from a known distribution, convert your 
+## values into CDF values for the distribution and use 'uniform'.
+## Do not use 'uniform' if the distribution parameters are estimated 
+## from the data itself, as this sharply biases the A statistic
+## toward smaller values.
 ##
-## These values differ significantly from those published elsewhere,
-## but I do not yet understand why.  I have included published values 
-## herein (the Acrit values), and the corrections to apply for small n.
-## Uncomment the last line if you prefer to use them.
-##
-## Critical values and small sample adjustments for normal and
-## exponential distributions comes from Charles Annis, P.E.
-##     http://www.statisticalengineering.com/goodness.htm
-## but identical information is available elsewhere on the web.
+## [1] Stephens, MA; (1986), "Tests based on EDF statistics", in
+## D'Agostino, RB; Stephens, MA; (eds.) Goodness-of-fit Techinques.
+## New York: Dekker.
 
 ## Author: Paul Kienzle
 ## This program is granted to the public domain.
-function [p,A] = anderson_darling_test(x,dist)
+function [q,A] = anderson_darling_test(x,dist)
 
   if size(x,1) == 1, x=x(:); end
   x = sort(x);
   n = size(x,1);
+  use_cdf = 0;
   switch dist
     case 'normal',
-      adj = 1 + .75/n + 2.25/n/n;
-      pvals = [ 1, 0.1, 0.05, 0.025, 0.01 ];
+      adj = 1 + (.75 + 2.25/n)/n;
+      qvals = [ 1, 0.1, 0.05, 0.025, 0.01 ];
       Acrit = [-Inf, 0.631, 0.752, 0.873, 1.035]/adj;
-      x = (1 + erf(x/sqrt(2))) / 2;
+      x = stdnormal_cdf(zscore(x));
+
     case 'uniform',
       ## Put invalid data at the limits of the distribution
+      ## This will drive the statistic to infinity.
       x(x<0) = 0;
       x(x>1) = 1;
-      pvals = [ 1, 0.1, 0.05, 0.01 ];
-      Acrit = [-Inf, 1.933, 2.492, 3.878];
-    case 'exponential',
-      ## Put invalid data at the limits of the distribution
-      x(x<0) = 0;
+      #qvals = [ 1, 0.1, 0.05, 0.025, 0.01 ];
+      #Acrit = [-Inf, 1.933, 2.492, 3.077, 3.878];
+      use_cdf = 1;
+
+    case 'XXXweibull',
       adj = 1 + 0.2/sqrt(n);
-      pvals = [ 1, 0.1, 0.05, 0.025, 0.01 ];
-      Acrit = [-Inf, 0.637, 0.757, 0.877, 1.038]/adj;
-      x = 1 - exp(-x);
+      Acrit = [-Inf, 0.637, 0.757, 0.877, 1.038]*adj;
+      ## XXX FIXME XXX how to fit alpha and sigma?
+      x = weibull_cdf(x,ones(n,1)*alpha,ones(n,1)*sigma);
+
+    case 'exponential',
+      qvals = [ 1, 0.1, 0.05, 0.025, 0.01 ];
+      adj = 1 + 0.6/n;
+      Acrit = [-Inf, 1.062, 1.321, 1.591, 1.959]/adj;
+
+      lambda = 1./mean(x);  # exponential parameter estimation
+      x = exponential_cdf(x,ones(n,1)*lambda);
+
     otherwise
-      error('use normal, exponential or uniform distribution');
+      error("Anderson-Darling test for %s not implemented", dist);
   end
+
   if any(x<0 | x>1)
     error('Anderson-Darling test requires data in CDF form');
   endif
 
   i = [1:n]'*ones(1,size(x,2));
   A = -n - sum( (2*i-1) .* (log(x) + log(1-x(n:-1:1,:))) )/n;
-  p = anderson_darling_cdf(A, n);
-  ##p = pvals(lookup(Acrit,A));
+
+  if use_cdf
+    q = 1-anderson_darling_cdf(A, n);
+  else
+    q = qvals(lookup(Acrit,A));
+  endif
+
+
+%!demo
+%! c = anderson_darling_test(10*rande(12,10000),'exponential');
+%! tabulate(100*c,100*[unique(c),1]);
+%! % The Fc column should report 100, 250, 500, 1000, 10000 more or less.
+
+%!demo
+%! c = anderson_darling_test(randn(12,10000),'normal');
+%! tabulate(100*c,100*[unique(c),1]);
+%! % The Fc column should report 100, 250, 500, 1000, 10000 more or less.
+
+%!demo
+%! c = anderson_darling_test(rand(12,10000),'uniform');
+%! hist(100*c,1:2:99);
+%! % The histogram should be flat more or less.
