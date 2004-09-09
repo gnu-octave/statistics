567cf16d39393a8cd9f853523d70a30a36f0e736
Paul Kienzle <pkienzle@users.sf.net>
2004-09-09 15:58:56 +0000

    small spelling correction; faster and cleaner implementation; added demo
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@1753
    hg:729b0e6719f9e28b6a7d9f9939c0c833fef76ee7

--- pareto.m
+++ pareto.m
@@ -33,8 +33,8 @@
 ##
 ## pareto (data) produces a Pareto chart where the values in the vector data are
 ## drawn as bars in descending order and labels each bar with its index.
-## pareto (data, vertical) does the same but if vertical is 0 the bar will be drawn
-## orizzontaly.
+## pareto (data, vertical) does the same but if vertical is 0 the bar
+## will be drawn horizontally.
 ##
 ## pareto (data, labels, vertical) or pareto (data, vertical, labels) 
 ## labels each element of data with the values from labels.
@@ -60,117 +60,100 @@
 
 function pareto (varargin)
 
-if nargin < 1 || nargin > 3
-   usage("pareto (data,labels,vertical)");
-endif
-
-data = varargin{1};
-if (min(size(data))~=1)
-   error("data must be a vector.");
-endif
-vertical = 1;
-
-if nargin == 2
-   if isscalar(varargin {2}) 
-     vertical = varargin {2};
-   else
-     labels = varargin {2};
-   endif
-endif
-
-if nargin == 3
-   if isscalar(varargin {2}) 
-     vertical = varargin {2};
-     labels = varargin {3};
-   else 
-     vertical = varargin {3};
-     labels = varargin {2};
-   endif
-endif
-
-data = data(:);
-if exist ("labels") && (length(labels) ~= length(data))
-  error("size of labels and data must be the same.");
-endif
-
-## sort data in decreasing order
-[y,j]=sort(data);
-y = flipud(y);
-j = flipud(j);
-
-## sort labels
-if exist ("labels")
-  oldlabels = labels;
-  ## use numbers as labels
-  if isstr(oldlabels) ~= 1
-    labels = [];
-    for i=1:1:length(oldlabels)
-      labels = [labels; num2str(oldlabels(i))];
-    endfor
+  if nargin < 1 || nargin > 3
+    usage("pareto (data,labels,vertical)");
   endif
+  
+  data = varargin{1};
+  if (min(size(data))~=1)
+    error("data must be a vector.");
+  endif
+  vertical = 1;
 
-  oldlabels = labels;
-  for i=1:1:length(oldlabels)
-    labels(i,:) = oldlabels(j(i),:);
-  endfor
-else ## use positions as labels
-  labels = [];
-  for i=1:1:length(j)
-    labels = [labels; num2str(j(i))];
-  endfor
-endif
-
-## compute the cumulative frequency
-cum = cumsum (y);
-cum = cum/cum(length(cum))*100;
-
-## seek 80-95 boundary
-for i=1:1:length(cum)
-  if cum(i) == 80 A = i;
-  elseif cum(i) < 80 && cum(i+1) >80
-    if abs(cum(i) - 80) < abs(cum(i+1) - 80)
-      A = i;
-    else A = i+1;
+  if nargin == 2
+    if isscalar(varargin {2}) 
+      vertical = varargin {2};
+    else
+      labels = varargin {2};
     endif
   endif
 
-  if cum(i) == 95 B = i;
-  elseif cum(i) < 95 && cum(i+1) >95
-    if abs(cum(i) - 95) < abs(cum(i+1) - 95)
-      B = i;
-    else B = i+1;
+  if nargin == 3
+    if isscalar(varargin {2}) 
+      vertical = varargin {2};
+      labels = varargin {3};
+    else 
+      vertical = varargin {3};
+      labels = varargin {2};
     endif
   endif
-endfor
+  
+  data = data(:);
+  if exist ("labels") && (length(labels) ~= length(data))
+    error("size of labels and data must be the same.");
+  endif
 
-[xb, yb] =  bar (y);
-dataA = [xb, yb](1:1:3*A+1,:);
-dataAB = [xb, yb](3*A+1:1:3*B+1,:);
-dataB = [xb, yb](3*B+1:1:length(yb),:);
-datacum = [[1:1:length(cum)]', cum];
+  ## sort data in decreasing order
+  [y,j]=sort(data);
+  y = flipud(y);
+  j = flipud(j);
+  n = length(data);
+  
+  ## by default use position number in original vector as a label
+  if ~exist ("labels"), labels = [1:n]; end
+  
+  ## check if using numbers as labels
+  if ~isstr(labels), labels = num2str(labels(:)); endif
+  
+  ## sort labels along with data
+  labels = labels(j,:);
+  
+  ## compute the cumulative frequency
+  cum = cumsum (y);
 
-## draw pareto chart (bar graph plus Lorentz function)
-if vertical ~= 0
-  if exist("labels")
-    tics ("x", 1:1:length(y), labels);
+  ## seek 80% and 95% boundaries
+  p80 = .8*cum(end);
+  p95 = .95*cum(end);
+  [m,Aidx] = min(abs(cum-p80));
+  [m,Bidx] = min(abs(cum-p95));
+
+  [xb, yb] =  bar (y);
+  A = 1:3*Aidx+1;
+  AB = 3*Aidx+1:3*Bidx+1;
+  B = 3*Bidx+1:length(yb);
+
+  ## draw pareto chart (bar graph plus Lorentz function)
+  text;
+  if vertical ~= 0
+    tics ("x", 1:n, labels);
     tics ("y");
-  endif
-###
-  gplot dataA axes x1y1 with lines 1, dataAB axes x1y1 with lines 7, dataB axes x1y1 with lines 3, datacum axes x1y2 with linespoints 13
-  gset y2tics; gset nox2tics; replot;
-  legend ("hide");
-### plot (xb , yb, "b-;;", [1:1:length(cum)], cum*max(y)/100, "c-*;;");
-else
-  if exist("labels")
-    tics ("y", 1:1:length(y), labels);
+    plot(xb(A),yb(A),"b-;;", xb(AB), yb(AB),"c-;;", xb(B), yb(B),"m-;;",
+	 [1:Aidx], cum(1:Aidx), "b-*;;", 
+	 [Aidx:Bidx], cum(Aidx:Bidx), "c-*;;",
+	 [Bidx:n], cum(Bidx:end), "m-*;;",
+	 [1,n+.5],[p80,p80],"b-;;",
+	 [1,n+.5],[p95,p95],"m-;;");
+    text(.5,p80," 80%",'HorizontalAlignment','left');
+    text(.5,p95," 95%",'HorizontalAlignment','left');
+  else
+    tics ("y", [1:n], labels);
     tics ("x");
+    plot(yb(A),xb(A),"b-;;", yb(AB), xb(AB),"c-;;", yb(B), xb(B),"m-;;",
+	 cum(1:Aidx), [1:Aidx], "b-*;;", 
+	 cum(Aidx:Bidx), [Aidx:Bidx], "c-*;;",
+	 cum(Bidx:end), [Bidx:n], "m-*;;",
+	 [p80,p80],[.5,n+.5],"b-;;",
+	 [p95,p95],[.5,n+.5],"m-;;");
+    text(p80,n,"80%",'HorizontalAlignment','right');
+    text(p95,n,"95%",'HorizontalAlignment','right');
   endif
-###
-  gplot fliplr(dataA) axes x1y1 with lines 1, fliplr(dataAB) axes x1y1 with lines 7, fliplr(dataB) axes x1y1 with lines 3, fliplr(datacum) axes x2y1 with linespoints 13
-  gset x2tics; gset noy2tics; replot;
-  legend ("hide");
-###  plot (yb , xb, "b-;;", cum*max(y)/100, [1:1:length(cum)], "r-*;;");
-endif
-
+  
 endfunction
 
+%!demo
+%! % Suppose that we want establish which products makes 80 % of turnover.
+%! CODES = ["AB4";"BD7";"CF8";"CC5";"AD11";"BB5";"BB3";"AD8";"DF3";"DE7"];
+%! Value = [2.35 7.9 2.45 1.1 0.15 13.45 5.4 2.05 0.85  1.65]';
+%! SoldUnits = [54723 41114 16939 1576091 168000 687197 120222 168195, ...
+%!              1084118 55576]';
+%! pareto (Value.*SoldUnits, CODES, 0);
