de345912cd30d11ab99b3fef4c23af44b4033978
Paul Kienzle <pkienzle@users.sf.net>
2006-12-02 21:16:19 +0000

    [for Martijn van Oosterhout] likelihood and distribution for gamma and fit
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@2944
    hg:954b5d7524fc358255a257d7e8091891f69a58b7

---
+++ inst/gamlike.m
@@ -0,0 +1,18 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} X = gamlike ([@var{A} @var{B}], @var{R})
+## Calculates the negative log-likelihood function for the Gamma
+## distribution over vector R, with the given parameters A and B.
+## @seealso{gampdf, gaminv, gamrnd, gamfit}
+## @end deftypefn
+
+## Written by Martijn van Oosterhout <kleptog@svana.org> (Nov 2006)
+## This code is public domain
+
+function res = gamlike(P,K)
+
+a=P(1);
+b=P(2);
+
+res = -sum( log( gampdf(K, a, b) ) )
+
+ 	  	 
