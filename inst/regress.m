c49dbf4c7cd3ac553cbf72f6d268b6c6fc99e104
David Bateman <adb104@users.sf.net>
2007-01-29 19:51:53 +0000

    Relax the error tolerance of test to allow for random failures of test code
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@3155
    hg:a1dbe2c344824c6a69e76f3115bc38e879f2d455

--- inst/regress.m
+++ inst/regress.m
@@ -215,4 +215,4 @@ endfunction
 %! assert(b,V(:,1),3e-6);
 %! assert(stats(1),Rsq,3e-13);
 %! assert(stats(2),F,3e-8);
-%! assert(((bint(:,1)-bint(:,2))/2)/t_inv(alpha/2,9),V(:,2),7.6e-3);
+%! assert(((bint(:,1)-bint(:,2))/2)/t_inv(alpha/2,9),V(:,2),1.e-2);
