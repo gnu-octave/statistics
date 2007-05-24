6672001cbe10c563c117c44d5e5416b6905ac7b2
David Bateman <adb104@users.sf.net>
2007-05-24 19:20:21 +0000

    Enlarge error tolerance slightly for regress
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@3542
    hg:5cd86a1f0ed01fe33f557156b0135f3b1f2df2ff

--- inst/regress.m
+++ inst/regress.m
@@ -213,6 +213,6 @@ endfunction
 %! alpha = 0.05;
 %! [b, bint, r, rint, stats] = regress (y, X, alpha);
 %! assert(b,V(:,1),3e-6);
-%! assert(stats(1),Rsq,3e-13);
+%! assert(stats(1),Rsq,1e-12);
 %! assert(stats(2),F,3e-8);
 %! assert(((bint(:,1)-bint(:,2))/2)/t_inv(alpha/2,9),V(:,2),-1.e-5);
