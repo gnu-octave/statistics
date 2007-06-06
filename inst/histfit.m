c055376c136150016f3ef1633ae58c93c5c18bb4
David Bateman <adb104@users.sf.net>
2007-06-06 20:12:34 +0000

    normal_pdf takes the variance and not the std (For Bernado D'Auria)
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@3637
    hg:5a4e5180bba47e0d28cfb9d07537291c9453acd0

--- inst/histfit.m
+++ inst/histfit.m
@@ -57,7 +57,7 @@ mr = nanmean(data); ## Estimates the parameter, MU, of the normal distribution.
 sr = nanstd(data);  ## Estimates the parameter, SIGMA, of the normal distribution.
 x=(-3*sr+mr:0.1*sr:3*sr+mr)';## Evenly spaced samples of the expected data range.
 [xb,yb] = bar(xbin,n);
-y = normal_pdf(x,mr,sr);  
+y = normal_pdf(x,mr,sr.^2);  
 binwidth = xbin(2)-xbin(1);
 y = row*y*binwidth;   ## Normalization necessary to overplot the histogram.
 plot(xb,yb,";;b",x,y,";;r-");     ## Plots density line over histogram.
