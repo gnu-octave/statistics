6379258b8ff80e04823f1a5abf2e81eae62f3e96
Søren Hauberg <hauberg@users.sf.net>
2008-10-10 09:50:12 +0000

    Use texinfo for help, and let the code style match the Octave style better
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@5349
    hg:dc50ee9469710227109170937950fd4ed8f569bb

--- inst/mad.m
+++ inst/mad.m
@@ -13,21 +13,41 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
-## a = mad(X)
-##    mean absolute deviation of X
-function a = mad(X,dim)
-  if nargin == 1
-    dim = min(find(size(X)>1));
-    if isempty(dim), dim=1; endif;
+## -*- texinfo -*-
+## @deftypefn {Function File} mad (@var{x})
+## @deftypefnx{Function File} mad (@var{x}, @var{dim})
+## Compute the mean absolute deviation of @var{x}.
+## @seealso{std}
+## @end deftypefn
+
+function a = mad (X, dim)
+  ## Check input
+  if (nargin < 1)
+    print_usage ();
   endif
-  if (nargin != 1 && nargin != 2)
-    usage("a = mad (X,dim)");
-  elseif (prod(size(X)) != size(X,dim))
-    sz = ones(1,length(size(X)));
-    sz(dim) = size(X,dim);
+  
+  if (!isnumeric (X))
+    error ("mad: first input must be numeric");
+  endif
+  
+  if (nargin == 1)
+    dim = min (find (size (X) > 1));
+    if (isempty(dim))
+      dim = 1;
+    endif
+  endif
+  
+  if (!isscalar (dim))
+    error ("mad: dimension argument must be a scalar");
+  endif
+  
+  ## Compute the mad
+  if (prod(size(X)) != size(X,dim))
+    sz = ones (1, length (size (X)));
+    sz (dim) = size (X,dim);
     a = nanmean (abs (X - repmat (nanmean (X, dim), sz)), dim);
-  elseif all (size (X) > 1)
-    a = nanmean (abs (X - ones(size(X,1),1) * nanmean(X, dim)), dim);
+  elseif (all (size (X) > 1))
+    a = nanmean (abs (X - ones (size(X, 1), 1) * nanmean (X, dim)), dim);
   else
     a = nanmean (abs (X - nanmean(X, dim)), dim);
   endif
