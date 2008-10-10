decc09fd55c4aceb8a92266ee3608c4af4c780c0
Søren Hauberg <hauberg@users.sf.net>
2008-10-10 10:33:48 +0000

    Document newly added flag
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@5356
    hg:6a794f4f1cff9ac890a15539dba2204316a7c211

--- inst/mad.m
+++ inst/mad.m
@@ -15,8 +15,9 @@
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} mad (@var{x})
-## @deftypefnx{Function File} mad (@var{x}, @var{dim})
-## Compute the mean absolute deviation of @var{x}.
+## @deftypefnx{Function File} mad (@var{x}, @var{flag})
+## @deftypefnx{Function File} mad (@var{x}, @var{flag}, @var{dim})
+## Compute the mean/median absolute deviation of @var{x}.
 ##
 ## The mean absolute deviation is computed as
 ##
@@ -24,44 +25,68 @@
 ## mean (abs (@var{x} - mean (@var{x})))
 ## @end example
 ##
+## and the median absolute deviation is computed as
+##
+## @example
+## median (abs (@var{x} - median (@var{x})))
+## @end example
+##
 ## Elements of @var{x} containing NaN or NA values are ignored during computations.
+##
+## If @var{flag} is 0, the absolute mean deviation is computed, and if @var{flag}
+## is 1, the absolute median deviation is computed. By default @var{flag} is 0.
+##
 ## This is done along the dimension @var{dim} of @var{x}. If this variable is not
-## given, the mean absolute devation s computed along the smallest dimension of
+## given, the mean/median absolute deviation s computed along the smallest dimension of
 ## @var{x}.
 ##
 ## @seealso{std}
 ## @end deftypefn
 
-function a = mad (X, dim)
+function a = mad (X, flag = 0, dim = [])
   ## Check input
   if (nargin < 1)
     print_usage ();
+  endif
+  if (nargin > 3)
+    error ("mad: too many input arguments");
   endif
   
   if (!isnumeric (X))
     error ("mad: first input must be numeric");
   endif
   
-  if (nargin == 1)
+  if (isempty (dim))
     dim = min (find (size (X) > 1));
     if (isempty(dim))
       dim = 1;
     endif
   endif
   
+  if (!isscalar (flag))
+    error ("mad: second input argument must be a scalar");
+  endif
   if (!isscalar (dim))
     error ("mad: dimension argument must be a scalar");
+  endif
+  
+  if (flag == 0)
+    f = @nanmean;
+  elseif (flag == 1)
+    f = @nanmedian;
+  else
+    error ("mad: second input argument must be 0 or 1");
   endif
   
   ## Compute the mad
   if (prod(size(X)) != size(X,dim))
     sz = ones (1, length (size (X)));
     sz (dim) = size (X,dim);
-    a = nanmean (abs (X - repmat (nanmean (X, dim), sz)), dim);
+    a = f (abs (X - repmat (f (X, dim), sz)), dim);
   elseif (all (size (X) > 1))
-    a = nanmean (abs (X - ones (size(X, 1), 1) * nanmean (X, dim)), dim);
+    a = f (abs (X - ones (size(X, 1), 1) * f (X, dim)), dim);
   else
-    a = nanmean (abs (X - nanmean(X, dim)), dim);
+    a = f (abs (X - f(X, dim)), dim);
   endif
 endfunction
 
