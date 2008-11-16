6528d958e672b46644ac554233334998e5ce3f3b
Francesco Potortì <fpoto@users.sf.net>
2008-11-16 19:49:09 +0000

    Corrected a typo: now "complete" works. Added more tests. Performance improvements.
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@5439
    hg:3aa083c9b4d5b5c961ad659d48532fc2c95bf73f

--- inst/linkage.m
+++ inst/linkage.m
@@ -22,6 +22,17 @@
 ## Return clusters generated from a distance vector created by the pdist
 ## function.
 ##
+## @var{x} is the dissimilarity matrix relative to @var{n} observations,
+## formatted as a @math{(n-1)*n/2}x1 vector as produced by @code{pdist}.
+## @code{linkage} starts by putting each observation into a singleton
+## cluster and numbering those from 1 to @var{n}.  Then it merges two
+## clusters, chosen according to @var{method}, to create a new cluster
+## numbered @var{n+1}, and so on until all observations are grouped into
+## a single cluster numbered @var{2*n-1}.  Row @var{m} of the
+## @math{m-1}x3 output matrix relates to cluster @math{n+m}: the first
+## two columns are the numbers of the two component clusters and column
+## 3 contains their distance.
+##
 ## Methods can be:
 ##
 ## @table @samp
@@ -50,16 +61,6 @@
 ## Inner squared distance (minimum variance)
 ##
 ## @end table
-##
-## @var{x} is the dissimilarity matrix relative to @var{n} observations,
-## formatted as a @math{(n-1)*n/2}x1 vector as produced by @code{pdist}.
-## @code{linkage} starts by putting each observation into a singleton
-## cluster and numbering those from 1 to @var{n}.  Then it merges two
-## clusters to create a new cluster numbered @var{n+1}, and so on
-## until all observations are grouped into a single cluster numbered
-## @var{2*n-1}.  Row @var{m} of the @math{m-1}x3 output matrix relates
-## to cluster @math{n+m}: the first two columns are the numbers of the
-## two component clusters and column 3 contains their distance.
 ##
 ## @seealso{cluster,pdist}
 ## @end deftypefn
@@ -90,43 +91,46 @@ function y = linkage (x, method)
       ## this is just a minimal spanning tree
       findfxn = @min;
     case "complete"
-      error ("linkage: %s is not yet implemented", method);
       findfxn = @max;
-    case { "median", "weighted" }
-      findfxn = @mean;
-    case { "average", "centroid", "ward" }
+    case { "median", "weighted", "average", "centroid", "ward" }
       error ("linkage: %s is not yet implemented", method);
     otherwise
       error ("linkage: %s: unknown method", method);
   endswitch
 
-  dissim = squareform (x, "tomatrix");
-  startsize = size (dissim, 1);
-  y = zeros (startsize - 1, 3);
-  cnameidx = 1:startsize;
-  for yidx = 1:startsize-1
-    ## Find the two nearest clusters.
-    available = logical(tril (ones(size(dissim))) - eye(size(dissim)));
-    [r, c] = find (min (dissim(available)) == dissim, 1);
-    ## Here is the new cluster.
-    y(yidx, :) = [cnameidx(r) cnameidx(c) dissim(r, c)];
-    ## Add it as a new cluster index and remove the old ones.
-    cnameidx(r) = yidx + startsize;
-    cnameidx(c) = [];
-    ## Update the dissimilarity matrix
+  dissim = squareform (x, "tomatrix"); # dissimilarity matrix in square format
+  n = rows (dissim);		       # the number of observations
+  diagidx = sub2ind ([n,n], 1:n, 1:n); # indices of diagonal elements
+  dissim(diagidx) = Inf;	# consider a cluster as far from itself
+  cname = 1:n;			# cluster names in dissim
+  y = zeros (n-1, 3);		# clusters from n+1 to 2*n-1
+  for yidx = 1:n-1
+    ## Find the two nearest clusters
+    [m midx] = min (dissim(:));	# the min distance and its first index
+    [r, c] = ind2sub (size (dissim), midx); # row and col number
+    ## Here is the new cluster
+    y(yidx, :) = [cname(r) cname(c) dissim(r, c)];
+    ## Add it as a new cluster index and remove the old ones
+    cname(r) = yidx + n;
+    cname(c) = [];
+    ## Add the new dissimilarities and remove the old ones
     newdissim = findfxn (dissim([r c], :));
+    newdissim(r) = Inf;
     dissim(r,:) = newdissim;
     dissim(:,r) = newdissim';
-    dissim(r,r) = 0;
     dissim(c,:) = [];
     dissim(:,c) = [];
   endfor
 
 endfunction
 
-%!shared xy, t
-%! xy = [3 1.7; 1 1; 2 3; 2 2.5; 1.2 1; 1.1 1.5; 3 1];
+%!shared x, y, t
+%! x = [3 1.7; 1 1; 2 3; 2 2.5; 1.2 1; 1.1 1.5; 3 1];
+%! y = reshape(mod(magic(6),5),[],3);
 %! t = 1e-6;
-%!assert (cond (linkage (pdist (xy))),             66.534612, t);
-%!assert (cond (linkage (pdist (xy), "single")),   66.534612, t);
-%!assert (cond (linkage (pdist (xy), "complete")), 27.071750, t);
+%!assert (cond (linkage (pdist (x))),             66.534612, t);
+%!assert (cond (linkage (pdist (y))),             34.945071, t);
+%!assert (cond (linkage (pdist (x), "single")),   66.534612, t);
+%!assert (cond (linkage (pdist (y), "single")),   34.945071, t);
+%!assert (cond (linkage (pdist (x), "complete")), 27.071750, t);
+%!assert (cond (linkage (pdist (y), "complete")), 20.296516, t);
