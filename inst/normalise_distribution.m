a6b8e4ffbe66f3edc148048ad9defbfdaa69369e
David Miguel Susano Pinto <carandraug@octave.org>
2011-11-08 11:35:49 +0000

    normalise_distribution: use fix on revision 9011 only in the octave 3.4 series
    svn:fc592f45-0881-4044-91ff-c96495a54512/trunk/octave-forge@9028
    hg:c1bc0d36ce3971cc8e9d99c5f446e87c8c074c0d

--- inst/normalise_distribution.m
+++ inst/normalise_distribution.m
@@ -194,19 +194,20 @@ function [ normalised ] = normalise_distribution ( data, distribution, dimension
 
       #Put normalised values in the places where they belong.
 
-      ## XXX the following 2 commented lines code worked fine in 3.2 but a
-      ## regression makes it fail in 3.4.3 (at least). It seems to be already
-      ## fixed on the dev release
+      ## A regression in the 3.4 series made this no longer work so we behave
+      ## differently depending on octave version. This applies the fix for all
+      ## 3.4 releases but it probably only appeared on 3.4.3 (can someone check?)
       ## See https://savannah.gnu.org/bugs/index.php?34765
-      ## Eventually, the following 2 lines can be uncommented and the for
-      ## loop removed
-#      f_remap = @( k ) ( normal ( k ) );
-#      normalised ( :, k ) = arrayfun ( f_remap, target_indices );
-      ## XXX start of fix
-      for index = 1:numel(target_indices)
-        normalised ( index, k ) = normal(target_indices(index));
-      endfor
-      ## XXX end of fix
+      if (compare_versions (OCTAVE_VERSION, "3.4", "<") || compare_versions (OCTAVE_VERSION, "3.6", ">"))
+        ## this is how it should work
+        f_remap = @( k ) ( normal ( k ) );
+        normalised ( :, k ) = arrayfun ( f_remap, target_indices );
+      else
+        ## this is the workaround because of bug in 3.4.??
+        for index = 1:numel(target_indices)
+          normalised ( index, k ) = normal(target_indices(index));
+        endfor
+      endif
 
     end
 
