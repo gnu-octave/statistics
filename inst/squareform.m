9a93a87e78996c04794e75c4044dfacee628e5d4
David Miguel Susano Pinto <carandraug@octave.org>
2015-06-04 19:16:46 +0100

    squareform: default to "tomatrix" for 1 element input.
    * squareform.m: currently code throws an error when input has 1 element
    and METHOD is not specified.  We now default to "tomatrix" (Matlab
    compatibility as well).  Also drop use of usage() (deprecated in core),
    some changes to input parsing (change ifs into a switch block and skip
    if method is a string for compliance with code guidelines).
    * NEWS: make note of this improvement but no details as it's very minor.
    
    hg:b68af186cd34b9faf5a0509a9699bbb69b48ec92

--- inst/squareform.m
+++ inst/squareform.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2014 JD Walsh. Based on code (C) 2006, 2008 Bill Denney
+## Copyright (C) 2015 Carnë Draug <carandraug@octave.org>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -14,11 +15,10 @@
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{y} =} squareform (@var{x})
-## @deftypefnx {Function File} {@var{y} =} squareform (@var{x}, @
-## "tovector")
-## @deftypefnx {Function File} {@var{y} =} squareform (@var{x}, @
-## "tomatrix")
+## @deftypefn  {Function File} {@var{z} =} squareform (@var{y})
+## @deftypefn  {Function File} {@var{y} =} squareform (@var{z})
+## @deftypefnx {Function File} {@var{z} =} squareform (@var{y}, @qcode{"tovector"})
+## @deftypefnx {Function File} {@var{y} =} squareform (@var{z}, @qcode{"tomatrix"})
 ## Converts a vector from the @code{pdist} function into a distance matrix or
 ## a distance matrix back to vector form.
 ##
@@ -31,7 +31,8 @@
 ## @var{x} is not symmetric.
 ##
 ## The second argument is used to specify the output type in case there
-## is a single element.
+## is a single element.  It will defaults to @qcode{"tomatrix"} otherwise.
+##
 ## @seealso{pdist}
 ## @end deftypefn
 
@@ -42,64 +43,63 @@
 
 function y = squareform (x, method)
 
-  if ((nargin < 1) ||  (~isnumeric(x)))
-    usage (['squareform: must input distance matrix or vector; see help']);
+  if (nargin < 1 || nargin > 2)
+    print_usage ();
+  elseif (! isnumeric (x) || ! ismatrix (x))
+    error ("squareform: Y or Z must be a numeric matrix or vector");
   endif
 
-  if (nargin < 2)
-    if (isscalar (x) || isvector (x))
-      method = 'tomatrix';
-    elseif (issquare (x))
-      method = 'tovector';
+  if (nargin == 1)
+    ## This is ambiguous when numel (x) == 1, but that's the whole reason
+    ## why the "method" option exists.
+    if (isvector (x))
+      method = "tomatrix";
     else
-      usage ('squareform: must input distance matrix or vector; see help');
-    endif
-  else
-    method = lower (method);
-    if (~any (strcmp ({'tovector' 'tomatrix'}, method)))
-      usage ('squareform: method must be either "tovector" or "tomatrix"');
+      method = "tovector";
     endif
   endif
 
-  if (strcmp ('tovector', method))
-    if (~issquare (x))
-      usage ('squareform: x is not a square matrix');
-    elseif (any (diag (x) ~= 0))
-      usage ('squareform: x is not a hollow matrix');
-    elseif (~issymmetric(x))
-      warning ('squareform:symmetric', ...
-               'squareform: x is not a symmetric matrix');
-    endif
+  switch (tolower (method))
+    case "tovector"
+      if (~issquare (x))
+        usage ('squareform: x is not a square matrix');
+      elseif (any (diag (x) ~= 0))
+        usage ('squareform: x is not a hollow matrix');
+      elseif (~issymmetric(x))
+        warning ('squareform:symmetric', ...
+                 'squareform: x is not a symmetric matrix');
+      endif
 
-    sx = size (x, 1);
-    y = zeros (1, (sx - 1) * sx / 2);
-    idx = 1;
-    for i = 2 : sx
-      newidx = idx + sx - i;
-      y(1, idx:newidx) = x(i:sx, i-1);
-      idx = newidx + 1;
-    endfor
-  else
-    ## we're converting to a matrix
-
-    ## make sure that x is a column
-    x = x(:);
-
-    ## the dimensions of y are the solution to the quadratic formula for:
-    ## length (x) = (sy - 1) * (sy / 2)
-    sy = (1 + sqrt (1 + 8 * length (x))) / 2;
-    if (floor (sy) ~= sy)
-      usage ('squareform: incorrect vector size; see help');
-    else
-      y = zeros (sy);
-      for i = 1 : sy-1
-        step = sy - i;
-        y((sy-step+1):sy, i) = x(1:step);
-        x(1:step) = [];
+      sx = size (x, 1);
+      y = zeros (1, (sx - 1) * sx / 2);
+      idx = 1;
+      for i = 2 : sx
+        newidx = idx + sx - i;
+        y(1, idx:newidx) = x(i:sx, i-1);
+        idx = newidx + 1;
       endfor
-      y = y + y';
-    endif
-  endif
+
+    case "tomatrix"
+      ## make sure that x is a column
+      x = x(:);
+
+      ## the dimensions of y are the solution to the quadratic formula for:
+      ## length (x) = (sy - 1) * (sy / 2)
+      sy = (1 + sqrt (1 + 8 * length (x))) / 2;
+      if (floor (sy) ~= sy)
+        usage ('squareform: incorrect vector size; see help');
+      else
+        y = zeros (sy);
+        for i = 1 : sy-1
+          step = sy - i;
+          y((sy-step+1):sy, i) = x(1:step);
+          x(1:step) = [];
+        endfor
+        y = y + y';
+      endif
+    otherwise
+      error ("squareform: invalid METHOD '%s'", method);
+  endswitch
 
 endfunction
 
@@ -113,3 +113,6 @@ endfunction
 %!assert(squareform(1), [0 1;1 0])
 %!assert(squareform(1, "tomatrix"), [0 1;1 0])
 %!assert(squareform(0, "tovector"), zeros(1,0))
+
+## default to "tomatrix" for 1 element input
+%!assert (squareform (1), [0 1; 1 0])
