de588eae976a59149b89542ce0944dfe92c19e49
David Miguel Susano Pinto <carandraug@octave.org>
2015-06-06 14:45:47 +0100

    nansum: note extra options for sum() and add tests.
    hg:b15b513a726577a806045e540659a23d3dab5af6

--- inst/nansum.m
+++ inst/nansum.m
@@ -14,22 +14,39 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {@var{v} =} nansum (@var{X})
-## @deftypefnx{Function File} {@var{v} =} nansum (@var{X}, @var{dim})
+## @deftypefn  {Built-in Function} {} nansum (@var{x})
+## @deftypefnx {Built-in Function} {} nansum (@var{x}, @var{dim})
+## @deftypefnx {Built-in Function} {} nansum (@dots{}, @qcode{"native"})
+## @deftypefnx {Built-in Function} {} nansum (@dots{}, @qcode{"double"})
+## @deftypefnx {Built-in Function} {} nansum (@dots{}, @qcode{"extra"})
 ## Compute the sum while ignoring NaN values.
 ##
-## @code{nansum} is identical to the @code{sum} function except that NaN values are
-## treated as 0 and so ignored.  If all values are NaN, the sum is 
+## @code{nansum} is identical to the @code{sum} function except that NaN
+## values are treated as 0 and so ignored.  If all values are NaN, the sum is
 ## returned as 0.
+##
+## See help text of @code{sum} for details on the options.
 ##
 ## @seealso{sum, nanmin, nanmax, nanmean, nanmedian}
 ## @end deftypefn
 
 function v = nansum (X, varargin)
-  if nargin < 1
-    print_usage;
+  if (nargin < 1)
+    print_usage ();
   else
-    X(isnan(X)) = 0;
+    X(isnan (X)) = 0;
     v = sum (X, varargin{:});
   endif
 endfunction
+
+%!assert (nansum ([2 4 NaN 7]), 13)
+%!assert (nansum ([2 4 NaN Inf]), Inf)
+
+%!assert (nansum ([1 NaN 3; NaN 5 6; 7 8 NaN]), [8 13 9])
+%!assert (nansum ([1 NaN 3; NaN 5 6; 7 8 NaN], 2), [4; 11; 15])
+%!assert (nansum (single ([1 NaN 3; NaN 5 6; 7 8 NaN])), single ([8 13 9]))
+%!assert (nansum (single ([1 NaN 3; NaN 5 6; 7 8 NaN]), "double"), [8 13 9])
+
+%!assert (nansum (uint8 ([2 4 1 7])), 14)
+%!assert (nansum (uint8 ([2 4 1 7]), "native"), uint8 (14))
+%!assert (nansum (uint8 ([2 4 1 7])), 14)
