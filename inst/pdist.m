e8f46306b3d8023ed1319c08afdac50d662fedab
David Miguel Susano Pinto <carandraug@octave.org>
2015-07-08 15:59:07 +0100

    maint: run dos2unix to convert line endings to unix.
    hg:22eab24069a7aeb3217917b5be8234d9641ccf74

--- inst/pdist.m
+++ inst/pdist.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2008 Francesco Potortì <pot@gnu.org>
+## Copyright (C) 2008 Francesco PotortÃ¬ <pot@gnu.org>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -87,7 +87,7 @@
 ## @seealso{linkage, mahalanobis, squareform, pdist2}
 ## @end deftypefn
 
-## Author: Francesco Potortì  <pot@gnu.org>
+## Author: Francesco PotortÃ¬  <pot@gnu.org>
 
 function y = pdist (x, metric, varargin)
 
