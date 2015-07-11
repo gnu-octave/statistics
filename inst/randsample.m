6b1880fa85a510563e6f3ad8a439d0d9e7972ce9
Nir Krakauer <mail@nirkrakauer.net>
2015-07-11 11:30:22 -0400

    randsample: allow weighted sampling without replacement based on patch by Lachlan Andrew (#savannah-bug:45518)
    hg:a53e2e73e315ce7d7daf8cfabdb1ae2260f30020

--- inst/randsample.m
+++ inst/randsample.m
@@ -1,6 +1,6 @@
 ## Copyright (C) 2014 - Nir Krakauer
 ##
-## This progrm is free software; you can redistribute it and/or modify
+## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 3 of the License, or
 ## (at your option) any later version.
@@ -23,7 +23,7 @@
 ##
 ## If @var{v} is a scalar, samples from 1:@var{v}.
 ##
-## If sampling with replacement, can specify a weight vector @var{w} of the same size as @var{v} such that the probablility of each element being sampled is proportional to @var{w}.
+## If a weight vector @var{w} of the same size as @var{v} is specified, the probablility of each element being sampled is proportional to @var{w}.  Unlike Matlab's function of the same name, this can be done for sampling with or without replacement.
 ##
 ## Randomization is performed using rand().
 ##
@@ -32,31 +32,49 @@
 
 function y = randsample(v,k,replacement=false,w=[])
 
-  if isscalar (v)
+  if (isscalar (v) && isreal (v))
     n = v;
     vector_v = false;
-  elseif isvector (v)
+  elseif (isvector (v))
     n = numel (v);
     vector_v = true;
   else
-    error ('Octave:invalid-input-arg', 'The input v must be a vector or positive integer.');
+    error ('Octave:invalid-input-arg', 'randsample: The input v must be a vector or positive integer.');
   endif
    
-  if k < 0
-    error ('Octave:invalid-input-arg', 'The input k must be a positive integer.');
+  if k < 0 || ( k > n && !replacement )
+    error ('Octave:invalid-input-arg', 'randsample: The input k must be a non-negative integer. Sampling without replacement needs k <= n.');
+  endif
+
+  if (all (length (w) != [0, n]))
+    error ('Octave:invalid-input-arg', 'randsample: the size w (%d) must match the first argument (%d)', length(w), n);
   endif
 
 
-  if replacement #sample with replacement
-    if isempty (w) #all elements are equally likely to be sampled
+  if (replacement)               # sample with replacement
+    if (isempty (w))             # all elements are equally likely to be sampled
       y = round (n * rand(1, k) + 0.5);
     else
-      w = w / sum(w);
-      w = [0 cumsum(w(:))'];
-      y = arrayfun(@(x) find(w <= x, 1, "last"), rand (1, k)); #distribute k uniform random deviates based on the given weighting
+      y = weighted_replacement (k, w);
     endif
-  else #sample without replacement
-    y = randperm (n, k);
+   else                           # sample without replacement
+     if (isempty (w))             # all elements are equally likely to be sampled
+       y = randperm (n, k);
+     else                         # use "accept-reject"-like sampling
+       y = weighted_replacement (k, w);
+       while (1)
+         [yy, idx] = sort (y);    # Note: sort keeps order of equal elements.
+         Idup = [false, (diff (yy)==0)];
+         if !any (Idup)
+           break
+         else
+           Idup(idx) = Idup;      # find duplicates in original vector
+           w(y) = 0;              # don't permit resampling
+                      # remove duplicates, then sample again
+           y = [y(~Idup), (weighted_replacement (sum (Idup), w))];
+         endif
+       endwhile
+     endif
   endif
   
   if vector_v
@@ -65,6 +83,12 @@ function y = randsample(v,k,replacement=false,w=[])
   
 endfunction 
  
+function y = weighted_replacement (k, w)
+  w = w / sum(w);
+  w = [0 cumsum(w(:))'];
+        # distribute k uniform random deviates based on the given weighting
+  y = arrayfun (@(x) find (w <= x, 1, "last"), rand (1, k));
+endfunction
 
 %!test
 %! n = 20;
@@ -93,3 +117,11 @@ endfunction
 %! assert (size(x), [k 1]);
 %! x = randsample((1:n)', k, true, ones(n, 1));
 %! assert (size(x), [k 1]);
+%! n = 10;
+%! k = 100;
+%! x = randsample(n, k, true, 1:n);
+%! assert (size(x), [1 k]);
+%! x = randsample((1:n)', k, true);
+%! assert (size(x), [k 1]);
+%! x = randsample(k, k, false, 1:k);
+%! assert (size(x), [1 k]);
