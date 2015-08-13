309b88da2b1a20dde859f969c4d7e96398965dd1
David Miguel Susano Pinto <carandraug@octave.org>
2015-08-13 20:28:52 +0100

    maint: fix license for hist3 (this is not Octave and update address)
    hg:5efadbf5975c24c4113393b92caf0e08e8db6652

--- inst/hist3.m
+++ inst/hist3.m
@@ -1,21 +1,18 @@
 ## Copyright (C) 2007 Roman Stanchak
 ##
-## This file is part of Octave.
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
 ##
-## Octave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2, or (at your option)
-## any later version.
-##
-## Octave is distributed in the hope that it will be useful, but
+## This program is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ## General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, write to the Free
-## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-## 02110-1301, USA.
+## along with this program; if not, see
+## <http:##www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
 ## @deftypefn {Function File} hist3(@var{X})
