840ac2a5cc4630f245b0d897cd5c3a9c1a2faada
John Donoghue <john.donoghue@ieee.org>
2018-05-09 10:21:00 -0400

    update files moved from octave to the latest before they removed.
    * install-conditionally/base/center.m, install-conditionally/base/corr.m,
      install-conditionally/base/cov.m, install-conditionally/base/crosstab.m,
      install-conditionally/base/histc.m, install-conditionally/base/kendall.m,
      install-conditionally/base/kurtosis.m, install-conditionally/base/mean.m,
      install-conditionally/base/meansq.m, install-conditionally/base/median.m,
      install-conditionally/base/moment.m, install-conditionally/base/prctile.m,
      install-conditionally/base/quantile.m, install-conditionally/base/skewness.m,
      install-conditionally/base/spearman.m, install-conditionally/base/std.m,
      install-conditionally/base/var.m, install-conditionally/tests/z_test_2.m
      : updated where changed from last octave changeset before removed
    
    hg:2ecdff487f61bac83ddc957acf734afbe2d9cd62

--- install-conditionally/base/crosstab.m
+++ install-conditionally/base/crosstab.m
@@ -16,58 +16,48 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {} {[@var{t}, @var{l_x}] =} crosstab (@var{x})
-## @deftypefnx {} {[@var{t}, @var{l_x}, @var{l_y}] =} crosstab (@var{x}, @var{y})
-## Create a contingency table @var{t} from data vectors.
+## @deftypefn {} {@var{t} =} crosstab (@var{x}, @var{y})
+## Create a cross-tabulation (contingency table) @var{t} from data vectors.
 ##
-## The @var{l_x} and @var{l_y} vectors are the corresponding levels.
+## The inputs @var{x}, @var{y} must be vectors of equal length with a data
+## type of numeric, logical, or char.
 ##
 ## Currently, only 1- and 2-dimensional tables are supported.
 ## @end deftypefn
 
-## Author: KH <Kurt.Hornik@wu-wien.ac.at>
-## Description: Cross tabulation
+function t = crosstab (x, y)
 
-function [t, v, w] = crosstab (x, y)
-
-  if (nargin < 1 || nargin > 2)
+  if (nargin != 2)
     print_usage ();
   endif
 
-  if (nargin == 1)
-    if (! isnumeric (x) || ! isvector (x))
-      error ("crosstab: X must be a numeric vector");
-    endif
-    v = unique (x);
-    for i = 1 : length (v)
-      t(i) = sum (x == v(i) | isnan (v(i)) * isnan (x));
-    endfor
-  elseif (nargin == 2)
-    if (! (   isvector (x) && isnumeric (x)
-           && isvector (y) && isnumeric (y)
-           && (length (x) == length (y))))
-      error ("crosstab: X and Y must be numeric vectors of the same length");
-    endif
-    v = unique (x);
-    w = unique (y);
-    for i = 1 : length (v)
-      for j = 1 : length (w)
-        t(i,j) = sum ((x == v(i) | isnan (v(i)) * isnan (x)) &
-                      (y == w(j) | isnan (w(j)) * isnan (y)));
-      endfor
-    endfor
+  if (! (   isvector (x) && isreal (x)
+         && isvector (y) && isreal (y)
+         && (numel (x) == numel (y))))
+    error ("crosstab: X and Y must be real vectors of the same length");
   endif
+
+  x = x(:);
+  y = y(:);
+  v = unique (x);
+  w = unique (y);
+  for i = 1 : length (v)
+    for j = 1 : length (w)
+      t(i,j) = sum ((x == v(i) | isnan (v(i)) * isnan (x)) &
+                    (y == w(j) | isnan (w(j)) * isnan (y)));
+    endfor
+  endfor
 
 endfunction
 
 
 ## Test input validation
 %!error crosstab ()
+%!error crosstab (1)
 %!error crosstab (1, 2, 3)
-%!error crosstab (ones (2))
-%!error crosstab ([true true])
-%!error crosstab (ones (2,1), true (2,1))
-%!error crosstab (true (2,1), ones (2,1))
-%!error crosstab (ones (2,2), ones (2,1))
-%!error crosstab (ones (2,1), ones (2,2))
-%!error crosstab (ones (2,1), ones (3,1))
+%!error <X .* must be .* vector> crosstab (ones (2), [1 1])
+%!error <Y must be .* vector> crosstab ([1 1], ones (2))
+%!error <X .* must be real> crosstab ({true, true}, [1 1])
+%!error <Y must be real> crosstab ([1 1], {true, true})
+%!error <X and Y must be .* of the same length> crosstab ([1], [1 1])
+%!error <X and Y must be .* of the same length> crosstab ([1 1], [1])
