54158e363e198bdd6f01637f11c42723c740c3de
Andreas Bertsatos <abertsatos@biol.uoa.gr>
2022-08-16 19:15:49 +0300

    news features

--- inst/crosstab.m
+++ inst/crosstab.m
@@ -1,6 +1,9 @@
+## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ## Copyright (C) 2021 Stefano Guidoni
 ## Copyright (C) 2018 John Donoghue
 ## Copyright (C) 1995-2017 Kurt Hornik
+##
+## This file is part of the statistics package for GNU Octave.
 ##
 ## This program is free software: you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -24,54 +27,54 @@
 ##   {[@var{t}, @var{chi-2}, @var{p}, @var{labels}] =} crosstab (...)
 ## Create a cross-tabulation (contingency table) @var{t} from data vectors.
 ##
-## The inputs @var{x1}, @var{x2}, ... @var{xn} must be vectors of equal length 
+## The inputs @var{x1}, @var{x2}, ... @var{xn} must be vectors of equal length
 ## with a data type of numeric, logical, char, or string (cell array).
 ##
-## As additional return values @code{crosstab} returns the chi-square statistics 
+## As additional return values @code{crosstab} returns the chi-square statistics
 ## @var{chi-2}, its p-value @var{p} and a cell array @var{labels}, containing
 ## the labels of each input argument.
 ##
 ## Currently @var{chi-2} and @var{p} are available only for 1 or 2-dimensional
-## @var{t}, with @code{crosstab} returning a NaN value for both @var{chi-2} and 
+## @var{t}, with @code{crosstab} returning a NaN value for both @var{chi-2} and
 ## @var{p} for 3-dimensional, or more, @var{t}.
 ## @end deftypefn
 ##
 ## @seealso{grp2idx,tabulate}
 
 function [t, chi2, p, labels] = crosstab (varargin)
-  
+
   ## check input
   if (nargin < 2)
     print_usage ();
   endif
-  
-  v_length = length (varargin{1});
-  
-  for i = 1 : nargin
-    if ((! isvector (varargin{i})) || (v_length != length (varargin{i})))
-      error ("crosstab: x1, x2 ... xn must be vectors of the same length");
-    endif
-  endfor
-  
-  
+
+  v_length = size (varargin{1}, 1);
+
   ## main - begin
   v_reshape = [];                    # vector of the dimensions of t
   X = [];                            # matrix of the indexed input values
   labels = {};                       # cell array of labels
-  
-  for k = 1 : nargin
-    [g, gn] = grp2idx (varargin{k});
-    
-    X = [X, g];
-    for h = 1 : length (gn)
-      labels{h, k} = gn{h, 1};
+
+  for i = 1 : nargin
+    ## If char array, convert to numerical vector
+    try
+      [vector, gnames] = grp2idx (varargin{i});
+    catch
+      error ("crosstab: x1, x2 ... xn must be vectors.");
+    end_try_catch
+    if ((! isvector (vector)) || (v_length != length (vector)))
+      error ("crosstab: x1, x2 ... xn must be vectors of the same length.");
+    endif
+    X = [X, vector];
+    for h = 1 : length (gnames)
+      labels{h, i} = gnames{h, 1};
     endfor
-    v_reshape(k) = length (unique (varargin{k}));
+    v_reshape(i) = length (unique (vector));
   endfor
-  
+
   v = unique (X(:, nargin));
   t = [];
-  
+
   ## core logic, this employs a recursive function "crosstab_recursive"
   ## given (x1, x2, x3, ... xn) as inputs
   ## t(i,j,k,...) = sum (x1(:) == v1(i) & x2(:) == v2(j) & ...)
@@ -79,12 +82,12 @@ function [t, chi2, p, labels] = crosstab (varargin)
     t = [t, (crosstab_recursive (nargin - 1,...
       (X(:, nargin) == v(i) | isnan (v(i)) * isnan (X(:, nargin)))))];
   endfor
-  
+
   t = reshape(t, v_reshape);         # build the nargin-dimensional matrix
-  
+
   ## additional statistics
   if (length (v_reshape) == 2)
-    [p, chi2] = chisquare_test_independence(t);
+    [p, chi2] = chi2test (t);
   elseif (length (v_reshape) > 2)
     ## FIXME!
     ## chisquare_test_independence works with 2D matrices only
@@ -93,14 +96,14 @@ function [t, chi2, p, labels] = crosstab (varargin)
     chi2 = NaN;                      # placeholder
   endif
   ## main - end
-  
-  
+
+
   ## function: crosstab_recursive
   ## while there are input vectors, let's do iterations over them
   function t_partial = crosstab_recursive (x_idx, t_parent)
     y = X(:, x_idx);
     w = unique (y);
-    
+
     t_partial = [];
     if (x_idx == 1)
       ## we have reached the last vector,
@@ -125,7 +128,20 @@ endfunction
 ## Test input validation
 %!error crosstab ()
 %!error crosstab (1)
-%!error <x1, x2 .* must be vectors .*> crosstab (ones (2), [1 1])
-%!error <x1, x2 .* must be vectors .*> crosstab ([1 1], ones (2))
-%!error <x1, x2 .* must be vectors of the same length> crosstab ([1], [1 1])
-%!error <x1, x2 .* must be vectors of the same length> crosstab ([1 1], [1])
+%!error <crosstab: x1, x2 ... xn must be vectors.> crosstab (ones (2), [1 1])
+%!error <crosstab: x1, x2 ... xn must be vectors.> crosstab ([1 1], ones (2))
+%!error <x1, x2 .* must be vectors of the same length> crosstab ([1], [1 2])
+%!error <x1, x2 .* must be vectors of the same length> crosstab ([1 2], [1])
+%!test
+%! load carbig
+%! warning ("off")
+%! [table, chi2, p, labels] = crosstab (cyl4, when, org);
+%! assert (table(2,3,1), 38);
+%! assert (labels{3,3}, "Japan");
+%!test
+%! load carbig
+%! warning ("off")
+%! [table, chi2, p, labels] = crosstab (cyl4, when, org);
+%! assert (table(2,3,2), 17);
+%! assert (labels{1,3}, "USA");
+
--- inst/grp2idx.m
+++ inst/grp2idx.m
@@ -1,4 +1,7 @@
+## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ## Copyright (C) 2015 Carnë Draug <carandraug@octave.org>
+##
+## This file is part of the statistics package for GNU Octave.
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -15,12 +18,13 @@
 ## <http:##www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{g}, @var{gn}, @var{gl}] =} grp2idx (@var{s})
+## @deftypefn {Function File} [@var{g}, @var{gn}, @var{gl}] = grp2idx (@var{s})
 ## Get index for group variables.
 ##
 ## For variable @var{s}, returns the indices @var{g}, into the variable
 ## groups @var{gn} and @var{gl}.  The first has a string representation of
-## the groups while the later has its actual values.
+## the groups while the later has its actual values. The group indices are
+## allocated in order of appearance in @var{s}.
 ##
 ## NaNs and empty strings in @var{s} appear as NaN in @var{g} and are
 ## not present on either @var{gn} and @var{gl}.
@@ -41,28 +45,44 @@ function [g, gn, gl] = grp2idx (s)
     error ("grp2idx: S must be a vector, cell array of strings, or char matrix");
   endif
 
-  ## FIXME once Octave core implements "sorted" and "stable" argument to
-  ##        unique(), we can use the following snippet so that we are fully
-  ##        Matlab compatible.
-#  set_order = "sorted";
-#  if (iscellstr (s))
-#    set_order = "stable";
-#  endif
-#  [gl, ~, g] = unique (s(:), set_order);
-
-  [gl, ~, g] = unique (s(:));
+  [gl, I, g] = unique (s(:));
+  ## Fix order in here, since unique does not support this yet
+  if (iscellstr (s))
+    I = sort(I);
+    for i = 1:length (gl)
+      gl_s(i) = gl(g(I(i)));
+      idx(i,:) = (g == g(I(i)));
+    endfor
+    for i = 1:length (gl)
+      g(idx(i,:)) = i;
+    endfor
+    gl = gl_s;
+    gl = gl';
+  else
+    I = sort(I);
+    for i = 1:length (gl)
+      gl_s(i) = gl(g(I(i)));
+      idx(i,:) = (g == g(I(i)));
+    endfor
+    for i = 1:length (gl)
+      g(idx(i,:)) = i;
+    endfor
+    gl = gl_s;
+    gl = gl';
+  endif
 
   ## handle NaNs and empty strings
   if (iscellstr (s))
-    ## FIXME empty strings appear at the front because unique is sorting
-    ##        them, so we only need to subtract one.  However, when fix the
-    ##        order for strings (when core's unique has the stable option),
-    ##        then we'll have to come up with something clever.
     empties = cellfun (@isempty, s);
     if (any (empties))
       g(empties) = NaN;
-      g--;
-      gl(1) = [];
+      while (min (g) > 1)
+        g--;
+      endwhile
+    endif
+    empties = cellfun (@isempty, gl);
+    if (any (empties))
+      gl(empties) = [];
     endif
   else
     ## This works fine because NaN come at the end after sorting, we don't
@@ -90,16 +110,16 @@ endfunction
 ## test boolean input and note that row or column vector makes no difference
 %!test
 %! in = [true false false true];
-%! out = {[2; 1; 1; 2] {"0"; "1"} [false; true]};
+%! out = {[1; 2; 2; 1] {"1"; "0"} [true; false]};
 %! assert (nthargout (1:3, @grp2idx, in), out)
 %! assert (nthargout (1:3, @grp2idx, in), nthargout (1:3, @grp2idx, in'))
 
-## test that groups are ordered in boolean
+## test that boolean groups are ordered in order of appearance
 %!test
-%! assert (nthargout (1:3, @grp2idx, [false true]),
+%! assert (nthargout (1:3, @grp2idx, [false, true]),
 %!         {[1; 2] {"0"; "1"} [false; true]});
-%! assert (nthargout (1:3, @grp2idx, [true false]),
-%!         {[2; 1] {"0"; "1"} [false; true]});
+%! assert (nthargout (1:3, @grp2idx, [true, false]),
+%!         {[1; 2] {"1"; "0"} [true; false]});
 
 ## test char matrix and cell array of strings
 %!assert (nthargout (1:3, @grp2idx, ["oct"; "sci"; "oct"; "oct"; "sci"]),
@@ -110,8 +130,8 @@ endfunction
 
 ## test numeric arrays
 %!assert (nthargout (1:3, @grp2idx, [ 1 -3 -2 -3 -3  2  1 -1  3 -3]),
-%!        {[4; 1; 2; 1; 1; 5; 4; 3; 6; 1] {"-3"; "-2"; "-1"; "1"; "2"; "3"} ...
-%!         [-3; -2; -1; 1; 2; 3]});
+%!        {[1; 2; 3; 2; 2; 4; 1; 5; 6; 2], {"1"; "-3"; "-2"; "2"; "-1"; "3"}, ...
+%!         [1; -3; -2; 2; -1; 3]});
 
 ## test for NaN and empty strings
 %!assert (nthargout (1:3, @grp2idx, [2 2 3 NaN 2 3]),
@@ -119,13 +139,10 @@ endfunction
 %!assert (nthargout (1:3, @grp2idx, {"et" "sa" "sa" "" "et"}),
 %!        {[1; 2; 2; NaN; 1] {"et"; "sa"} {"et"; "sa"}})
 
-## FIXME this fails because unique() in core does not yet have set_order
-##        option implemented.  See code for code to uncomment once it is
-##        implemented in core.
 ## Test that order when handling strings is by order of appearance
-%!xtest <51928> assert (nthargout (1:3, @grp2idx, ["sci"; "oct"; "sci"; "oct"; "oct"]),
+%!test assert (nthargout (1:3, @grp2idx, ["sci"; "oct"; "sci"; "oct"; "oct"]),
 %!        {[1; 2; 1; 2; 2] {"sci"; "oct"} ["sci"; "oct"]});
-%!xtest <51928> assert (nthargout (1:3, @grp2idx, {"sci"; "oct"; "sci"; "oct"; "oct"}),
+%!test assert (nthargout (1:3, @grp2idx, {"sci"; "oct"; "sci"; "oct"; "oct"}),
 %!        {[1; 2; 1; 2; 2] {"sci"; "oct"} {"sci"; "oct"}});
-%!xtest <51928> assert (nthargout (1:3, @grp2idx, {"sa" "et" "et" "" "sa"}),
+%!test assert (nthargout (1:3, @grp2idx, {"sa" "et" "et" "" "sa"}),
 %!        {[1; 2; 2; NaN; 1] {"sa"; "et"} {"sa"; "et"}})
