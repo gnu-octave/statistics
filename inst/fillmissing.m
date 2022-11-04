a9d21a30ba5315b12be0072e16bb91d530f8dc6b
pr0m1th3as <abertsatos@biol.uoa.gr>
2022-11-04 17:12:21 +0200

    fix copyright notice

--- inst/fillmissing.m
+++ inst/fillmissing.m
@@ -1,36 +1,28 @@
-########################################################################
-##
 ## Copyright (C) 1995-2021 The Octave Project Developers
 ##
-## See the file COPYRIGHT.md in the top-level directory of this
-## distribution or <https://octave.org/copyright/>.
+## This file is part of the statistics package for GNU Octave.
 ##
-## This file is part of Octave.
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
 ##
-## Octave is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
 ##
-## Octave is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
-## <https://www.gnu.org/licenses/>.
-##
-########################################################################
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {} {@var{B} =} fillmissing (@var{A}, 'constant', @var{v})
-## @deftypefnx {} {@var{B} =} fillmissing (@var{A}, @var{method})
-## @deftypefnx {} {@var{B} =} fillmissing (@var{A}, @var{move_method}, @var{window_size})
-## @deftypefnx {} {@var{B} =} fillmissing (@var{A}, @var{fill_function}, @var{window_size})
-## @deftypefnx {} {@var{B} =} fillmissing (@dots{}, @var{dim})
-## @deftypefnx {} {@var{B} =} fillmissing (@dots{}, @var{PropertyName}, @var{PropertyValue})
-## @deftypefnx {} {[@var{B}, @var{idx}] =} fillmissing (@dots{})
+## @deftypefn {} @var{B} = fillmissing (@var{A}, 'constant', @var{v})
+## @deftypefnx {} @var{B} = fillmissing (@var{A}, @var{method})
+## @deftypefnx {} @var{B} = fillmissing (@var{A}, @var{move_method}, @var{window_size})
+## @deftypefnx {} @var{B} = fillmissing (@var{A}, @var{fill_function}, @var{window_size})
+## @deftypefnx {} @var{B} = fillmissing (@dots{}, @var{dim})
+## @deftypefnx {} @var{B} = fillmissing (@dots{}, @var{PropertyName}, @var{PropertyValue})
+## @deftypefnx {} [@var{B}, @var{idx}] = fillmissing (@dots{})
 ##
 ## Replace missing entries of array @var{A} either with values in @var{v} or
 ## as determined by other specified methods.  'missing' values are determined
