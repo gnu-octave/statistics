83a87ecef9aeb1ad79d240029922357587e8abed
pr0m1th3as <abertsatos@biol.uoa.gr>
2022-11-18 13:20:24 +0200

    hist3: fix texinfo and copyright header

--- inst/hist3.m
+++ inst/hist3.m
@@ -1,5 +1,7 @@
 ## Copyright (C) 2015 Carnë Draug <carandraug@octave.org>
 ##
+## This file is part of the statistics package for GNU Octave.
+##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
 ## published by the Free Software Foundation; either version 3 of the
