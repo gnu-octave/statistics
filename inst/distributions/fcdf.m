a3aaed2ae11454c538ef9b0a4a94ede3b8553da1
pr0m1th3as <abertsatos@biol.uoa.gr>
2022-11-19 05:33:57 +0200

    fcdf: fix copyright

--- inst/distributions/fcdf.m
+++ inst/distributions/fcdf.m
@@ -1,5 +1,5 @@
-## Copyright (C) 1995-2016 Kurt Hornik
 ## Copyright (C) 2012 Rik Wehbring
+## Copyright (C) 1995-2016 Kurt Hornik
 ## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
