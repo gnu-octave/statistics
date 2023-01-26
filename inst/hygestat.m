97b9ad610bf2ba59523fb6e1d410a3d4de7e468c
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-01-26 23:41:33 +0200

    hygestat: fixed documentation

--- inst/hygestat.m
+++ inst/hygestat.m
@@ -16,7 +16,7 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} {[@var{mn}, @var{v}] =} hygestat (@var{t}, @var{m}, @var{n})
+## @deftypefn  {Function File} {[@var{mn}, @var{v}] =} hygestat (@var{t}, @var{m}, @var{n})
 ##
 ## Compute mean and variance of the hypergeometric distribution.
 ##
