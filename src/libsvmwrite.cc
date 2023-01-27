37dc0d808281a39f27dc92cbd71d40315dc45c07
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-01-27 11:40:38 +0200

    libsvmwrite: fixed tempfile in BISTs

--- src/libsvmwrite.cc
+++ src/libsvmwrite.cc
@@ -136,14 +136,14 @@ specified by @var{filename}.  @var{data} must be a sparse matrix.  Both \
 %! [L, D] = libsvmread (file_in_loadpath ("heart_scale.dat"));
 %!error <libsvmwrite: error opening file for write.> libsvmwrite ("", L, D);
 %!error <libsvmwrite: length of label vector does not match instances.> ...
-%! libsvmwrite (fullfile (tempdir (), "filename"), [L;L], D);
+%! libsvmwrite (tempname (), [L;L], D);
 %!error <libsvmwrite: wrong number of output arguments.> ...
-%! OUT = libsvmwrite ("filename", L, D);
+%! OUT = libsvmwrite (tempname (), L, D);
 %!error <libsvmwrite: label vector and instance matrix must be double.> ...
-%! libsvmwrite ("filename", single (L), D);
+%! libsvmwrite (tempname (), single (L), D);
 %!error <libsvmwrite: filename must be a string.> libsvmwrite (13412, L, D);
 %!error <libsvmwrite: instance_matrix must be sparse.> ...
-%! libsvmwrite ("filename", L, full (D));
+%! libsvmwrite (tempname (), L, full (D));
 %!error <libsvmwrite: wrong number of input arguments.> ...
-%! libsvmwrite ("filename", L, D, D);
+%! libsvmwrite (tempname (), L, D, D);
 */
