ef903a99be43b8daa7006bd0d939ca31c68284ef
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-01-28 18:09:25 +0200

    laplace_cdf: updated functionality, updated docstrings and made input/output argument names consistent across distribution functions

--- inst/distributions/laplace_cdf.m
+++ inst/distributions/laplace_cdf.m
@@ -1,5 +1,8 @@
 ## Copyright (C) 2012 Rik Wehbring
 ## Copyright (C) 1995-2016 Kurt Hornik
+## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
 ##
 ## This program is free software: you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,27 +19,57 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {} {} laplace_cdf (@var{x})
-## @deftypefnx {} {} laplace_pdf (@var{x}, @var{mu}, @var{beta})
+## @deftypefn  {statistics} @var{p} = laplace_cdf (@var{x})
+## @deftypefnx {statistics} @var{p} = laplace_cdf (@var{x}, @var{mu})
+## @deftypefnx {statistics} @var{p} = laplace_pdf (@var{x}, @var{mu}, @var{beta})
+##
+## Laplace cumulative distribution function (CDF).
+##
 ## For each element of @var{x}, compute the cumulative distribution function
 ## (CDF) at @var{x} of the Laplace distribution with a location parameter
-## @var{mu} and a scale parameter (i.e. "diversity") @var{beta}.
+## @var{mu} and a scale parameter (i.e. "diversity") @var{beta}.  The size of
+## @var{p} is the common size of @var{x}, @var{mu}, and @var{beta}.  A scalar
+## input functions as a constant matrix of the same size as the other inputs.
+##
+## Default values are @var{nu} = 0, @var{beta} = 1.
+##
+## @seealso{laplace_inv, laplace_pdf, laplace_rnd}
 ## @end deftypefn
 
-## Author: KH <Kurt.Hornik@wu-wien.ac.at>
-## Description: CDF of the Laplace distribution
+function p = laplace_cdf (x, mu = 0, beta = 1)
 
-function cdf = laplace_cdf (x, mu = 0, beta = 1)
-
-  if (nargin != 1)
+  ## Check for valid number of input arguments
+  if (nargin < 1 || nargin > 3)
     print_usage ();
   endif
 
-  if (iscomplex (x))
-    error ("laplace_cdf: X must not be complex");
+  ## Check for common size of X, MU, and SIGMA
+  if (! isscalar (x) || ! isscalar (mu) || ! isscalar(beta))
+    [retval, x, mu, beta] = ...
+        common_size (x, mu, beta);
+    if (retval > 0)
+      error (strcat (["laplace_cdf: X, MU, and BETA must be of"], ...
+                     [" common size or scalars."]));
+    endif
   endif
 
-  cdf = (1 + sign (x - mu) .* (1 - exp (- abs (x - mu) / beta))) / 2;
+  ## Check for X, MU, and SIGMA being reals
+  if (iscomplex (x) || iscomplex (mu) || iscomplex (beta))
+    error ("laplace_cdf: X, MU, and BETA must not be complex.");
+  endif
+
+  ## Check for appropriate class
+  if (isa (x, "single") || isa (mu, "single") || isa (beta, "single"));
+    is_class = "single";
+  else
+    is_class = "double";
+  endif
+
+  ## Compute Laplace CDF
+  p = (1 + sign (x - mu) .* (1 - exp (- abs (x - mu) ./ beta))) ./ 2;
+
+  ## Cast to appropriate class
+  p = cast (p, is_class);
 
 endfunction
 
@@ -52,4 +85,12 @@ endfunction
 ## Test input validation
 %!error laplace_cdf ()
 %!error laplace_cdf (1,2,3,4)
-%!error laplace_cdf (i)
+%!error<laplace_cdf: X, MU, and BETA must be of common size or scalars.> ...
+%! laplace_cdf (1, ones (2), ones (3))
+%!error<laplace_cdf: X, MU, and BETA must be of common size or scalars.> ...
+%! laplace_cdf (ones (2), 1, ones (3))
+%!error<laplace_cdf: X, MU, and BETA must be of common size or scalars.> ...
+%! laplace_cdf (ones (2), ones (3), 1)
+%!error laplace_cdf (i, 2, 3)
+%!error laplace_cdf (1, i, 3)
+%!error laplace_cdf (1, 2, i)
