cb69d0ba6979262a85c3cf2d48cc06fc9b806227
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-01-31 00:47:03 +0200

    uniform disrete distribution functions: updated docstrings and made input/output argument names consistent across distribution functions

--- inst/distributions/unifinv.m
+++ inst/distributions/unifinv.m
@@ -1,5 +1,8 @@
 ## Copyright (C) 2012 Rik Wehbring
 ## Copyright (C) 1995-2016 Kurt Hornik
+## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
 ##
 ## This program is free software: you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,68 +19,71 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {} {} unifinv (@var{x})
-## @deftypefnx {} {} unifinv (@var{x}, @var{a}, @var{b})
-## For each element of @var{x}, compute the quantile (the inverse of the CDF)
-## at @var{x} of the uniform distribution on the interval [@var{a}, @var{b}].
+## @deftypefn  {statistics} @var{x} = unifinv (@var{p})
+## @deftypefnx {statistics} @var{x} = unifcdf (@var{p}, @var{a})
+## @deftypefnx {statistics} @var{x} = unifcdf (@var{p}, @var{a}, @var{b})
+##
+## Inverse of the uniform cumulative distribution function (iCDF).
+##
+## For each element of @var{p}, compute the quantile (the inverse of the CDF)
+## at @var{p} of the uniform distribution on the interval [@var{a}, @var{b}].
+## The size of @var{x} is the common size of the input arguments.  A scalar
+## input functions as a constant matrix of the same size as the other inputs.
 ##
 ## Default values are @var{a} = 0, @var{b} = 1.
+##
+## @seealso{unifcdf, unifpdf, unifrnd, unifstat}
 ## @end deftypefn
 
-## Author: KH <Kurt.Hornik@wu-wien.ac.at>
-## Description: Quantile function of the uniform distribution
+function x = unifinv (p, a = 0, b = 1)
 
-function inv = unifinv (x, a = 0, b = 1)
-
-  if (nargin != 1 && nargin != 3)
+  if (nargin < 1 && nargin > 3)
     print_usage ();
   endif
 
-  if (! isscalar (a) || ! isscalar (b))
-    [retval, x, a, b] = common_size (x, a, b);
+  if (! isscalar (p) || ! isscalar (a) || ! isscalar (b))
+    [retval, p, a, b] = common_size (p, a, b);
     if (retval > 0)
-      error ("unifinv: X, A, and B must be of common size or scalars");
+      error ("unifinv: P, A, and B must be of common size or scalars.");
     endif
   endif
 
-  if (iscomplex (x) || iscomplex (a) || iscomplex (b))
-    error ("unifinv: X, A, and B must not be complex");
+  if (iscomplex (p) || iscomplex (a) || iscomplex (b))
+    error ("unifinv: P, A, and B must not be complex.");
   endif
 
-  if (isa (x, "single") || isa (a, "single") || isa (b, "single"))
-    inv = NaN (size (x), "single");
+  if (isa (p, "single") || isa (a, "single") || isa (b, "single"))
+    x = NaN (size (p), "single");
   else
-    inv = NaN (size (x));
+    x = NaN (size (p));
   endif
 
-  k = (x >= 0) & (x <= 1) & (a < b);
-  if (isscalar (a) && isscalar (b))
-    inv(k) = a + x(k) * (b - a);
-  else
-    inv(k) = a(k) + x(k) .* (b(k) - a(k));
-  endif
+  k = (p >= 0) & (p <= 1) & (a < b);
+  x(k) = a(k) + p(k) .* (b(k) - a(k));
 
 endfunction
 
 
-%!shared x
-%! x = [-1 0 0.5 1 2];
-%!assert (unifinv (x, ones (1,5), 2*ones (1,5)), [NaN 1 1.5 2 NaN])
-%!assert (unifinv (x, 1, 2*ones (1,5)), [NaN 1 1.5 2 NaN])
-%!assert (unifinv (x, ones (1,5), 2), [NaN 1 1.5 2 NaN])
-%!assert (unifinv (x, [1 2 NaN 1 1], 2), [NaN NaN NaN 2 NaN])
-%!assert (unifinv (x, 1, 2*[1 0 NaN 1 1]), [NaN NaN NaN 2 NaN])
-%!assert (unifinv ([x(1:2) NaN x(4:5)], 1, 2), [NaN 1 NaN 2 NaN])
+%!shared p
+%! p = [-1 0 0.5 1 2];
+%!assert (unifinv (p, ones (1,5), 2*ones (1,5)), [NaN 1 1.5 2 NaN])
+%!assert (unifinv (p), [NaN 1 1.5 2 NaN] - 1)
+%!assert (unifinv (p, 0), [NaN 1 1.5 2 NaN] - 1)
+%!assert (unifinv (p, 0, 1), [NaN 1 1.5 2 NaN] - 1)
+%!assert (unifinv (p, 1, 2*ones (1,5)), [NaN 1 1.5 2 NaN])
+%!assert (unifinv (p, ones (1,5), 2), [NaN 1 1.5 2 NaN])
+%!assert (unifinv (p, [1 2 NaN 1 1], 2), [NaN NaN NaN 2 NaN])
+%!assert (unifinv (p, 1, 2*[1 0 NaN 1 1]), [NaN NaN NaN 2 NaN])
+%!assert (unifinv ([p(1:2) NaN p(4:5)], 1, 2), [NaN 1 NaN 2 NaN])
 
 ## Test class of input preserved
-%!assert (unifinv ([x, NaN], 1, 2), [NaN 1 1.5 2 NaN NaN])
-%!assert (unifinv (single ([x, NaN]), 1, 2), single ([NaN 1 1.5 2 NaN NaN]))
-%!assert (unifinv ([x, NaN], single (1), 2), single ([NaN 1 1.5 2 NaN NaN]))
-%!assert (unifinv ([x, NaN], 1, single (2)), single ([NaN 1 1.5 2 NaN NaN]))
+%!assert (unifinv ([p, NaN], 1, 2), [NaN 1 1.5 2 NaN NaN])
+%!assert (unifinv (single ([p, NaN]), 1, 2), single ([NaN 1 1.5 2 NaN NaN]))
+%!assert (unifinv ([p, NaN], single (1), 2), single ([NaN 1 1.5 2 NaN NaN]))
+%!assert (unifinv ([p, NaN], 1, single (2)), single ([NaN 1 1.5 2 NaN NaN]))
 
 ## Test input validation
 %!error unifinv ()
-%!error unifinv (1,2)
 %!error unifinv (1,2,3,4)
 %!error unifinv (ones (3), ones (2), ones (2))
 %!error unifinv (ones (2), ones (3), ones (2))
--- inst/distributions/unifpdf.m
+++ inst/distributions/unifpdf.m
@@ -1,5 +1,8 @@
 ## Copyright (C) 2012 Rik Wehbring
 ## Copyright (C) 1995-2016 Kurt Hornik
+## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
 ##
 ## This program is free software: you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,49 +19,50 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {} {} unifpdf (@var{x})
-## @deftypefnx {} {} unifpdf (@var{x}, @var{a}, @var{b})
+## @deftypefn  {statistics} @var{y} = unifpdf (@var{x})
+## @deftypefnx {statistics} @var{y} = unifpdf (@var{x}, @var{a})
+## @deftypefnx {statistics} @var{y} = unifpdf (@var{x}, @var{a}, @var{b})
+##
+## Uniform probability density function (PDF).
+##
 ## For each element of @var{x}, compute the probability density function (PDF)
 ## at @var{x} of the uniform distribution on the interval [@var{a}, @var{b}].
+## The size of @var{y} is the common size of the input arguments.  A scalar
+## input functions as a constant matrix of the same size as the other inputs.
 ##
 ## Default values are @var{a} = 0, @var{b} = 1.
+##
+## @seealso{unifcdf, unifinv, unifrnd, unifstat}
 ## @end deftypefn
 
-## Author: KH <Kurt.Hornik@wu-wien.ac.at>
-## Description: PDF of the uniform distribution
+function y = unifpdf (x, a = 0, b = 1)
 
-function pdf = unifpdf (x, a = 0, b = 1)
-
-  if (nargin != 1 && nargin != 3)
+  if (nargin < 1 && nargin > 3)
     print_usage ();
   endif
 
-  if (! isscalar (a) || ! isscalar (b))
+  if (! isscalar (x) || ! isscalar (a) || ! isscalar (b))
     [retval, x, a, b] = common_size (x, a, b);
     if (retval > 0)
-      error ("unifpdf: X, A, and B must be of common size or scalars");
+      error ("unifpdf: X, A, and B must be of common size or scalars.");
     endif
   endif
 
   if (iscomplex (x) || iscomplex (a) || iscomplex (b))
-    error ("unifpdf: X, A, and B must not be complex");
+    error ("unifpdf: X, A, and B must not be complex.");
   endif
 
   if (isa (x, "single") || isa (a, "single") || isa (b, "single"))
-    pdf = zeros (size (x), "single");
+    y = zeros (size (x), "single");
   else
-    pdf = zeros (size (x));
+    y = zeros (size (x));
   endif
 
   k = isnan (x) | !(a < b);
-  pdf(k) = NaN;
+  y(k) = NaN;
 
   k = (x >= a) & (x <= b) & (a < b);
-  if (isscalar (a) && isscalar (b))
-    pdf(k) = 1 / (b - a);
-  else
-    pdf(k) = 1 ./ (b(k) - a(k));
-  endif
+  y(k) = 1 ./ (b(k) - a(k));
 
 endfunction
 
@@ -72,6 +76,9 @@ endfunction
 %!assert (unifpdf (x, [2 NaN 1 1 1], 2), [NaN NaN y(3:5)])
 %!assert (unifpdf (x, 1, 2*[0 NaN 1 1 1]), [NaN NaN y(3:5)])
 %!assert (unifpdf ([x, NaN], 1, 2), [y, NaN])
+%!assert (unifpdf (x), [1 1 0 0 0])
+%!assert (unifpdf (x, 0), [1 1 0 0 0])
+%!assert (unifpdf (x, 0, 1), [1 1 0 0 0])
 
 ## Test class of input preserved
 %!assert (unifpdf (single ([x, NaN]), 1, 2), single ([y, NaN]))
@@ -80,7 +87,6 @@ endfunction
 
 ## Test input validation
 %!error unifpdf ()
-%!error unifpdf (1,2)
 %!error unifpdf (1,2,3,4)
 %!error unifpdf (ones (3), ones (2), ones (2))
 %!error unifpdf (ones (2), ones (3), ones (2))
