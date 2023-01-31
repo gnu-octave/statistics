1ba88640ef0f052e8341f7a9d8f6617bce0a09fe
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-01-31 16:41:33 +0200

    fixed bugs in von Mises distribution functions

--- inst/distributions/vmcdf.m
+++ inst/distributions/vmcdf.m
@@ -23,13 +23,14 @@
 ## Von Mises probability density function (PDF).
 ##
 ## For each element of @var{theta}, compute the probability density function
-## (PDF) at @var{theta} of the von Mises distribution with parameters @var{mu}
-## and @var{k} on the interval [-pi, pi}].  The size of @var{x} is the common
-## size of the input arguments.  A scalar input functions as a constant matrix
-## of the same same size as the other inputs.  @var{k} must be a real positive
-## scalar.
+## (PDF) at @var{theta} of the von Mises distribution with mean direction
+## parameter @var{mu} and concentration parameter @var{k} on the interval
+## [-pi, pi].  The size of @var{x} is the common size of the input arguments.
+##  A scalar input functions as a constant matrix of the same same size as the
+## other inputs.
 ##
-## Default values are @var{mu} = 0, @var{k} = 1.
+## Default values are @var{mu} = 0, @var{k} = 1.  The function returns NaN for
+## negative @var{k}.
 ##
 ## @seealso{vmpdf, vmrnd}
 ## @end deftypefn
@@ -52,18 +53,42 @@ function p = vmcdf (theta, mu = 0, k = 1)
   endif
 
   if (isa (theta, "single") || isa (mu, "single") || isa (k, "single"))
-    y = zeros (size (theta), "single");
+    p = zeros (size (theta), "single");
   else
-    y = zeros (size (theta));
+    p = zeros (size (theta));
   endif
 
   ## Evaluate Von Mises CDF by integrating from -PI to PI
-  interval = linspace (-pi, pi, 1e6);
+  interval = linspace (-pi, pi, 1e6)';
   f = exp (k .* cos (interval - mu)) ./ (2 .* pi .* besseli (0, k));
   c = cumtrapz (interval, f);
-  p = interp1 (interval, c, theta, "spline");
+  p = diag (interp1 (interval, c, theta, "spline"))';
 
   ## Force Nan for negative K
-  p(k <= 0) = NaN;
+  p(k < 0) = NaN;
 
 endfunction
+
+%!shared theta, p0, p1
+%! theta = [-pi:pi/2:pi];
+%! p0 = [0, 0.10975, 0.5, 0.89025, 1];
+%! p1 = [0, 0.03752, 0.5, 0.99622, 1];
+%!assert (vmcdf (theta), p0, 1e-5)
+%!assert (vmcdf (theta, zeros (1,5), ones (1,5)), p0, 1e-5)
+%!assert (vmcdf (theta, 0, [1 2 3 4 5]), p1, 1e-5)
+
+## Test class of input preserved
+%!assert (isa (vmcdf (single (pi), 0, 1), "single"), true)
+%!assert (isa (vmcdf (pi, single (0), 1), "single"), true)
+%!assert (isa (vmcdf (pi, 0, single (1)), "single"), true)
+
+## Test input validation
+%!error vmcdf ()
+%!error vmcdf (1, 2, 3, 4)
+%!error vmcdf (ones (3), ones (2), ones (2))
+%!error vmcdf (ones (2), ones (3), ones (2))
+%!error vmcdf (ones (2), ones (2), ones (3))
+%!error vmcdf (i, 2, 2)
+%!error vmcdf (2, i, 2)
+%!error vmcdf (2, 2, i)
+
--- inst/distributions/vmrnd.m
+++ inst/distributions/vmrnd.m
@@ -17,65 +17,132 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} @var{theta} = vmrnd (@var{mu}, @var{k})
-## @deftypefnx {statistics} @var{theta} = vmrnd (@var{mu}, @var{k}, @var{sz})
+## @deftypefn  {statistics} @var{r} = vmrnd (@var{mu}, @var{k})
+## @deftypefnx {statistics} @var{r} = vmrnd (@var{mu}, @var{k}, @var{rows})
+## @deftypefnx {statistics} @var{r} = vmrnd (@var{mu}, @var{k}, @var{rows}, @var{cols}, @dots{})
+## @deftypefnx {statistics} @var{r} = vmrnd (@var{mu}, @var{k}, [@var{sz}])
 ##
-## Draw random angles from a Von Mises distribution with mean @var{mu} and
-## concentration @var{k}.
+## Random arrays from the von Mises distribution.
 ##
-## The Von Mises distribution has probability density function
-## @example
-## f (@var{x}) = exp (@var{k} * cos (@var{x} - @var{mu})) / @var{Z} ,
-## @end example
-## where @var{Z} is a normalisation constant.
+## @code{@var{r} = vmrnd (@var{mu}, @var{k})} returns an array of random angles
+## chosen from a von Mises distribution with mean direction parameter @var{mu}
+## and concentration parameter @var{k} on the interval [-pi, pi].  The size of
+## @var{r} is the common size of the input arguments. A scalar input functions
+## as a constant matrix of the same size as the other inputs.
 ##
-## The output, @var{theta}, is a matrix of size @var{sz} containing random angles
-## drawn from the given Von Mises distribution. By default, @var{mu} is 0
-## and @var{k} is 1.
+## When called with a single size argument, return a square matrix with the
+## dimension specified.  When called with more than one scalar argument the
+## first two arguments are taken as the number of rows and columns and any
+## further arguments specify additional matrix dimensions.  The size may also
+## be specified with a vector of dimensions @var{sz}.
 ##
 ## @seealso{vmcdf, vmpdf}
 ## @end deftypefn
 
-function theta = vmrnd (mu = 0, k = 1, sz = 1)
-  ## Check input
-  if (!isreal (mu))
-    error ("vmrnd: first input must be a scalar.");
+function r = vmrnd (mu, k, varargin)
+
+  if (nargin < 2)
+    print_usage ();
   endif
 
-  if (!isreal (k) || k <= 0)
-    error ("vmrnd: second input must be a real positive scalar.");
+  if (! isscalar (mu) || ! isscalar (k))
+    [retval, mu, k] = common_size (mu, k);
+    if (retval > 0)
+      error ("vmrnd: MU and K must be of common size or scalars.");
+    endif
   endif
 
-  if (isscalar (sz))
-    sz = [sz, sz];
-  elseif (!isvector (sz))
-    error ("vmrnd: third input must be a scalar or a vector.");
+  if (iscomplex (mu) || iscomplex (k))
+    error ("unifrnd: MU and K must not be complex.");
+  endif
+
+  if (nargin == 2)
+    sz = size (mu);
+  elseif (nargin == 3)
+    if (isscalar (varargin{1}) && varargin{1} >= 0)
+      sz = [varargin{1}, varargin{1}];
+    elseif (isrow (varargin{1}) && all (varargin{1} >= 0))
+      sz = varargin{1};
+    else
+      error (strcat (["vmrnd: dimension vector must be row vector"], ...
+                     [" of non-negative integers."]));
+    endif
+  elseif (nargin > 3)
+    if (any (cellfun (@(x) (! isscalar (x) || x < 0), varargin)))
+      error ("vmrnd: dimensions must be non-negative integers.");
+    endif
+    sz = [varargin{:}];
+  endif
+
+  if (! isscalar (mu) && ! isequal (size (k), sz))
+    error ("vmrnd: MU and K must be scalar or of size SZ.");
   endif
 
   ## Simulate!
   if (k < 1e-6)
     ## k is small: sample uniformly on circle
-    theta = 2 * pi * rand (sz) - pi;
+    r = mu + (2 * pi * rand (sz) - pi);
 
   else
-    a = 1 + sqrt (1 + 4 * k.^2);
-    b = (a - sqrt (2 * a)) / (2 * k);
-    r = (1 + b^2) / (2 * b);
+    a = 1 + sqrt (1 + 4 .* k .^ 2);
+    b = (a - sqrt (2 .* a)) ./ (2 .* k);
+    r_tmp = (1 + b .^ 2) ./ (2 .* b);
 
     N = prod (sz);
+    if (isscalar (k))
+      r_tmp = repmat (r_tmp, 1, N);
+      k_tmp = repmat (k, 1, N);
+      mu_rs = repmat (mu, 1, N);
+    else
+      r_tmp = reshape (r_tmp, 1, N);
+      k_tmp = reshape (k, 1, N);
+      mu_rs = reshape (mu, 1, N);
+    endif
     notdone = true (N, 1);
     while (any (notdone))
-      u (:, notdone) = rand (3, N);
+      u (:, notdone) = (rand (3, N))(:,notdone);
 
-      z (notdone) = cos (pi * u (1, notdone));
-      f (notdone) = (1 + r * z (notdone)) ./ (r + z (notdone));
-      c (notdone) = k * (r - f (notdone));
+      z (notdone) = (cos (pi .* u (1, :)))(notdone);
+      f (notdone) = ((1 + r_tmp(notdone) .* z(notdone)) ./ (r_tmp(notdone) + z(notdone)));
+      c (notdone) = (k_tmp(notdone) .* (r_tmp(notdone) - f(notdone)));
 
       notdone = (u (2, :) >= c .* (2 - c)) & (log (c) - log (u (2, :)) + 1 - c < 0);
-      N = sum (notdone);
+      #N = sum (notdone);
     endwhile
 
-    theta = mu + sign (u (3, :) - 0.5) .* acos (f);
-    theta = reshape (theta, sz);
+    r = mu_rs + sign (u (3, :) - 0.5) .* acos (f);
+    r = reshape (r, sz);
   endif
 endfunction
+
+
+%!assert (size (vmrnd (1, 2)), [1, 1])
+%!assert (size (vmrnd (1 * ones (2, 1), 2)), [2, 1])
+%!assert (size (vmrnd (1 * ones (2, 2), 2)), [2, 2])
+%!assert (size (vmrnd (1, 2 * ones (2, 1))), [2, 1])
+%!assert (size (vmrnd (1, 2 * ones (2, 2))), [2, 2])
+%!assert (size (vmrnd (1, 2, 3)), [3, 3])
+%!assert (size (vmrnd (1, 2, [4, 1])), [4, 1])
+%!assert (size (vmrnd (1, 2, 4, 1)), [4, 1])
+
+## Test class of input preserved
+%!assert (class (vmrnd (1, 2)), "double")
+%!assert (class (vmrnd (single (1), 2)), "single")
+%!assert (class (vmrnd (single ([1, 1]), 2)), "single")
+%!assert (class (vmrnd (1, single (2))), "single")
+%!assert (class (vmrnd (1, single ([2, 2]))), "single")
+
+## Test input validation
+%!error vmrnd ()
+%!error vmrnd (1)
+%!error vmrnd (ones (3), 2 * ones (2))
+%!error vmrnd (ones (2), 2 * ones (3))
+%!error vmrnd (i, 2)
+%!error vmrnd (1, i)
+%!error vmrnd (1, 2, -1)
+%!error vmrnd (1, 2, ones (2))
+%!error vmrnd (1, 2, [2 -1 2])
+%!error vmrnd (1 * ones (2),2, 3)
+%!error vmrnd (1 * ones (2),2, [3, 2])
+%!error vmrnd (1 * ones (2),2, 3, 2)
+
