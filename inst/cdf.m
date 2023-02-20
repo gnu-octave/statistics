a98b22f0c85e5a0df921b4df8af2d6eb370bb637
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-02-20 21:54:40 +0200

    cdf: update to handle new CDFs

--- inst/cdf.m
+++ inst/cdf.m
@@ -17,142 +17,170 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {@var{retval} =} cdf (@var{name}, @var{X}, @dots{})
+## @deftypefn  {statistics} {@var{retval} =} cdf (@var{name}, @var{x}, @dots{})
 ##
-## Return cumulative density function of @var{name} function for value @var{x}.
+## Return the CDF of @var{name} distribution function for value @var{x}.
 ##
-## This is a wrapper around various @var{name}cdf and @var{name}_cdf
-## functions. See the individual functions help to learn the signification of
-## the arguments after @var{x}. Supported functions and corresponding number of
-## additional arguments are:
+## This is a wrapper around various @qcode{name}cdf and @qcode{name}_cdf
+## functions. See the corresponding functions' help to learn the signification
+## of the arguments after @var{x}.
 ##
-## @multitable @columnfractions 0.02 0.3 0.45 0.2
-## @headitem @tab function @tab alternative @tab args
-## @item @tab "bbs" @tab "Birnbaum-Saunders" @tab 3
-## @item @tab "beta" @tab @tab 2
-## @item @tab "bino" @tab "binomial" @tab 2
-## @item @tab "bino" @tab "binomial" @tab 3 include 'upper'
-## @item @tab "burr" @tab "Burr" @tab 2
-## @item @tab "cauchy" @tab "Cauchy" @tab 2
-## @item @tab "chi2" @tab "chi-square" @tab 1
-## @item @tab "copula" @tab "Copula family" @tab 2
-## @item @tab "copula" @tab "Copula family" @tab 3 include nu
-## @item @tab "discrete" @tab "univariate discrete" @tab 2
-## @item @tab "empirical" @tab "univariate empirical" @tab 1
-## @item @tab "exp" @tab "exponential" @tab 1
-## @item @tab "f" @tab @tab 2
-## @item @tab "gam" @tab "gamma" @tab  2
-## @item @tab "geo" @tab "geometric" @tab 1
-## @item @tab "gev" @tab "generalized extreme value" @tab  3
-## @item @tab "gp" @tab "generalized Pareto" @tab 3
-## @item @tab "hyge" @tab "hypergeometric" @tab 3
-## @item @tab "jsu" @tab "Johnson SU" @tab 2
-## @item @tab "ks" @tab "Kolmogorov-Smirnov" @tab 1
-## @item @tab "laplace" @tab "Laplace" @tab 0
-## @item @tab "logistic" @tab @tab 0
-## @item @tab "logn" @tab "lognormal" @tab 0  defaults: mu=0,sigma=1
-## @item @tab "logn" @tab "lognormal" @tab 2
-## @item @tab "mvn" @tab "multivariate normal" @tab 2
-## @item @tab "mvn" @tab "multivariate normal" @tab 3 include low limit a
-## @item @tab "mvt" @tab "multivariate Student" @tab 2
-## @item @tab "mvt" @tab "multivariate Student" @tab 3 include low limit a
-## @item @tab "naka" @tab "Nakagami" @tab 2
-## @item @tab "nbin" @tab "negative binomial" @tab 2
-## @item @tab "norm" @tab "normal" @tab 0  defaults: mu=0,sigma=1
-## @item @tab "norm" @tab "normal" @tab 2
-## @item @tab "poiss" @tab "Poisson" @tab 1
-## @item @tab "rayl" @tab "Rayleigh" @tab 1
-## @item @tab "stdnormal" @tab "standard normal" @tab 0
-## @item @tab "t" @tab @tab 1
-## @item @tab "tri" @tab "triangular" @tab 3
-## @item @tab "unid" @tab "uniform discrete" @tab 1
-## @item @tab "unif" @tab "uniform" @tab 0  defaults: a=0,b=1
-## @item @tab "unif" @tab "uniform" @tab 2
-## @item @tab "wbl" @tab "Weibull" @tab 2
+## @var{name} must be a char string of the name or the abbreviation of the
+## desired cumulative distribution functionas listed in the followng table.
+## The last column shows the maximum number of extra arguments that can be
+## passed to the desired CDF.
+##
+## @multitable @columnfractions 0.02 0.43 0.2 0.35
+## @headitem @tab Distribution Name @tab Abbreviation @tab Max. Extra Arguments
+## @item @tab @qcode{"Birnbaum-Saunders"} @tab @qcode{"bbs"} @tab 3
+## @item @tab @qcode{"Beta"} @tab @qcode{"beta"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Binomial"} @tab @qcode{"bino"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Burr"} @tab @qcode{"burr"} @tab 2
+## @item @tab @qcode{"Bivariate"} @tab @qcode{"bvn"} @tab 2
+## @item @tab @qcode{"Cauchy"} @tab @qcode{"cauchy"} @tab 2
+## @item @tab @qcode{"Chi-square"} @tab @qcode{"chi2"} @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Copula family"} @tab @qcode{"copula"} @tab 3
+## @item @tab @qcode{"Extreme value"} @tab @qcode{"ev"} @tab 5
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Exponential"} @tab @qcode{"exp"} @tab 4
+## (including @qcode{"upper"})
+## @item @tab @qcode{"F-Distribution"} @tab @qcode{"f"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Gamma"} @tab @qcode{"gam"} @tab 5
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Geometric"} @tab @qcode{"geo"} @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Generalized extreme value"} @tab @qcode{"gev"} @tab 4
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Generalized Pareto"} @tab @qcode{"gp"} @tab 4
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Hypergeometric"} @tab @qcode{"hyge"} @tab 4
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Johnson SU"} @tab @qcode{"jsu"} @tab 2
+## @item @tab @qcode{"Laplace"} @tab @qcode{"laplace"} @tab 2
+## @item @tab @qcode{"Logistic"} @tab @qcode{"logistic"} @tab 2
+## @item @tab @qcode{"Lognormal"} @tab @qcode{"logn"} @tab 5
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Multivariate normal"} @tab @qcode{"mvn"} @tab 4
+## @item @tab @qcode{"Multivariate Student T"} @tab @qcode{"mvt"} @tab 3
+## @item @tab @qcode{"Quasi-Monte-Carlo Multivariate Student T"}
+## @tab @qcode{"mvtqmc"} @tab 6
+## @item @tab @qcode{"Nakagami"} @qcode{"naka"} @tab @tab 2
+## @item @tab @qcode{"Negative binomial"} @tab @qcode{"nbin"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Noncentral F-Distribution"} @tab @qcode{"ncf"} @tab 4
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Noncentral Student T"} @tab @qcode{"nct"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Noncentral Chi-Square"} @tab @qcode{"ncx2"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Normal"} @qcode{"norm"} @tab @tab 5
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Poisson"} @qcode{"poiss"} @tab @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Rayleigh"} @qcode{"rayl"} @tab @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Standard normal"} @tab @qcode{"stdnormal"} @tab 0
+## @item @tab @qcode{"Student T"} @tab @qcode{"t"} @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Triangular"} @tab @qcode{"tri"} @tab 3
+## @item @tab @qcode{"Discrete uniform"} @tab @qcode{"unid"} @tab 2
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Uniform"} @tab @qcode{"unif"} @tab 3
+## (including @qcode{"upper"})
+## @item @tab @qcode{"Von Mises"} @tab @qcode{"vm"} @tab 2
+## @item @tab @qcode{"Weibull"} @tab @qcode{"wbl"} @tab 5
+## (including @qcode{"upper"})
 ## @end multitable
 ##
 ## @seealso{pdf, rnd}
 ## @end deftypefn
 
-function [retval] = cdf (varargin)
+function [retval] = cdf (name, varargin)
   ## implemented functions
   persistent allcdf = { ...
-            {"bbs", "Birnbaum-Saunders"}, @bbscdf, 3, ...
-            {"beta"}, @betacdf, 2, ...
-            {"bino", "binomial"}, @binocdf, 2, ...
-            {"bino", "binomial"}, @binocdf, 3, ... ## include 'upper'
-            {"burr", "Burr"}, @burrcdf, 2, ...
-            {"cauchy", "Cauchy"}, @cauchy_cdf, 2, ...
-            {"chi2", "chi-square"}, @chi2cdf, 1, ...
-            {"copula", "Copula family"}, @copulacdf, 2, ...
-            {"copula", "Copula family"}, @copulacdf, 3, ... ## include nu
-            {"discrete", "univariate discrete"}, @discrete_cdf, 2, ...
-            {"empirical", "univariate empirical"}, @empirical_cdf, 1, ...
-            {"exp", "exponential"}, @expcdf, 1, ...
-            {"f"}, @fcdf, 2, ...
-            {"gam", "gamma"}, @gamcdf, 2, ...
-            {"geo", "geometric"}, @geocdf, 1, ...
-            {"gev", "generalized extreme value"}, @gevcdf, 3, ...
-            {"gp", "generalized Pareto"}, @gpcdf, 3, ...
-            {"hyge", "hypergeometric"}, @hygecdf, 3, ...
-            {"jsu", "Johnson SU"}, @jsucdf, 2, ...
-            {"ks", "kolmogorov-smirnov"}, @kolmogorov_smirnov_cdf, 1, ...
-            {"laplace", "Laplace"}, @laplace_cdf, 0, ...
-            {"logistic"}, @logistic_cdf, 0, ...
-            {"logn", "lognormal"}, @logncdf, 0, ... ## mu = 0, sigma = 1
-            {"logn", "lognormal"}, @logncdf, 2, ...
-            {"mvn", "multivariate normal"}, @mvncdf, 2, ...
-            {"mvn", "multivariate normal"}, @mvncdf, 3, ... ## include alpha
-            {"mvt", "multivariate Student"}, @mvncdf, 2, ...
-            {"mvt", "multivariate Student"}, @mvncdf, 3, ... ## include alpha
-            {"naka", "Nakagami"}, @nakacdf, 2, ...
-            {"nbin", "negative binomial"}, @nbincdf, 2, ...
-            {"norm", "normal"}, @normcdf, 0, ... ## mu = 0, sigma = 1
-            {"norm", "normal"}, @normcdf, 2, ...
-            {"poiss", "Poisson"}, @poisscdf, 1, ...
-            {"rayl", "Rayleigh"}, @raylcdf, 1, ...
-            {"stdnormal",  "standard normal"}, @stdnormal_cdf, 0, ...
-            {"t"}, @tcdf, 1, ...
-            {"tri", "triangular"}, @tricdf, 3, ...
-            {"unit", "uniform discrete"}, @unidcdf, 1, ...
-            {"unif", "uniform"}, @unifcdf, 0, ...
-            {"unif", "uniform"}, @unifcdf, 2, ...
-            {"wbl", "Weibull"}, @wblcdf, 2};
+    {"bbs"      , "Birnbaum-Saunders"},         @bbscdf,       3, ...
+    {"beta"     , "Beta"},                      @betacdf,      3, ... # "upper"
+    {"bino"     , "Binomial"},                  @binocdf,      3, ... # "upper"
+    {"burr"     , "Burr"},                      @burrcdf,      2, ...
+    {"bvn"      , "Bivariate"},                 @bvncdf,       2, ...
+    {"cauchy"   , "Cauchy"},                    @cauchy_cdf,   2, ...
+    {"chi2"     , "chi-square"},                @chi2cdf,      2, ... # "upper"
+    {"copula"   , "Copula family"},             @copulacdf,    3, ...
+    {"ev"       , "Extreme value"},             @evcdf,        5, ... # "upper"
+    {"exp"      , "Exponential"},               @expcdf,       4, ... # "upper"
+    {"f"        , "F-Distribution"},            @fcdf,         3, ... # "upper"
+    {"gam"      , "Gamma"},                     @gamcdf,       5, ... # "upper"
+    {"geo"      , "Geometric"},                 @geocdf,       2, ... # "upper"
+    {"gev"      , "Generalized extreme value"}, @gevcdf,       4, ... # "upper"
+    {"gp"       , "Generalized Pareto"},        @gpcdf,        4, ... # "upper"
+    {"hyge"     , "Hypergeometric"},            @hygecdf,      4, ... # "upper"
+    {"jsu"      , "Johnson SU"},                @jsucdf,       2, ...
+    {"laplace"  , "Laplace"},                   @laplace_cdf,  2, ...
+    {"logistic" , "Logistic"},                  @logistic_cdf, 2, ...
+    {"logn"     , "Lognormal"},                 @logncdf,      5, ... # "upper"
+    {"mvn"      , "Multivariate normal"},       @mvncdf,       4, ...
+    {"mvt"      , "Multivariate Student T"},    @mvtcdf,       3, ...
+    {"mvtqmc"   , "Quasi-Monte-Carlo Multivariate Student T"}, @mvtcdfqmc,6, ...
+    {"naka"     , "Nakagami"},                  @nakacdf,      2, ...
+    {"nbin"     , "Negative binomial"},         @nbincdf,      3, ... # "upper"
+    {"ncf"      , "Noncentral F-Distribution"}, @ncfcdf,       4, ... # "upper"
+    {"nct"      , "Noncentral Student T"},      @nctcdf,       3, ... # "upper"
+    {"ncx2"     , "Noncentral Chi-Square"},     @ncx2cdf,      3, ... # "upper"
+    {"norm"     , "Normal"},                    @normcdf,      5, ... # "upper"
+    {"poiss"    , "Poisson"},                   @poisscdf,     2, ... # "upper"
+    {"rayl"     , "Rayleigh"},                  @raylcdf,      2, ... # "upper"
+    {"stdnormal", "Standard normal"},           @stdnormal_cdf,0, ...
+    {"t"        , "Student T"},                 @tcdf,         2, ... # "upper"
+    {"tri"      , "Triangular"},                @tricdf,       3, ...
+    {"unid"     , "Discrete uniform"},          @unidcdf,      2, ... # "upper"
+    {"unif"     , "Uniform"},                   @unifcdf,      3, ... # "upper"
+    {"vm"       , "Von Mises"},                 @vmcdf,        2, ...
+    {"wbl"      , "Weibull"},                   @wblcdf,       5};    # "upper"
 
-  if (numel (varargin) < 2 || ! ischar (varargin{1}))
+  if (numel (varargin) < 1 || ! ischar (name))
     print_usage ();
   endif
 
-  name = varargin{1};
-  x = varargin{2};
-
-  varargin(1:2) = [];
+  x = varargin{1};
+  varargin(1) = [];
   nargs = numel (varargin);
 
   cdfnames = allcdf(1:3:end);
-  cdfhdl = allcdf(2:3:end);
-  cdfargs = allcdf(3:3:end);
+  cdfhandl = allcdf(2:3:end);
+  cdfargs  = allcdf(3:3:end);
 
-  idx = cellfun (@(x, y)any(strcmpi (name, x) & nargs == y), cdfnames, cdfargs);
+  idx = cellfun (@(x)any(strcmpi (name, x)), cdfnames);
   ## Add special list
-  special = {"copula", "Copula family", "mvn", "multivariate normal", ...
-             "mvt", "multivariate Student"};
+  special = {"copula", "Copula family", "mvt", "multivariate Student"};
 
   if (any (idx))
-    if (nargs == cdfargs{idx} && ! any (strcmpi (name, special)))
-      retval = feval (cdfhdl{idx}, x, varargin{:});
-    elseif (nargs == cdfargs{idx} && any (strcmpi (name, special)))
-      if ((any (strcmpi (name, special(3:6))) && nargs == 3) ||
-          any (strcmpi (name, special(1:2))))
-        retval = feval (cdfhdl{idx}, varargin{1}, x, varargin{2:end});
-      else
-        retval = feval (cdfhdl{idx}, x, varargin{:});
-      endif
-    else
-      error ("cdf: %s requires %d arguments", name, cdfargs{idx})
+
+    if (nargs > cdfargs{idx})
+      error ("cdf: %s takes up to %d extra arguments.", name, cdfargs{idx})
     endif
+
+    if (! any (strcmpi (name, special)))
+      retval = feval (cdfhandl{idx}, x, varargin{:});
+
+    elseif (any (strcmpi (name, special(1:2))))
+      retval = feval (cdfhandl{idx}, varargin{1}, x, varargin{2:end});
+
+    elseif (any (strcmpi (name, special(3:4))))
+
+      if (nargs == 2)
+        retval = feval (cdfhandl{idx}, x, varargin{:});
+      else
+        retval = feval (cdfhandl{idx}, varargin{1}, x, varargin{2:end});
+      endif
+
+    endif
+
   else
-    error ("cdf: %s not implemented", name);
+    error ("cdf: %s distribution is not implemented in Statistics.", name);
   endif
 
 endfunction
@@ -173,6 +201,8 @@ endfunction
 %! sigma = [1.0 0.5; 0.5 1.0];
 %! assert (cdf ("multivariate normal", x, mu, sigma), ...
 %!         mvncdf (x, mu, sigma), 0.01)
-%! a = [-inf 0];
-%! assert (cdf ("mvn", x, a, mu, sigma), ...
-%!         mvncdf (a, x, mu, sigma), 0.01)
+%!test
+%! x = 1.2;
+%! mu = [1.0 0.5; 0.5 1.0];
+%! sigma = [3];
+%! assert (cdf ("norm", x, mu, sigma), normcdf (x, mu, sigma), 0.01)
