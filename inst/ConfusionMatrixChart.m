4f011507f1581201147c6ec643a7513a85d45b76
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-02-27 17:31:58 +0200

    ConfusionMatrixChart: added BIST and DEMO

--- inst/ConfusionMatrixChart.m
+++ inst/ConfusionMatrixChart.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2020-2021 Stefano Guidoni <ilguido@users.sf.net>
+## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
