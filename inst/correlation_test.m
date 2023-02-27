a37c25221d410ddf0f3b9b2470a21f94fa98bb0b
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-02-27 19:25:04 +0200

    correlation_test: update functionality

--- inst/correlation_test.m
+++ inst/correlation_test.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 1995-2017 Kurt Hornik
+## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -17,117 +18,202 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {@var{t} =} cor_test (@var{x}, @var{y})
-## @deftypefnx {statistics} {@var{t} =} cor_test (@var{x}, @var{y}, @var{alt}, @var{method})
+## @deftypefn  {statistics} {@var{h} =} correlation_test (@var{x}, @var{y})
+## @deftypefnx {statistics} {[@var{h}, @var{pval}] =} correlation_test (@var{y}, @var{x})
+## @deftypefnx {statistics} {[@var{h}, @var{pval}, @var{stats}] =} correlation_test (@var{y}, @var{x})
+## @deftypefnx {statistics} {[@dots{}] =} correlation_test (@var{y}, @var{x}, @var{Name}, @var{Value})
 ##
-## Test whether two samples @var{x} and @var{y} come from uncorrelated
-## populations.
+## Perform a correlation coefficient test whether two samples @var{x} and
+## @var{y} come from uncorrelated populations.
 ##
-## The optional argument string @var{alt} describes the alternative
-## hypothesis, and can be @qcode{"!="} or @qcode{"<>"} (nonzero), @qcode{">"}
-## (greater than 0), or @qcode{"<"} (less than 0).  The default is the
-## two-sided case.
+## @code{@var{h} = correlation_test (@var{y}, @var{x})} tests the null
+## hypothesis that the two samples @var{x} and @var{y} come from uncorrelated
+## populations.  The result is @var{h} = 0 if the null hypothesis cannot be
+### rejected at the 5% significance level, or @var{h} = 1 if the null hypothesis
+## can be rejected at the 5% level.  @var{y} and @var{x} must be vectors of
+## equal length with finite real numbers.
 ##
-## The optional argument string @var{method} specifies which correlation
-## coefficient to use for testing.  If @var{method} is @qcode{"pearson"}
-## (default), the (usual) Pearson's product moment correlation coefficient is
-## used.  In this case, the data should come from a bivariate normal
-## distribution.  Otherwise, the other two methods offer nonparametric
-## alternatives.  If @var{method} is @qcode{"kendall"}, then Kendall's rank
-## correlation tau is used.  If @var{method} is @qcode{"spearman"}, then
-## Spearman's rank correlation rho is used.  Only the first character is
-## necessary.
+## The p-value of the test is returned in @var{pval}.  @var{stats} is a
+## structure with the following fields:
+## @multitable @columnfractions 0.05 0.2 0.05 0.70
+## @headitem @tab Field @tab @tab Value
+## @item @tab @qcode{method} @tab @tab the type of correlation coefficient used
+## for the test
+## @item @tab @qcode{df} @tab @tab the degrees of freedom (where applicable)
+## @item @tab @qcode{corrcoef} @tab @tab the correlation coefficient
+## @item @tab @qcode{stat} @tab @tab the test's statistic
+## @item @tab @qcode{dist} @tab @tab the respective distribution for the test
+## @item @tab @qcode{alt} @tab @tab the alternative hypothesis for the test
+## @end multitable
 ##
-## The output @var{t} is a structure with the following elements:
 ##
-## @itemize
-## @item pval
-## The p-value of the test.
+## @code{[@dots{}] = correlation_test (@dots{}, @var{name}, @var{value})}
+## specifies one or more of the following name/value pairs:
 ##
-## @item stat
-## The value of the test statistic.
+## @multitable @columnfractions 0.05 0.2 0.75
+## @headitem @tab Name @tab Value
+## @item @tab @qcode{"alpha"} @tab the significance level. Default is 0.05.
 ##
-## @item dist
-## The distribution of the test statistic.
+## @item @tab @qcode{"tail"} @tab a string specifying the alternative hypothesis
+## @end multitable
+## @multitable @columnfractions 0.1 0.25 0.65
+## @item @tab @qcode{"both"} @tab @math{corrcoef} is not 0 (two-tailed, default)
+## @item @tab @qcode{"left"} @tab @math{corrcoef} is less than 0 (left-tailed)
+## @item @tab @qcode{"right"} @tab @math{corrcoef} is greater than 0
+## (right-tailed)
+## @end multitable
 ##
-## @item params
-## The parameters of the null distribution of the test statistic.
+## @multitable @columnfractions 0.05 0.2 0.75
+## @item @tab @qcode{"method"} @tab a string specifying the correlation
+## coefficient used for the test
+## @end multitable
+## @multitable @columnfractions 0.1 0.25 0.65
+## @item @tab @qcode{"pearson"} @tab Pearson's product moment correlation
+## (Default)
+## @item @tab @qcode{"kendall"} @tab Kendall's rank correlation tau
+## @item @tab @qcode{"spearman"} @tab Spearman's rank correlation rho
+## @end multitable
 ##
-## @item alternative
-## The alternative hypothesis.
-##
-## @item method
-## The method used for testing.
-## @end itemize
-##
-## If no output argument is given, the p-value is displayed.
+## @seealso{regression_ftest, regression_ttest}
 ## @end deftypefn
 
-function t = cor_test (x, y, alt, method)
+function [h, pval, stats] = correlation_test (x, y, varargin)
 
-  if (nargin < 2 || nargin > 4)
+  if (nargin < 2)
     print_usage ();
   endif
 
   if (! isvector (x) || ! isvector (y) || length (x) != length (y))
-    error ("cor_test: X and Y must be vectors of the same length");
+    error ("correlation_test: X and Y must be vectors of equal length.");
   endif
 
-  if (nargin < 3)
-    alt = "!=";
-  elseif (! ischar (alt))
-    error ("cor_test: ALT must be a string");
+  ## Check for finite real numbers in X and Y
+  if (! all (isfinite (x)) || ! isreal (x))
+    error ("correlation_test: X must contain finite real numbers.");
+  endif
+  if (! all (isfinite (y(:))) || ! isreal (y))
+    error ("correlation_test: Y must contain finite real numbers.");
   endif
 
-  if (nargin < 4)
-    method = "pearson";
-  elseif (! ischar (method))
-    error ("cor_test: METHOD must be a string");
-  endif
+  ## Set default arguments
+  alpha = 0.05;
+  tail = "both";
+  method = "pearson";
+
+  ## Check additional options
+  i = 1;
+  while (i <= length (varargin))
+    switch lower (varargin{i})
+      case "alpha"
+        i = i + 1;
+        alpha = varargin{i};
+        ## Check for valid alpha
+        if (! isscalar (alpha) || ! isnumeric (alpha) || ...
+                    alpha <= 0 || alpha >= 1)
+          error ("correlation_test: invalid value for alpha.");
+        endif
+      case "tail"
+        i = i + 1;
+        tail = varargin{i};
+        if (! any (strcmpi (tail, {"both", "left", "right"})))
+          error ("correlation_test: invalid value for tail.");
+        endif
+      case "method"
+        i = i + 1;
+        method = varargin{i};
+        if (! any (strcmpi (method, {"pearson", "kendall", "spearman"})))
+          error ("correlation_test: invalid value for method.");
+        endif
+      otherwise
+        error ("correlation_test: invalid Name argument.");
+    endswitch
+    i = i + 1;
+  endwhile
 
   n = length (x);
-  m = method(1);
 
-  if (m == "p")
+  if (strcmpi (method, "pearson"))
     r = corr (x, y);
-    df = n - 2;
-    t.method = "Pearson's product moment correlation";
-    t.params = df;
-    t.stat = sqrt (df) .* r / sqrt (1 - r.^2);
-    t.dist = "t";
-    cdf = tcdf (t.stat, df);
-  elseif (m == "k")
+    stats.method = "Pearson's product moment correlation";
+    stats.df = n - 2;
+    stats.corrcoef = r;
+    stats.stat = sqrt (stats.df) .* r / sqrt (1 - r.^2);
+    stats.dist = "Student's t";
+    cdf = tcdf (stats.stat, stats.df);
+  elseif (strcmpi (method, "kendall"))
     tau = kendall (x, y);
-    t.method = "Kendall's rank correlation tau";
-    t.params = [];
-    t.stat = tau / sqrt ((2 * (2*n+5)) / (9*n*(n-1)));
-    t.dist = "stdnormal";
-    cdf = stdnormal_cdf (t.stat);
-  elseif (m == "s")
+    stats.method = "Kendall's rank correlation tau";
+    stats.df = [];
+    stats.corrcoef = tau;
+    stats.stat = tau / sqrt ((2 * (2*n+5)) / (9*n*(n-1)));
+    stats.dist = "standard normal";
+    cdf = stdnormal_cdf (stats.stat);
+  else  # spearman
     rho = spearman (x, y);
-    t.method = "Spearman's rank correlation rho";
-    t.params = [];
-    t.stat = sqrt (n-1) * (rho - 6/(n^3-n));
-    t.dist = "stdnormal";
-    cdf = stdnormal_cdf (t.stat);
-  else
-    error ("cor_test: METHOD '%s' not recognized", method);
+    stats.method = "Spearman's rank correlation rho";
+    stats.df = [];
+    stats.corrcoef = rho;
+    stats.stat = sqrt (n-1) * (rho - 6/(n^3-n));
+    stats.dist = "standard normal";
+    cdf = stdnormal_cdf (stats.stat);
   endif
 
-  if (strcmp (alt, "!=") || strcmp (alt, "<>"))
-    t.pval = 2 * min (cdf, 1 - cdf);
-  elseif (strcmp (alt, ">"))
-    t.pval = 1 - cdf;
-  elseif (strcmp (alt, "<"))
-    t.pval = cdf;
-  else
-    error ("cor_test: alternative '%s' not recognized", alt);
-  endif
+  ## Based on the "tail" argument determine the P-value
+  switch lower (tail)
+    case "both"
+      pval = 2 * min (cdf, 1 - cdf);
+    case "right"
+      pval = 1 - cdf;
+    case "left"
+      pval = cdf;
+  endswitch
 
-  t.alternative = alt;
+  stats.alt = tail;
 
-  if (nargout == 0)
-    printf ("pval: %g\n", t.pval);
-  endif
+  ## Determine the test outcome
+  h = double (pval < alpha);
 
 endfunction
+
+## Test input validation
+%!error<Invalid call to correlation_test.  Correct usage> correlation_test ();
+%!error<Invalid call to correlation_test.  Correct usage> correlation_test (1);
+%!error<correlation_test: X must contain finite real numbers.> ...
+%! correlation_test ([1 2 NaN]', [2 3 4]');
+%!error<correlation_test: X must contain finite real numbers.> ...
+%! correlation_test ([1 2 Inf]', [2 3 4]');
+%!error<correlation_test: X must contain finite real numbers.> ...
+%! correlation_test ([1 2 3+i]', [2 3 4]');
+%!error<correlation_test: Y must contain finite real numbers.> ...
+%! correlation_test ([1 2 3]', [2 3 NaN]');
+%!error<correlation_test: Y must contain finite real numbers.> ...
+%! correlation_test ([1 2 3]', [2 3 Inf]');
+%!error<correlation_test: Y must contain finite real numbers.> ...
+%! correlation_test ([1 2 3]', [3 4 3+i]');
+%!error<correlation_test: X and Y must be vectors of equal length.> ...
+%! correlation_test ([1 2 3]', [3 4 4 5]');
+%!error<correlation_test: invalid value for alpha.> ...
+%! correlation_test ([1 2 3]', [2 3 4]', "alpha", 0);
+%!error<correlation_test: invalid value for alpha.> ...
+%! correlation_test ([1 2 3]', [2 3 4]', "alpha", 1.2);
+%!error<correlation_test: invalid value for alpha.> ...
+%! correlation_test ([1 2 3]', [2 3 4]', "alpha", [.02 .1]);
+%!error<correlation_test: invalid value for alpha.> ...
+%! correlation_test ([1 2 3]', [2 3 4]', "alpha", "a");
+%!error<correlation_test: invalid Name argument.> ...
+%! correlation_test ([1 2 3]', [2 3 4]', "some", 0.05);
+%!error<correlation_test: invalid value for tail.>  ...
+%! correlation_test ([1 2 3]', [2 3 4]', "tail", "val");
+%!error<correlation_test: invalid value for tail.>  ...
+%! correlation_test ([1 2 3]', [2 3 4]', "alpha", 0.01, "tail", "val");
+%!error<correlation_test: invalid value for method.>  ...
+%! correlation_test ([1 2 3]', [2 3 4]', "method", 0.01);
+%!error<correlation_test: invalid value for method.>  ...
+%! correlation_test ([1 2 3]', [2 3 4]', "method", "some");
+
+%!test
+%! x = [6 7 7 9 10 12 13 14 15 17];
+%! y = [19 22 27 25 30 28 30 29 25 32];
+%! [h, pval, stats] = correlation_test (x, y);
+%! assert (stats.corrcoef, corr (x, y), 1e-14);
+%! assert (pval, 0.0223, 1e-4);
