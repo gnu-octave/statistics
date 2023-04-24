fd6330a8301d4cc17b8276e10829994bc13b6a76
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-04-24 19:47:35 +0300

    normlike: change variable name

--- inst/dist_fit/normlike.m
+++ inst/dist_fit/normlike.m
@@ -17,65 +17,65 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {@var{nlogL} =} normlike (@var{params}, @var{data})
-## @deftypefnx {statistics} {[@var{nlogL}, @var{avar}] =} normlike (@var{params}, @var{data})
-## @deftypefnx {statistics} {[@dots{}] =} normlike (@var{params}, @var{data}, @var{censor})
-## @deftypefnx {statistics} {[@dots{}] =} normlike (@var{params}, @var{data}, @var{censor}, @var{freq})
+## @deftypefn  {statistics} {@var{nlogL} =} normlike (@var{params}, @var{x})
+## @deftypefnx {statistics} {[@var{nlogL}, @var{avar}] =} normlike (@var{params}, @var{x})
+## @deftypefnx {statistics} {[@dots{}] =} normlike (@var{params}, @var{x}, @var{censor})
+## @deftypefnx {statistics} {[@dots{}] =} normlike (@var{params}, @var{x}, @var{censor}, @var{freq})
 ##
 ## Negative log-likelihood for the normal distribution.
 ##
-## @code{@var{nlogL} = normlike (@var{params}, @var{data})} returns the
-## negative of the log-likelihood for the normal distribution, evaluated at
-## parameters @var{params(1)} = mean and @var{params(2)} = standard deviation,
-## given @var{data}.  @var{nlogL} is a scalar.
+## @code{@var{nlogL} = normlike (@var{params}, @var{x})} returns the negative
+## log-likelihood for the normal distribution, evaluated at parameters
+## @var{params(1)} = mean and @var{params(2)} = standard deviation, given
+## @var{x}.  @var{nlogL} is a scalar.
 ##
-## @code{[@var{nlogL}, @var{avar}] = normlike (@var{params}, @var{data})}
+## @code{[@var{nlogL}, @var{avar}] = normlike (@var{params}, @var{x})}
 ## returns the inverse of Fisher's information matrix, @var{avar}.  If the input
 ## parameter values in @var{params} are the maximum likelihood estimates, the
 ## diagonal elements of @var{avar} are their asymptotic variances.  @var{avar}
 ## is based on the observed Fisher's information, not the expected information.
 ##
-## @code{[@dots{}] = normlike (@var{params}, @var{data}, @var{censor})} accepts
-## a boolean vector of the same size as @var{data} that is 1 for observations
+## @code{[@dots{}] = normlike (@var{params}, @var{x}, @var{censor})} accepts
+## a boolean vector of the same size as @var{x} that is 1 for observations
 ## that are right-censored and 0 for observations that are observed exactly.
 ##
-## @code{[@dots{}] = normlike (@var{params}, @var{data}, @var{censor},
-## @var{freq})} accepts a frequency vector of the same size as @var{data}.
+## @code{[@dots{}] = normlike (@var{params}, @var{x}, @var{censor},
+## @var{freq})} accepts a frequency vector of the same size as @var{x}.
 ## @var{freq} typically contains integer frequencies for the corresponding
-## elements in @var{data}, but it may contain any non-integer non-negative
+## elements in @var{x}, but it may contain any non-integer non-negative
 ## values.  Pass in [] for @var{censor} to use its default value.
 ##
 ## @seealso{normcdf, norminv, normpdf, normrnd, normfit, normstat}
 ## @end deftypefn
 
-function [nlogL, avar] = normlike (params ,data, censor, freq)
+function [nlogL, avar] = normlike (params ,x, censor, freq)
 
   ## Check input arguments
   if (nargin < 2)
     error ("normlike: too few input arguments.");
   endif
-  if (! isvector (data))
-    error ("normlike: DATA must be a vector.");
+  if (! isvector (x))
+    error ("normlike: X must be a vector.");
   endif
   if (numel (params) != 2)
     error ("normlike: PARAMS must be a two-element vector.");
   endif
   if (nargin < 3 || isempty (censor))
-    censor = zeros (size (data));
-  elseif (! isequal (size (data), size (censor)))
-    error ("normlike: DATA and CENSOR vectors mismatch.");
+    censor = zeros (size (x));
+  elseif (! isequal (size (x), size (censor)))
+    error ("normlike: X and CENSOR vectors mismatch.");
   endif
   if nargin < 4 || isempty(freq)
-    freq = ones (size (data));
-  elseif (isequal (size (data), size (freq)))
+    freq = ones (size (x));
+  elseif (isequal (size (x), size (freq)))
     nulls = find (freq == 0);
     if (numel (nulls) > 0)
-      data(nulls) = [];
+      x(nulls) = [];
       censor(nulls) = [];
       freq(nulls) = [];
     endif
   else
-    error ("normlike: DATA and FREQ vectors mismatch.");
+    error ("normlike: X and FREQ vectors mismatch.");
   endif
 
   ## Get mu and sigma values
@@ -87,8 +87,8 @@ function [nlogL, avar] = normlike (params ,data, censor, freq)
   endif
 
   ## Compute the individual log-likelihood terms.  Force a log(0)==-Inf for
-  ## data from extreme right tail, instead of getting exp(Inf-Inf)==NaN.
-  z = (data - mu) ./ sigma;
+  ## x from extreme right tail, instead of getting exp(Inf-Inf)==NaN.
+  z = (x - mu) ./ sigma;
   L = -0.5 .* z .^ 2 - log (sqrt (2 .* pi) .* sigma);
   if (any (censor))
     censored = censor == 1;
@@ -123,47 +123,47 @@ endfunction
 
 ## Test input validation
 %!error<normlike: too few input arguments.> normlike ([12, 15]);
-%!error<normlike: DATA must be a vector.> normlike ([12, 15], ones (2));
+%!error<normlike: X must be a vector.> normlike ([12, 15], ones (2));
 %!error<normlike: PARAMS must be a two-element vector.> ...
 %! normlike ([12, 15, 3], [1:50]);
-%!error<normlike: DATA and CENSOR vectors mismatch.> ...
+%!error<normlike: X and CENSOR vectors mismatch.> ...
 %! normlike ([12, 15], [1:50], [1, 2, 3]);
-%!error<normlike: DATA and FREQ vectors mismatch.> ...
+%!error<normlike: X and FREQ vectors mismatch.> ...
 %! normlike ([12, 15], [1:50], [], [1, 2, 3]);
 
 ## Results compared with Matlab
 %!test
-%! data = 1:50;
-%! [nlogL, avar] = normlike ([2.3, 1.2], data);
+%! x = 1:50;
+%! [nlogL, avar] = normlike ([2.3, 1.2], x);
 %! avar_out = [7.5767e-01, -1.8850e-02; -1.8850e-02, 4.8750e-04];
 %! assert (nlogL, 13014.95883783327, 1e-10);
 %! assert (avar, avar_out, 1e-4);
 %!test
-%! data = 1:50;
-%! [nlogL, avar] = normlike ([2.3, 1.2], data * 0.5);
+%! x = 1:50;
+%! [nlogL, avar] = normlike ([2.3, 1.2], x * 0.5);
 %! avar_out = [3.0501e-01, -1.5859e-02; -1.5859e-02, 9.1057e-04];
 %! assert (nlogL, 2854.802587833265, 1e-10);
 %! assert (avar, avar_out, 1e-4);
 %!test
-%! data = 1:50;
-%! [nlogL, avar] = normlike ([21, 15], data);
+%! x = 1:50;
+%! [nlogL, avar] = normlike ([21, 15], x);
 %! avar_out = [5.460474308300396, -1.600790513833993; ...
 %!             -1.600790513833993, 2.667984189723321];
 %! assert (nlogL, 206.738325604233, 1e-12);
 %! assert (avar, avar_out, 1e-14);
 %!test
-%! data = 1:50;
+%! x = 1:50;
 %! censor = ones (1, 50);
 %! censor([2, 4, 6, 8, 12, 14]) = 0;
-%! [nlogL, avar] = normlike ([2.3, 1.2], data, censor);
+%! [nlogL, avar] = normlike ([2.3, 1.2], x, censor);
 %! avar_out = [3.0501e-01, -1.5859e-02; -1.5859e-02, 9.1057e-04];
 %! assert (nlogL, Inf);
 %! assert (avar, [NaN, NaN; NaN, NaN]);
 %!test
-%! data = 1:50;
+%! x = 1:50;
 %! censor = ones (1, 50);
 %! censor([2, 4, 6, 8, 12, 14]) = 0;
-%! [nlogL, avar] = normlike ([21, 15], data, censor);
+%! [nlogL, avar] = normlike ([21, 15], x, censor);
 %! avar_out = [24.4824488866131, -10.6649544179636; ...
 %!             -10.6649544179636, 6.22827849965737];
 %! assert (nlogL, 86.9254371829733, 1e-12);
