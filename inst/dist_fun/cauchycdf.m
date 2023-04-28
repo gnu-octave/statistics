55c7e32b31cbc014f29ae03d58077768e634e21c
pr0m1th3as <abertsatos@biol.uoa.gr>
2023-04-28 17:10:25 +0300

    update copyright header in Cauchy dist functions

--- inst/dist_fun/cauchycdf.m
+++ inst/dist_fun/cauchycdf.m
@@ -2,6 +2,8 @@
 ## Copyright (C) 1995-2016 Kurt Hornik
 ## Copyright (C) 2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
+## This file is part of the statistics package for GNU Octave.
+##
 ## This program is free software: you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
 ## published by the Free Software Foundation, either version 3 of the
