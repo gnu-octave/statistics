ff7a9701de05f6d8e95167f694ce33979d9e4f9e
AZM999 <azmatraza27@gmail.com>
2023-05-22 01:26:10 +0530

    Added acetylene demo example to produce ridge traces, fixed coding style, fixed pseudo observationcolumn error, added text to help for clear explanation of the function, texinfo seems to be breaking in my version of octave please review

--- inst/ridge.m
+++ inst/ridge.m
@@ -19,15 +19,35 @@
 ## @deftypefn  {statistics} {@var{b} =} ridge (@var{y}, @var{X}, [@var{alpha}])
 ## @deftypefnx {statistics} {@var{b} =} ridge (@var{y}, @var{X}, [@var{alpha}], @var{flag})
 ##
-## @code{@var{b} = ridge (@var{y}, @var{X}, @var{alpha}) returns the coefficient
-## estimates for ridge ridgeion from ridgeor matrix @var{X} and the response @var{y}.
+##
+## Here,
+##
+## @itemize
+## @item
+## @code{y} is a column vector of observed values
+## @item
+## @code{X} is a matrix of regressors, with the first column filled with
+## the constant value 1
+## @item
+## @code{alpha} is ridge parameter
+## @item
+## @code{flag} is used as indicator for scaling the data back to original data scale
+## @end itemize
+##
+##
+## @code{@var{b} = ridge (@var{y}, @var{X}, @var{alpha}) returns the vector of coefficient
+## estimates by applying ridge regression from predictor matrix @var{X} and the response @var{y}.
 ## Each value of @var{b} is the coefficient for the respective ridge parameter given @var{alpha}.
 ## by default @var{b} is calculated after centering and scaling to mean 0 and standard deviation 1.
+## In general it can be used to produce ridge traces ( see demo example ) with coefficients as the
+## function returns a vector of regression coefficients.
 ##
-## @code{@var{b} = ridge (@var{y}, @var{X}, @var{alpha}, @var{flag}) returns the
+## @code{@var{b} = ridge (@var{y}, @var{X}, @var{alpha}, @var{flag}) performs the regression returns the
 ## coefficient estimates for specified scaling by @var{flag}. when @var{flag} is given 0,
-## the function restores the coefficient to the scale of the original data.
-## for @var{flag} = 1 is same as the above function as no restoration is done.
+## the function restores the coefficient to the scale of the original data thus is more useful for
+## making predictions. for @var{flag} = 1 is same as the above function as no restoration is done to the scale.
+##
+##
 ##
 ## @seealso{ridge, lasso, stepwisefit}
 ##
@@ -35,16 +55,16 @@
 
 
 
-function b = ridge (y,X,alpha,flag)
+function b = ridge (y, X, alpha, flag)
 
-  % input parameter check
+  ## input parameter check
   if  (nargin > 4 || nargin < 3)
     error ("ridge: Invalid number of arguments.");
   endif
 
 
   if (! ismatrix (y))
-    error ("ridge: y must be a numeric matrix.");
+    error ("ridge: Y must be a numeric matrix.");
   endif
   if (! ismatrix (X))
     error ("ridge: X must be a numeric matrix.");
@@ -61,91 +81,98 @@ function b = ridge (y,X,alpha,flag)
 
 
   if (columns (y) != 1)
-    error ("ridge: y must be a column vector.");
+    error ("ridge: Y must be a column vector.");
   endif
 
 
   if (rows (y) != rows (X))
-    error ("ridge: y and X must contain the same number of rows.");
+    error ("ridge: Y and X must contain the same number of rows.");
   endif
 
-  % remove any missing values
+  ## remove any missing values
   notnans = !logical (sum (isnan ([y X]), 2));
   y = y(notnans);
   X = X(notnans,:);
 
-  % normalising X to mean zero and Std deviation ones
+  ## normalising X to mean zero and Std deviation ones
   m = mean(X);
   stdx = std(X,0,1);
 
   z = (X - m) ./ stdx;
 
-  % add pseudo observations
-  Z_pseudo = [X; (sqrt(alpha(1)) * eye (rows(X)))];
-  Y_pseudo = [y; zeros(rows(X), 1)];
-  % coefficient
+  ## add pseudo observations
+  Z_pseudo = [z; (sqrt(alpha(1)) .* eye (columns(X)))];
+  Y_pseudo = [y; zeros(columns(X), 1)];
+  ## coefficient
+
   b = Z_pseudo \ Y_pseudo;
+
 
   alphas = numel(alpha);
 
-  % compute the coefficient estimates for additional ridge parameters.
+
+  ## compute the coefficient estimates for additional ridge parameters.
   if (alphas >= 2)
 
-    % adding a multiple of the identity matrix to the last p rows.
+    ## adding a multiple of the identity matrix to the last p rows.
 
-    % b is set to 0 for the current ridge parameter value
-   b(alphas) = 0;
+    ## b is set to 0 for the current ridge parameter value
+    b(end,alphas) = 0;
+
 
    for i=2:alphas
-     Z_pseudo ( end - rows(X) + 1 : end)  =  sqrt( alpha(i) * eye( rows(X)));
+     Z_pseudo ( end - columns(X) + 1 : end,:)  =  sqrt( alpha(i)) .* eye( columns(X));
      b(:,i) = Z_pseudo \ Y_pseudo;
 
    endfor
   endif
 
-   % changing back to the scale
-   if( unscale)
-   b = b ./ repmat( stdx', 1, alphas);
-   b = [ mean(y) - m * b; b];
+   ## changing back to the scale
+   if (unscale)
+    b = b ./ repmat (stdx', 1, alphas);
+    b = [ mean(y)-m.*b; b];
    endif
 
 endfunction
 
 
+%! demo
+%!
+%! load acetylene
+%!
+%! X = [x1 x2 x3];
+%!
+%!
+%! x0 = ones(16,1);
+%! x1x2 = x1 .* x2;
+%! x1x3 = x1 .* x3;
+%! x2x3 = x2 .* x3;
+%!
+%! D = [x1 x2 x3 x1x2 x1x3 x2x3];
+%!
+%! k = 0:1e-5:5e-3;
+%!
+%! b = ridge(y,D,k);
+%!
+%! figure
+%! plot(k,b,'LineWidth',2)
+%! ylim([-100 100])
+%! grid on
+%! xlabel('Ridge Parameter')
+%! ylabel('Standardized Coefficient')
+%! title('Ridge Trace')
+%! legend('x1','x2','x3','x1x2','x1x3','x2x3')
+%!
+%!
+%! % carbig dataset example can be added once the reproducibility problem is fixed with cvpartition
+%!
 ## test input validation
 %!error<ridge: Invalid number of arguments.> ridge (1, 2)
 %!error<ridge: Invalid number of arguments.> ridge (1, 2, 3, 4, 5)
 %!error<ridge: y must be a column vector.> ridge ([1, 2, 3], [], 4)
 %!error<ridge: y and X must contain the same number of rows.> ridge ([1; 2; 3; 4; 5], [1, 2, 3], 3)
 %!
-%!
-%!demo
-%!
-%! load carbig
-%! X = [Acceleration Weight Displacement Horsepower];
-%! y = MPG;
-%!
-%! n = length(y);
-%! rng('default')
-%! c = cvpartition(n,'HoldOut',0.3);
-%! idxTrain = training(c,1);
-%! idxTest = ~idxTrain;
-%!
-%! Xt = X(idxTrain,:);
-%! yt = y(idxTrain);
-%!
-%! k = 5;
-%!
-%! b = ridge(yt,Xt(:,1),k,0);
-%!
-%! yhat = b(1) + X(idxTest,:)*b(2:end);
-%!
-%! scatter(y(idxTest),yhat)
-%! hold on
-%! plot(y(idxTest),y(idxTest))
-%! xlabel('Actual MPG')
-%! ylabel('Predicted MPG')
-%! hold off
+
 
 
 
