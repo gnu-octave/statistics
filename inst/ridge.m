ee1b1ec256ecef7536e88e10434ed53be5b85b2a
AZM999 <azmatraza27@gmail.com>
2023-06-26 20:16:50 +0530

    added second demo for carbig dataset to predict mpg values, fixed the unscale block, added BISTs for different values of k in acetylene dataset

--- inst/ridge.m
+++ inst/ridge.m
@@ -122,7 +122,7 @@ function b = ridge (y, X, k, scaled)
   ## Changing back to the scale
   if (unscale)
     b = b ./ repmat (stdx', 1, nk);
-    b = [mean(y)-m.*b; b];
+    b = [mean(y)-m*b; b];
   endif
 
 endfunction
@@ -135,7 +135,6 @@ endfunction
 %!
 %! X = [x1, x2, x3];
 %!
-%! x0 = ones (16, 1);
 %! x1x2 = x1 .* x2;
 %! x1x3 = x1 .* x3;
 %! x2x3 = x2 .* x3;
@@ -154,6 +153,37 @@ endfunction
 %! ylabel ("Standardized Coefficient")
 %! title ("Ridge Trace")
 %! legend ("x1", "x2", "x3", "x1x2", "x1x3", "x2x3")
+%!
+%!demo
+%!
+%! load carbig
+%! X = [Acceleration Weight Displacement Horsepower];
+%! y = MPG;
+%!
+%! n = length(y);
+%!
+%! rand("seed",1); % For reproducibility
+%!
+%! c = cvpartition(n,'HoldOut',0.3);
+%! idxTrain = training(c,1);
+%! idxTest = ~idxTrain;
+%!
+%! idxTrain = training(c,1);
+%! idxTest = ~idxTrain;
+%!
+%! k = 5;
+%! b = ridge(y(idxTrain),X(idxTrain,:),k,0);
+%!
+%! % Predict MPG values for the test data using the model.
+%! yhat = b(1) + X(idxTest,:)*b(2:end);
+%! scatter(y(idxTest),yhat)
+%!
+%! hold on
+%! plot(y(idxTest),y(idxTest),"r")
+%! xlabel('Actual MPG')
+%! ylabel('Predicted MPG')
+%! hold off
+%!
 
 ## Test output
 %!test
@@ -162,6 +192,36 @@ endfunction
 %!test
 %! b = ridge ([1 2 3 4]', [1 2 3 4; 2 3 4 5]', 2);
 %! assert (b, [0.4841; 0.4841], 1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0);
+%! assert (b,[10.2273;1.97128;-0.601818],1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0.0005);
+%! assert (b,[10.2233;1.9712;-0.6056],1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0.001);
+%! assert (b,[10.2194;1.9711;-0.6094],1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0.002);
+%! assert (b,[10.2116;1.9709;-0.6169],1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0.005);
+%! assert (b,[10.1882;1.9704;-0.6393],1e-4);
+%!test
+%! load acetylene
+%! x = [x1, x2, x3];
+%! b = ridge (y, x, 0.01);
+%! assert (b,[10.1497;1.9695;-0.6761],1e-4);
 
 ## Test input validation
 %!error<ridge: function called with too few input arguments.> ridge (1)
