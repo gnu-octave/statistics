dd9d6a90b1049ca28f4146e73eca1423d392a546
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-01-08 20:45:39 +0200

    pdist: overhaul function for matlab compatibility

--- inst/pdist.m
+++ inst/pdist.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2008 Francesco Potortì <pot@gnu.org>
+## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -16,213 +17,278 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {@var{y} =} pdist (@var{x})
-## @deftypefnx {statistics} {@var{y} =} pdist (@var{x}, @var{metric})
-## @deftypefnx {statistics} {@var{y} =} pdist (@var{x}, @var{metric}, @var{metricarg}, @dots{})
+## @deftypefn  {statistics} {@var{D} =} pdist (@var{X})
+## @deftypefnx {statistics} {@var{D} =} pdist (@var{X}, @var{Distance})
+## @deftypefnx {statistics} {@var{D} =} pdist (@var{X}, @var{Distance}, @var{DistParameter})
 ##
-## Return the distance between any two rows in @var{x}.
+## Return the distance between any two rows in @var{X}.
 ##
-## @var{x} is the @var{n}x@var{d} matrix representing @var{q} row
-## vectors of size @var{d}.
+## @code{@var{D} = pdist (@var{X}} calculates the euclidean distance between
+## pairs of observations in @var{X}.  @var{X} must be an @math{MxP} numeric
+## matrix representing @math{M} points in @math{P}-dimensional space.  This
+## function computes the pairwise distances returned in @var{D} as an
+## @math{Mx(M-1)/P} row vector.  Use @code{@var{Z} = squareform (@var{D})} to
+## convert the row vector @var{D} into a an @math{MxM} symmetric matrix @var{Z},
+## where @qcode{@var{Z}(i,j)} corresponds to the pairwise distance between
+## points @qcode{i} and @qcode{j}.
 ##
-## The output is a dissimilarity matrix formatted as a row vector
-## @var{y}, @math{(n-1)*n/2} long, where the distances are in
-## the order [(1, 2) (1, 3) @dots{} (2, 3) @dots{} (n-1, n)].  You can
-## use the @code{squareform} function to display the distances between
-## the vectors arranged into an @var{n}x@var{n} matrix.
+## @code{@var{D} = pdist (@var{X}, @var{Y}, @var{Distance})} returns the
+## distance between pairs of observations in @var{X} using the metric specified
+## by @var{Distance}, which can be any of the following options.
 ##
-## @code{metric} is an optional argument specifying how the distance is
-## computed. It can be any of the following ones, defaulting to
-## "euclidean", or a user defined function that takes two arguments
-## @var{x} and @var{y} plus any number of optional arguments,
-## where @var{x} is a row vector and and @var{y} is a matrix having the
-## same number of columns as @var{x}.  @code{metric} returns a column
-## vector where row @var{i} is the distance between @var{x} and row
-## @var{i} of @var{y}. Any additional arguments after the @code{metric}
-## are passed as metric (@var{x}, @var{y}, @var{metricarg1},
-## @var{metricarg2} @dots{}).
+## @multitable @columnfractions 0.23 0.02 0.65
+## @item @qcode{"euclidean"} @tab @tab Euclidean distance.
+## @item @qcode{"squaredeuclidean"} @tab @tab Squared Euclidean distance.
+## @item @qcode{"seuclidean"} @tab @tab standardized Euclidean distance.  Each
+## coordinate difference between the rows in @var{X} and the query matrix
+## @var{Y} is scaled by dividing by the corresponding element of the standard
+## deviation computed from @var{X}.  A different scaling vector can be specified
+## with the subsequent @var{DistParameter} input argument.
+## @item @qcode{"mahalanobis"} @tab @tab Mahalanobis distance, computed using a
+## positive definite covariance matrix.  A different covariance matrix can be
+## specified with the subsequent @var{DistParameter} input argument.
+## @item @qcode{"cityblock"} @tab @tab City block distance.
+## @item @qcode{"minkowski"} @tab @tab Minkowski distance.  The default exponent
+## is 2.  A different exponent can be specified with the subsequent
+## @var{DistParameter} input argument.
+## @item @qcode{"chebychev"} @tab @tab Chebychev distance (maximum coordinate
+## difference).
+## @item @qcode{"cosine"} @tab @tab One minus the cosine of the included angle
+## between points (treated as vectors).
+## @item @qcode{"correlation"} @tab @tab One minus the sample linear correlation
+## between observations (treated as sequences of values).
+## @item @qcode{"hamming"} @tab @tab Hamming distance, which is the percentage
+## of coordinates that differ.
+## @item @qcode{"jaccard"} @tab @tab One minus the Jaccard coefficient, which is
+## the percentage of nonzero coordinates that differ.
+## @item @qcode{"spearman"} @tab @tab One minus the sample Spearman's rank
+## correlation between observations (treated as sequences of values).
+## @item @var{@@distfun} @tab @tab Custom distance function handle.  A distance
+## function of the form @code{function @var{D2} = distfun (@var{XI}, @var{YI})},
+## where @var{XI} is a @math{1xP} vector containing a single observation in
+## @math{P}-dimensional space, @var{YI} is an @math{NxP} matrix containing an
+## arbitrary number of observations in the same @math{P}-dimensional space, and
+## @var{D2} is an @math{NxP} vector of distances, where @qcode{(@var{D2}k)} is
+## the distance between observations @var{XI} and @qcode{(@var{YI}k,:)}.
+## @end multitable
 ##
-## Predefined distance functions are:
+## @code{@var{D} = pdist (@var{X}, @var{Y}, @var{Distance}, @var{DistParameter})}
+## returns the distance using the metric specified by @var{Distance} and
+## @var{DistParameter}.  The latter one can only be specified when the selected
+## @var{Distance} is @qcode{"seuclidean"}, @qcode{"minkowski"}, and
+## @qcode{"mahalanobis"}.
 ##
-## @table @samp
-## @item "euclidean"
-## Euclidean distance (default).
-##
-## @item "squaredeuclidean"
-## Squared Euclidean distance. It omits the square root from the calculation
-## of the Euclidean distance. It does not satisfy the triangle inequality.
-##
-## @item "seuclidean"
-## Standardized Euclidean distance. Each coordinate in the sum of
-## squares is inverse weighted by the sample variance of that
-## coordinate.
-##
-## @item "mahalanobis"
-## Mahalanobis distance: see the function mahalanobis.
-##
-## @item "cityblock"
-## City Block metric, aka Manhattan distance.
-##
-## @item "minkowski"
-## Minkowski metric.  Accepts a numeric parameter @var{p}: for @var{p}=1
-## this is the same as the cityblock metric, with @var{p}=2 (default) it
-## is equal to the euclidean metric.
-##
-## @item "cosine"
-## One minus the cosine of the included angle between rows, seen as
-## vectors.
-##
-## @item "correlation"
-## One minus the sample correlation between points (treated as
-## sequences of values).
-##
-## @item "spearman"
-## One minus the sample Spearman's rank correlation between
-## observations, treated as sequences of values.
-##
-## @item "hamming"
-## Hamming distance: the quote of the number of coordinates that differ.
-##
-## @item "jaccard"
-## One minus the Jaccard coefficient, the quote of nonzero
-## coordinates that differ.
-##
-## @item "chebychev"
-## Chebychev distance: the maximum coordinate difference.
-## @end table
-## @seealso{linkage, mahalanobis, squareform, pdist2}
+## @seealso{pdist2, squareform, linkage}
 ## @end deftypefn
 
-function y = pdist (x, metric, varargin)
+function D = pdist (X, varargin)
 
+  ## Check input data
   if (nargin < 1)
-    print_usage ();
-  elseif ((nargin > 1)
-          && ! ischar (metric)
-          && ! isa (metric, "function_handle"))
-    error (["pdist: the distance function must be either a string or a "
-            "function handle."]);
+	  error ("pdist: too few input arguments.");
   endif
-
-  if (nargin < 2)
-    metric = "euclidean";
+  if (! isnumeric (X) || isempty (X))
+    error ("pdist: X must be a nonempty numeric matrix.");
   endif
-
-  if (! ismatrix (x) || isempty (x))
-    error ("pdist: x must be a nonempty matrix");
-  elseif (length (size (x)) > 2)
-    error ("pdist: x must be 1 or 2 dimensional");
+  if (ndims (X) != 2)
+	  error ("pdist: X must be a two-dimensional matrix.");
   endif
-
-  y = [];
-  if (rows(x) == 1)
+  if (rows (X) < 2)
+    D = cast (zeros (1, 0), class (X));
     return;
   endif
 
-  if (ischar (metric))
-    order = nchoosek(1:rows(x),2);
-    Xi = order(:,1);
-    Yi = order(:,2);
-    X = x';
-    metric = lower (metric);
-    switch (metric)
-      case "euclidean"
-        d = X(:,Xi) - X(:,Yi);
-        y = norm (d, "cols");
+  ## Add default values
+  Distance = "euclidean";   # Distance metric
+  DistParameter = [];       # Distance parameter
 
-      case "squaredeuclidean"
-        d = X(:,Xi) - X(:,Yi);
-        y = sumsq (d);
-
-      case "seuclidean"
-        d = X(:,Xi) - X(:,Yi);
-        weights = inv (diag (var (x, 0, 1)));
-        y = sqrt (sum ((weights * d) .* d, 1));
-
-      case "mahalanobis"
-        d = X(:,Xi) - X(:,Yi);
-        weights = inv (cov (x));
-        y = sqrt (sum ((weights * d) .* d, 1));
-
-      case "cityblock"
-        d = X(:,Xi) - X(:,Yi);
-        if (str2num(version()(1:3)) > 3.1)
-          y = norm (d, 1, "cols");
-        else
-          y = sum (abs (d), 1);
-        endif
-
-      case "minkowski"
-        d = X(:,Xi) - X(:,Yi);
-        p = 2;                  # default
-        if (nargin > 2)
-          p = varargin{1};      # explicitly assigned
-        endif;
-        y = norm (d, p, "cols");
-
-      case "cosine"
-        w = sqrt (sumsq (X, 1));
-        y = 1 - ((X'*X)./(w.*w'))(((1:rows(x))'>(1:rows(x)))(:))';
-
-      case "correlation"
-        if (rows(X) == 1)
-          error ("pdist: correlation distance between scalars not defined")
-        endif
-        cor = corr (X);
-        y = 1 - cor (sub2ind (size (cor), Xi, Yi))';
-
-      case "spearman"
-        if (rows(X) == 1)
-          error ("pdist: spearman distance between scalars not defined")
-        endif
-        cor = spearman (X);
-        y = 1 - cor (sub2ind (size (cor), Xi, Yi))';
-
-      case "hamming"
-        d = logical (X(:,Xi) - X(:,Yi));
-        y = sum (d, 1) / rows (X);
-
-      case "jaccard"
-        d = logical (X(:,Xi) - X(:,Yi));
-        weights = X(:,Xi) | X(:,Yi);
-        y = sum (d & weights, 1) ./ sum (weights, 1);
-
-      case "chebychev"
-        d = X(:,Xi) - X(:,Yi);
-        y = norm (d, Inf, "cols");
-
-    endswitch
+  ## Parse additional Distance metric and Distance parameter (if available)
+  DMs = {"euclidean", "squaredeuclidean", "seuclidean", ...
+         "mahalanobis", "cityblock", "minkowski", "chebychev", ...
+         "cosine", "correlation", "hamming", "jaccard", "spearman"};
+  if (numel (varargin) > 0)
+    if (any (strcmpi (DMs, varargin{1})))
+      Distance = tolower (varargin{1});
+    elseif (is_function_handle (varargin{1}))
+      Distance = varargin{1};
+    else
+      error ("pdist: invalid value for Distance input argument.");
+    endif
+  endif
+  if (numel (varargin) > 1)
+    if (isnumeric (varargin{2}))
+      DistParameter = varargin{2};
+    else
+      error ("pdist: invalid value for DistParameter input argument.");
+    endif
   endif
 
-  if (isempty (y))
-    ## Metric is a function handle or the name of an external function
-    l = rows (x);
-    y = zeros (1, nchoosek (l, 2));
-    idx = 1;
-    for ii = 1:l-1
-      for jj = ii+1:l
-        y(idx++) = feval (metric, x(ii,:), x, varargin{:})(jj);
-      endfor
+  ## Calculate selected distance
+  order = nchoosek (1:rows (X) ,2);
+  ix = order (:,1);
+  iy = order (:,2);
+
+  ## Handle build-in distance metric
+  if (ischar (Distance))
+    switch (Distance)
+      case "euclidean"
+        D = sqrt (sum ((X(ix(:),:) - X(iy(:),:)) .^ 2, 2));
+
+      case "squaredeuclidean"
+        D = sum ((X(ix(:),:) - X(iy(:),:)) .^ 2, 2);
+
+      case "seuclidean"
+        if (isempty (DistParameter))
+          DistParameter = std (X, [], 1);
+        else
+          if (numel (DistParameter) != columns (X))
+            error (strcat (["pdist2: DistParameter for standardized"], ...
+                           [" euclidean must be a vector of equal length"], ...
+                           [" to the number of columns in X."]));
+          endif
+          if (any (DistParameter < 0))
+            error (strcat (["pdist2: DistParameter for standardized"], ...
+                           [" euclidean must be a nonnegative vector."]));
+          endif
+        endif
+        DistParameter(DistParameter == 0) = 1;  # fix constant variable
+        D = sqrt (sum (((X(ix(:),:) - X(iy(:),:)) ./ DistParameter) .^ 2, 2));
+
+      case "mahalanobis"
+        if (isempty (DistParameter))
+          DistParameter = cov (X(! any (isnan (X), 2),:));
+        else
+          if (columns (DistParameter) != columns (X))
+            error (strcat (["pdist2: DistParameter for mahalanobis"], ...
+                           [" distance must be a covariance matrix with"], ...
+                           [" the same number of columns as X."]));
+          endif
+          [~, p] = chol (DistParameter);
+          if (p != 0)
+            error (strcat (["pdist2: covariance matrix for mahalanobis"], ...
+                           [" distance must be symmetric and positive"], ...
+                           [" definite."]));
+          endif
+        endif
+        dxx = X(ix(:),:) - X(iy(:),:);
+        D   = sqrt (sum ((dxx  * inv (DistParameter)) .* dxx, 2));
+
+      case "cityblock"
+        D = sum (abs (X(ix(:),:) - X(iy(:),:)), 2);
+
+      case "minkowski"
+        if (isempty (DistParameter))
+          DistParameter = 2;
+        else
+          if (! (isnumeric (DistParameter) && isscalar (DistParameter)
+                                           && DistParameter > 0))
+            error (strcat (["pdist2: DistParameter for minkowski distance"],...
+                           [" must be a positive scalar."]));
+          endif
+        endif
+        D = sum (abs (X(ix(:),:) - X(iy(:),:)) .^ DistParameter, 2) .^ ...
+                (1 / DistParameter);
+
+      case "chebychev"
+        D = max (abs (X(ix(:),:) - X(iy(:),:)), [], 2);
+
+      case "cosine"
+        sx = sum (X .^ 2, 2) .^ (-1 / 2);
+        D  = 1 - sum (X(ix(:),:) .* X(iy(:),:), 2) .* sx(ix(:)) .* sx(iy(:));
+
+      case "correlation"
+        mX = mean (X(ix(:),:), 2);
+        mY = mean (X(iy(:),:), 2);
+        xy = sum ((X(ix(:),:) - mX) .* (X(iy(:),:) - mY), 2);
+        xx = sqrt (sum ((X(ix(:),:) - mX) .* (X(ix(:),:) - mX), 2));
+        yy = sqrt (sum ((X(iy(:),:) - mY) .* (X(iy(:),:) - mY), 2));
+        D = 1 - (xy ./ (xx .* yy));
+
+      case "hamming"
+        D = mean (abs (X(ix(:),:) != X(iy(:),:)), 2);
+
+      case "jaccard"
+        xx0 = (X(ix(:),:) != 0 | X(iy(:),:) != 0);
+        D = sum ((X(ix(:),:) != X(iy(:),:)) & xx0, 2) ./ sum (xx0, 2);
+
+      case "spearman"
+        for i = 1:size (X, 1)
+          rX(i,:) = tiedrank (X(i,:));
+        endfor
+        rM = (size (X, 2) + 1) / 2;
+        xy = sum ((rX(ix(:),:) - rM) .* (rX(iy(:),:) - rM), 2);
+        xx = sqrt (sum ((rX(ix(:),:) - rM) .* (rX(ix(:),:) - rM), 2));
+        yy = sqrt (sum ((rX(iy(:),:) - rM) .* (rX(iy(:),:) - rM), 2));
+        D = 1 - (xy ./ (xx .* yy));
+
+  endswitch
+  ## Force output ot row vector
+  D = D';
+  endif
+
+  ## Handle a function handle
+  if (is_function_handle (Distance))
+    ## Check the input output sizes of the user function
+    D2 = [];
+    try
+      D2 = Distance (X(1,:), X([2:end],:));
+    catch ME
+      error ("pdist: invalid function handle for distance metric.");
+    end_try_catch
+    Xrows = rows (X) - 1;
+    if (! isequal (size (D2), [Xrows, 1]))
+      error ("pdist: custom distance function produces wrong output size.");
+    endif
+    ## Evaluate user defined distance metric function
+    D = zeros (1, numel (ix));
+    id_beg = 1;
+    for r = 1:Xrows
+      id_end = id_beg + size (X([r+1:end],:), 1) - 1;
+      D(id_beg:id_end) = feval (Distance, X(r,:), X([r+1:end],:));
+      id_beg = id_end + 1;
     endfor
   endif
 
 endfunction
 
-%!shared xy, t, eucl
+## Test output
+%!shared xy, t, eucl, x
 %! xy = [0 1; 0 2; 7 6; 5 6];
 %! t = 1e-3;
 %! eucl = @(v,m) sqrt(sumsq(repmat(v,rows(m),1)-m,2));
-%!assert(pdist(xy),              [1.000 8.602 7.071 8.062 6.403 2.000],t);
-%!assert(pdist(xy,eucl),         [1.000 8.602 7.071 8.062 6.403 2.000],t);
-%!assert(pdist(xy,"euclidean"),  [1.000 8.602 7.071 8.062 6.403 2.000],t);
-%!assert(pdist(xy,"seuclidean"), [0.380 2.735 2.363 2.486 2.070 0.561],t);
-%!assert(pdist(xy,"mahalanobis"),[1.384 1.967 2.446 2.384 1.535 2.045],t);
-%!assert(pdist(xy,"cityblock"),  [1.000 12.00 10.00 11.00 9.000 2.000],t);
-%!assert(pdist(xy,"minkowski"),  [1.000 8.602 7.071 8.062 6.403 2.000],t);
-%!assert(pdist(xy,"minkowski",3),[1.000 7.763 6.299 7.410 5.738 2.000],t);
-%!assert(pdist(xy,"cosine"),     [0.000 0.349 0.231 0.349 0.231 0.013],t);
-%!assert(pdist(xy,"correlation"),[0.000 2.000 0.000 2.000 0.000 2.000],t);
-%!assert(pdist(xy,"spearman"),   [0.000 2.000 0.000 2.000 0.000 2.000],t);
-%!assert(pdist(xy,"hamming"),    [0.500 1.000 1.000 1.000 1.000 0.500],t);
-%!assert(pdist(xy,"jaccard"),    [1.000 1.000 1.000 1.000 1.000 0.500],t);
-%!assert(pdist(xy,"chebychev"),  [1.000 7.000 5.000 7.000 5.000 2.000],t);
+%! x = [1 2 3; 4 5 6; 7 8 9; 3 2 1];
+%!assert (pdist (xy),                 [1.000 8.602 7.071 8.062 6.403 2.000], t);
+%!assert (pdist (xy, eucl),           [1.000 8.602 7.071 8.062 6.403 2.000], t);
+%!assert (pdist (xy, "euclidean"),    [1.000 8.602 7.071 8.062 6.403 2.000], t);
+%!assert (pdist (xy, "seuclidean"),   [0.380 2.735 2.363 2.486 2.070 0.561], t);
+%!assert (pdist (xy, "mahalanobis"),  [1.384 1.967 2.446 2.384 1.535 2.045], t);
+%!assert (pdist (xy, "cityblock"),    [1.000 12.00 10.00 11.00 9.000 2.000], t);
+%!assert (pdist (xy, "minkowski"),    [1.000 8.602 7.071 8.062 6.403 2.000], t);
+%!assert (pdist (xy, "minkowski", 3), [1.000 7.763 6.299 7.410 5.738 2.000], t);
+%!assert (pdist (xy, "cosine"),       [0.000 0.349 0.231 0.349 0.231 0.013], t);
+%!assert (pdist (xy, "correlation"),  [0.000 2.000 0.000 2.000 0.000 2.000], t);
+%!assert (pdist (xy, "spearman"),     [0.000 2.000 0.000 2.000 0.000 2.000], t);
+%!assert (pdist (xy, "hamming"),      [0.500 1.000 1.000 1.000 1.000 0.500], t);
+%!assert (pdist (xy, "jaccard"),      [1.000 1.000 1.000 1.000 1.000 0.500], t);
+%!assert (pdist (xy, "chebychev"),    [1.000 7.000 5.000 7.000 5.000 2.000], t);
+%!assert (pdist (x), [5.1962, 10.3923, 2.8284, 5.1962, 5.9161, 10.7703], 1e-4);
+%!assert (pdist (x, "euclidean"), ...
+%!        [5.1962, 10.3923, 2.8284, 5.1962, 5.9161, 10.7703], 1e-4);
+%!assert (pdist (x, eucl), ...
+%!        [5.1962, 10.3923, 2.8284, 5.1962, 5.9161, 10.7703], 1e-4);
+%!assert (pdist (x, "squaredeuclidean"), [27, 108, 8, 27, 35, 116]);
+%!assert (pdist (x, "seuclidean"), ...
+%!        [1.8071, 3.6142, 0.9831, 1.8071, 1.8143, 3.4854], 1e-4);
+%!warning<matrix singular to machine precision> pdist (x, "mahalanobis");
+%!assert (pdist (x, "cityblock"), [9, 18, 4, 9, 9, 18]);
+%!assert (pdist (x, "minkowski"), ...
+%!        [5.1962, 10.3923, 2.8284, 5.1962, 5.9161, 10.7703], 1e-4);
+%!assert (pdist (x, "minkowski", 3), ...
+%!        [4.3267, 8.6535, 2.5198, 4.3267, 5.3485, 9.2521], 1e-4);
+%!assert (pdist (x, "cosine"), ...
+%!        [0.0254, 0.0406, 0.2857, 0.0018, 0.1472, 0.1173], 1e-4);
+%!assert (pdist (x, "correlation"), [0, 0, 2, 0, 2, 2], 1e-14);
+%!assert (pdist (x, "spearman"), [0, 0, 2, 0, 2, 2], 1e-14);
+%!assert (pdist (x, "hamming"), [1, 1, 2/3, 1, 1, 1]);
+%!assert (pdist (x, "jaccard"), [1, 1, 2/3, 1, 1, 1]);
+%!assert (pdist (x, "chebychev"), [3, 6, 2, 3, 5, 8]);
