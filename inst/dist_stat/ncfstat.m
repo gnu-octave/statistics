850ef9951229a030b1a4eadb401d723967921da7
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-02-08 21:29:24 +0200

    update documentation, input validation and BISTs in ncfstat and nctstat functions

--- inst/dist_stat/ncfstat.m
+++ inst/dist_stat/ncfstat.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2022-2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -16,36 +16,52 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {[@var{m}, @var{v}] =} ncfstat (@var{df1}, @var{df1}, @var{delta})
+## @deftypefn  {statistics} {[@var{m}, @var{v}] =} ncfstat (@var{df1}, @var{df1}, @var{lambda})
 ##
-## Compute statistics for the noncentral @math{F} distribution.
+## Compute statistics for the noncentral @math{F}-distribution.
 ##
-## @code{[@var{m}, @var{v}] = ncfstat (@var{df1}, @var{df1}, @var{delta})}
-## returns the mean and variance of the noncentral F distribution with @var{df1}
-## and @var{df2} degrees of freedom and noncentrality parameter @var{delta}.
+## @code{[@var{m}, @var{v}] = ncfstat (@var{df1}, @var{df1}, @var{lambda})}
+## returns the mean and variance of the noncentral @math{F}-distribution with
+## @var{df1} and @var{df2} degrees of freedom and noncentrality parameter
+## @var{lambda}.
 ##
-## The size of @var{m} and @var{v} is the common size of the input arguments.
-## Scalar input arguments @var{df1}, @var{df2}, and @var{delta} are regarded as
-## constant matrices of the same size as the other input.
+## The size of @var{m} (mean) and @var{v} (variance) is the common size of the
+## input arguments.  A scalar input functions as a constant matrix of the same
+## size as the other inputs.
 ##
-## @seealso{ncfcdf, ncfinv, ncfpdf, ncfrnd}
+## Further information about the noncentral @math{F}-distribution can be found
+## at @url{https://en.wikipedia.org/wiki/Noncentral_F-distribution}
+##
+## @seealso{ncfcdf, ncfinv, ncfpdf, ncfrnd, fstat}
 ## @end deftypefn
 
-function [m, v] = ncfstat (df1, df2, delta)
+function [m, v] = ncfstat (df1, df2, lambda)
 
-  ## Check for valid input arguments
-  if (nargin <  3)
-    error ("ncfstat: too few input arguments.");
+  ## Check for valid number of input arguments
+  if (nargin < 3)
+    error ("ncfstat: function called with too few input arguments.");
   endif
 
-  ## Check and fix size of input arguments
-  [err, df1, df2, delta] = common_size (df1, df2, delta);
-  if (err > 0)
-    error ("ncfstat: input size mismatch.");
+  ## Check for DF1, DF2, and LAMBDA being numeric
+  if (! (isnumeric (df1) && isnumeric (df2) && isnumeric (lambda)))
+    error ("ncfstat: DF1, DF2, and LAMBDA must be numeric.");
+  endif
+
+  ## Check for DF1, DF2, and LAMBDA being reals
+  if (iscomplex (df1) || iscomplex (df2) || iscomplex (lambda))
+    error ("ncfstat: DF1, DF2, and LAMBDA must not be complex.");
+  endif
+
+  ## Check for common size of DF1, DF2, and LAMBDA
+  if (! isscalar (df1) || ! isscalar (df2) || ! isscalar (lambda))
+    [retval, df1, df2, lambda] = common_size (df1, df2, lambda);
+    if (retval > 0)
+      error ("ncfstat: DF1, DF2, and LAMBDA must be of common size or scalars.");
+    endif
   endif
 
   ## Initialize mean and variance
-  if (isa (df1, "single") || isa (df2, "single") || isa (delta, "single"))
+  if (isa (df1, "single") || isa (df2, "single") || isa (lambda, "single"))
     m = zeros (size (df1), "single");
     v = m;
   else
@@ -60,31 +76,42 @@ function [m, v] = ncfstat (df1, df2, delta)
   ## Compute mean and variance for valid parameter values.
   k = (df2 > 2);
   if (any (k(:)))
-    m(k) = df2(k) .* (df1(k) + delta(k)) ./ (df1(k) .* (df2(k) - 2));
+    m(k) = df2(k) .* (df1(k) + lambda(k)) ./ (df1(k) .* (df2(k) - 2));
   endif
   k = (df2 > 4);
   if (any (k(:)))
-    df1_idx = df1(k) + delta(k);
+    df1_idx = df1(k) + lambda(k);
     df2_idx = df2(k) - 2;
     df1_df2 = (df2(k) ./ df1(k)) .^ 2;
-    v(k) = 2 * df1_df2 .* (df1_idx .^ 2 + (df1_idx + delta(k)) .* ...
+    v(k) = 2 * df1_df2 .* (df1_idx .^ 2 + (df1_idx + lambda(k)) .* ...
                            df2_idx) ./ ((df2(k) - 4) .* df2_idx .^ 2);
   endif
 
 endfunction
 
 ## Input validation tests
-%!error<ncfstat: too few input arguments.> p = ncfstat ();
-%!error<ncfstat: too few input arguments.> p = ncfstat (1);
-%!error<ncfstat: too few input arguments.> p = ncfstat (1, 2);
-%!error<ncfstat: input size mismatch.> p = ncfstat (5, [4, 3], [3, 4, 5]);
+%!error<ncfstat: function called with too few input arguments.> ncfstat ()
+%!error<ncfstat: function called with too few input arguments.> ncfstat (1)
+%!error<ncfstat: function called with too few input arguments.> ncfstat (1, 2)
+%!error<ncfstat: DF1, DF2, and LAMBDA must be numeric.> ncfstat ({}, 2, 3)
+%!error<ncfstat: DF1, DF2, and LAMBDA must be numeric.> ncfstat (1, "", 3)
+%!error<ncfstat: DF1, DF2, and LAMBDA must be numeric.> ncfstat (1, 2, "")
+%!error<ncfstat: DF1, DF2, and LAMBDA must not be complex.> ncfstat (i, 2, 3)
+%!error<ncfstat: DF1, DF2, and LAMBDA must not be complex.> ncfstat (1, i, 3)
+%!error<ncfstat: DF1, DF2, and LAMBDA must not be complex.> ncfstat (1, 2, i)
+%!error<ncfstat: DF1, DF2, and LAMBDA must be of common size or scalars.> ...
+%! ncfstat (ones (3), ones (2), 3)
+%!error<ncfstat: DF1, DF2, and LAMBDA must be of common size or scalars.> ...
+%! ncfstat (ones (2), 2, ones (3))
+%!error<ncfstat: DF1, DF2, and LAMBDA must be of common size or scalars.> ...
+%! ncfstat (1, ones (2), ones (3))
 
 ## Output validation tests
-%!shared df1, df2, delta
+%!shared df1, df2, lambda
 %! df1 = [2, 0, -1, 1, 4, 5];
 %! df2 = [2, 4, -1, 5, 6, 7];
-%! delta = [1, NaN, 3, 0, 2, -1];
-%!assert (ncfstat (df1, df2, delta), [NaN, NaN, NaN, 1.6667, 2.25, 1.12], 1e-4);
+%! lambda = [1, NaN, 3, 0, 2, -1];
+%!assert (ncfstat (df1, df2, lambda), [NaN, NaN, NaN, 1.6667, 2.25, 1.12], 1e-4);
 %!assert (ncfstat (df1(4:6), df2(4:6), 1), [3.3333, 1.8750, 1.6800], 1e-4);
 %!assert (ncfstat (df1(4:6), df2(4:6), 2), [5.0000, 2.2500, 1.9600], 1e-4);
 %!assert (ncfstat (df1(4:6), df2(4:6), 3), [6.6667, 2.6250, 2.2400], 1e-4);
--- inst/dist_stat/nctstat.m
+++ inst/dist_stat/nctstat.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2022-2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -16,36 +16,51 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {[@var{m}, @var{v}] =} nctstat (@var{df}, @var{delta})
+## @deftypefn  {statistics} {[@var{m}, @var{v}] =} nctstat (@var{df}, @var{mu})
 ##
-## Compute statistics for the noncentral @math{t} distribution.
+## Compute statistics for the noncentral @math{t}-distribution.
 ##
-## @code{[@var{m}, @var{v}] = nctstat (@var{df}, @var{delta})} returns the mean
-## and variance of the noncentral @math{t} distribution with @var{df} degrees
-## of freedom and noncentrality parameter @var{delta}.
+## @code{[@var{m}, @var{v}] = nctstat (@var{df}, @var{mu})} returns the mean
+## and variance of the noncentral @math{t}-distribution with @var{df} degrees
+## of freedom and noncentrality parameter @var{mu}.
 ##
-## The size of @var{m} and @var{v} is the common size of the input arguments.
-## Scalar input arguments @var{df} and @var{delta} are regarded as constant
-## matrices of the same size as the other input.
+## The size of @var{m} (mean) and @var{v} (variance) is the common size of the
+## input arguments.  A scalar input functions as a constant matrix of the same
+## size as the other inputs.
 ##
-## @seealso{nctcdf, nctinv, nctpdf, nctrnd}
+## Further information about the noncentral @math{t}-distribution can be found
+## at @url{https://en.wikipedia.org/wiki/Noncentral_t-distribution}
+##
+## @seealso{nctcdf, nctinv, nctpdf, nctrnd, tstat}
 ## @end deftypefn
 
-function [m, v] = nctstat (df, delta)
+function [m, v] = nctstat (df, mu)
 
-  ## Check for valid input arguments
-  if (nargin <  2)
-    error ("nctstat: too few input arguments.");
+  ## Check for valid number of input arguments
+  if (nargin < 2)
+    error ("nctstat: function called with too few input arguments.");
   endif
 
-  ## Check and fix size of input arguments
-  [err, df, delta] = common_size (df, delta);
-  if (err > 0)
-    error ("nctstat: input size mismatch.");
+  ## Check for DF and MU being numeric
+  if (! (isnumeric (df) && isnumeric (mu)))
+    error ("nctstat: DF and MU must be numeric.");
+  endif
+
+  ## Check for DF and MU being real
+  if (iscomplex (df) || iscomplex (mu))
+    error ("nctstat: DF and MU must not be complex.");
+  endif
+
+  ## Check for common size of DF and MU
+  if (! isscalar (df) || ! isscalar (mu))
+    [retval, df, mu] = common_size (df, mu);
+    if (retval > 0)
+      error ("nctstat: DF and MU must be of common size or scalars.");
+    endif
   endif
 
   ## Initialize mean and variance
-  if (isa (df, "single") || isa (delta, "single"))
+  if (isa (df, "single") || isa (mu, "single"))
     m = NaN (size (df), "single");
     v = m;
   else
@@ -56,32 +71,39 @@ function [m, v] = nctstat (df, delta)
   ## Compute mean and variance for valid parameter values.
   mk = df > 1;
   if (any (mk(:)))
-    m(mk) = delta(mk) .* sqrt ((df(mk) / 2)) .* ...
+    m(mk) = mu(mk) .* sqrt ((df(mk) / 2)) .* ...
             gamma ((df(mk) - 1) / 2) ./ gamma (df(mk) / 2);
   endif
   vk = df > 2;
   if (any (vk(:)))
     v(vk) = (df(vk) ./ (df(vk) - 2)) .* ...
-            (1 + delta(vk) .^2) - 0.5 * (df(vk) .* delta(vk) .^ 2) .* ...
+            (1 + mu(vk) .^2) - 0.5 * (df(vk) .* mu(vk) .^ 2) .* ...
             exp (2 * (gammaln ((df(vk) - 1) / 2) - gammaln (df(vk) / 2)));
   endif
 
 endfunction
 
 ## Input validation tests
-%!error<nctstat: too few input arguments.> p = nctstat ();
-%!error<nctstat: too few input arguments.> p = nctstat (1);
-%!error<nctstat: input size mismatch.> p = nctstat ([4, 3], [3, 4, 5]);
+%!error<nctstat: function called with too few input arguments.> nctstat ()
+%!error<nctstat: function called with too few input arguments.> nctstat (1)
+%!error<nctstat: DF and MU must be numeric.> nctstat ({}, 2)
+%!error<nctstat: DF and MU must be numeric.> nctstat (1, "")
+%!error<nctstat: DF and MU must not be complex.> nctstat (i, 2)
+%!error<nctstat: DF and MU must not be complex.> nctstat (1, i)
+%!error<nctstat: DF and MU must be of common size or scalars.> ...
+%! nctstat (ones (3), ones (2))
+%!error<nctstat: DF and MU must be of common size or scalars.> ...
+%! nctstat (ones (2), ones (3))
 
 ## Output validation tests
-%!shared df, d1
+%!shared df, mu
 %! df = [2, 0, -1, 1, 4];
-%! d1 = [1, NaN, 3, -1, 2];
-%!assert (nctstat (df, d1), [1.7725, NaN, NaN, NaN, 2.5066], 1e-4);
+%! mu = [1, NaN, 3, -1, 2];
+%!assert (nctstat (df, mu), [1.7725, NaN, NaN, NaN, 2.5066], 1e-4);
 %!assert (nctstat ([df(1:2), df(4:5)], 1), [1.7725, NaN, NaN, 1.2533], 1e-4);
 %!assert (nctstat ([df(1:2), df(4:5)], 3), [5.3174, NaN, NaN, 3.7599], 1e-4);
 %!assert (nctstat ([df(1:2), df(4:5)], 2), [3.5449, NaN, NaN, 2.5066], 1e-4);
-%!assert (nctstat (2, [d1(1), d1(3:5)]), [1.7725,5.3174,-1.7725,3.5449], 1e-4);
-%!assert (nctstat (0, [d1(1), d1(3:5)]), [NaN, NaN, NaN, NaN]);
-%!assert (nctstat (1, [d1(1), d1(3:5)]), [NaN, NaN, NaN, NaN]);
-%!assert (nctstat (4, [d1(1), d1(3:5)]), [1.2533,3.7599,-1.2533,2.5066], 1e-4);
+%!assert (nctstat (2, [mu(1), mu(3:5)]), [1.7725,5.3174,-1.7725,3.5449], 1e-4);
+%!assert (nctstat (0, [mu(1), mu(3:5)]), [NaN, NaN, NaN, NaN]);
+%!assert (nctstat (1, [mu(1), mu(3:5)]), [NaN, NaN, NaN, NaN]);
+%!assert (nctstat (4, [mu(1), mu(3:5)]), [1.2533,3.7599,-1.2533,2.5066], 1e-4);
