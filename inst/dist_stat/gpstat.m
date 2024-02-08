a09effab88ecb506fbf85b71e1a6f8ff097e0093
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-02-08 23:50:32 +0200

    gpstat: update documentation, input validation, BISTs

--- inst/dist_stat/gpstat.m
+++ inst/dist_stat/gpstat.m
@@ -17,91 +17,118 @@
 ## <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {[@var{m}, @var{v}] =} gpstat (@var{shape}, @var{scale}, @var{location})
+## @deftypefn  {statistics} {[@var{m}, @var{v}] =} gpstat (@var{k}, @var{sigma}, @var{mu})
 ##
 ## Compute statistics of the generalized Pareto distribution.
 ##
-## @code{[@var{m}, @var{v}] = gpstat (@var{shape}, @var{scale}, @var{location})}
+## @code{[@var{m}, @var{v}] = gpstat (@var{k}, @var{sigma}, @var{mu})}
 ## returns the mean and variance of the generalized Pareto distribution with
-## @var{shape}, @var{scale}, and @var{location} parameeters.
+## with shape parameter @var{k}, scale parameter @var{sigma}, and location
+## parameter @var{mu}.
 ##
-## The default value for @var{location} is 0.
+## The size of @var{m} (mean) and @var{v} (variance) is the common size of the
+## input arguments.  A scalar input functions as a constant matrix of the
+## same size as the other inputs.
 ##
-## When @var{shape} = 0 and @var{location} = 0, the generalized Pareto
+## When @var{k} = 0 and @var{mu} = 0, the generalized Pareto
 ## distribution is equivalent to the exponential distribution.  When
-## @code{@var{shape} > 0} and @code{@var{location} = @var{scale} / @var{shape}},
+## @code{@var{k} > 0} and @code{@var{mu} = @var{sigma} / @var{k}},
 ## the generalized Pareto distribution is equivalent to the Pareto distribution.
 ## The mean of the generalized Pareto distribution is not finite when
-## @code{@var{shape} >= 1}, and the variance is not finite when
-## @code{@var{shape} >= 1/2}.  When @code{@var{shape} >= 0}, the generalized
-## Pareto distribution has positive density for @code{@var{x} > @var{location}},
-## or, when @code{@var{shape} < 0}, for
-## @code{0 <= (@var{x} -  @var{location}) / @var{scale} <= -1 / @var{shape}}.
+## @code{@var{k} >= 1}, and the variance is not finite when
+## @code{@var{k} >= 1/2}.  When @code{@var{k} >= 0}, the generalized
+## Pareto distribution has positive density for @code{@var{x} > @var{mu}},
+## or, when @code{@var{k} < 0}, for
+## @code{0 <= (@var{x} -  @var{mu}) / @var{sigma} <= -1 / @var{k}}.
+##
+## Further information about the generalized Pareto distribution can be found at
+## @url{https://en.wikipedia.org/wiki/Generalized_Pareto_distribution}
 ##
 ## @seealso{gpcdf, gpinv, gppdf, gprnd, gpfit, gplike}
 ## @end deftypefn
 
-function [m, v] = gpstat (shape, scale, location)
+function [m, v] = gpstat (k, sigma, mu)
 
-  ## Check input arguments
-  if (nargin < 2)
-      error ("gpstat: too few input arguments.");
-  endif
-  if (nargin < 3 || isempty(location))
-    location = 0;
+  ## Check for valid number of input arguments
+  if (nargin < 3)
+    error ("gpstat: function called with too few input arguments.");
   endif
 
-  [err, shape, scalelocationmu] = common_size (shape, scale, location);
-  if (err > 0)
-    error ("gpstat: inputs must be of common size or scalars.");
+  ## Check for K, SIGMA, and MU being numeric
+  if (! (isnumeric (k) && isnumeric (sigma) && isnumeric (mu)))
+    error ("gpstat: K, SIGMA, and MU must be numeric.");
+  endif
+
+  ## Check for K, SIGMA, and MU being real
+  if (iscomplex (k) || iscomplex (sigma) || iscomplex (mu))
+    error ("gpstat: K, SIGMA, and MU must not be complex.");
+  endif
+
+  ## Check for common size of K, SIGMA, and MU
+  if (! isscalar (k) || ! isscalar (sigma) || ! isscalar (mu))
+    [retval, k, sigma, mu] = common_size (k, sigma, mu);
+    if (retval > 0)
+      error ("gpstat: K, SIGMA, and MU must be of common size or scalars.");
+    endif
   endif
 
   ## Return NaNs for out of range SCALE parameters.
-  scale(scale <= 0) = NaN;
+  sigma(sigma <= 0) = NaN;
 
   ## Check for appropriate class
-  if (isa (shape, "single") || isa (scale, "single") || ...
-                               isa (location, "single"));
+  if (isa (k, "single") || isa (sigma, "single") || isa (mu, "single"));
     is_class = "single";
   else
     is_class = "double";
   endif
 
-  ## Check for X, MU, and SIGMA being reals
-  if (iscomplex (shape) || iscomplex (scale) || iscomplex (location))
-    error ("gpstat: SHAPE, SCALE, and LOCATION must not be complex.");
-  endif
-
   ## Prepare output
-  m = NaN (size (shape), is_class);
-  v = NaN (size (shape), is_class);
+  m = NaN (size (k), is_class);
+  v = NaN (size (k), is_class);
 
   ## Compute cases for SHAPE == 0
-  k = (abs (shape) < eps (is_class));
-  m(k) = 1;
-  v(k) = 1;
+  knot0 = (abs (k) < eps (is_class));
+  m(knot0) = 1;
+  v(knot0) = 1;
 
   ## Compute cases for SHAPE != 0
-  k = ! k;
+  knot0 = ! knot0;
   ## SHAPE < 1
-  kk = k & (shape < 1);
-  m(kk) = 1 ./ (1 - shape(kk));
+  kless = knot0 & (k < 1);
+  m(kless) = 1 ./ (1 - k(kless));
   ## SHAPE > 1
-  m(shape >= 1) = Inf;
+  m(k >= 1) = Inf;
   ## SHAPE < 1/2
-  % Find the shape~=0 cases and fill in the variance.
-  kk = k & (shape < 1/2);
-  v(kk) = 1 ./ ((1-shape(kk)).^2 .* (1-2.*shape(kk)));
+  ## Find the k~=0 cases and fill in the variance.
+  kless = knot0 & (k < 1/2);
+  v(kless) = 1 ./ ((1-k(kless)).^2 .* (1-2.*k(kless)));
   ## SHAPE > 1/2
-  v(shape >= 1/2) = Inf;
+  v(k >= 1/2) = Inf;
 
   ## Compute mean and variance
-  m = location + scale .* m;
-  v = scale.^2 .* v;
+  m = mu + sigma .* m;
+  v = sigma .^ 2 .* v;
 
 endfunction
 
-## Test results
+## Input validation tests
+%!error<gpstat: function called with too few input arguments.> gpstat ()
+%!error<gpstat: function called with too few input arguments.> gpstat (1)
+%!error<gpstat: function called with too few input arguments.> gpstat (1, 2)
+%!error<gpstat: K, SIGMA, and MU must be numeric.> gpstat ({}, 2, 3)
+%!error<gpstat: K, SIGMA, and MU must be numeric.> gpstat (1, "", 3)
+%!error<gpstat: K, SIGMA, and MU must be numeric.> gpstat (1, 2, "")
+%!error<gpstat: K, SIGMA, and MU must not be complex.> gpstat (i, 2, 3)
+%!error<gpstat: K, SIGMA, and MU must not be complex.> gpstat (1, i, 3)
+%!error<gpstat: K, SIGMA, and MU must not be complex.> gpstat (1, 2, i)
+%!error<gpstat: K, SIGMA, and MU must be of common size or scalars.> ...
+%! gpstat (ones (3), ones (2), 3)
+%!error<gpstat: K, SIGMA, and MU must be of common size or scalars.> ...
+%! gpstat (ones (2), 2, ones (3))
+%!error<gpstat: K, SIGMA, and MU must be of common size or scalars.> ...
+%! gpstat (1, ones (2), ones (3))
+
+## Output validation tests
 %!shared x, y
 %! x = [-Inf, -1, 0, 1/2, 1, Inf];
 %! y = [0, 0.5, 1, 2, Inf, Inf];
@@ -110,20 +137,7 @@ endfunction
 ## Test class of input preserved
 %!assert (gpstat (single (x), 1, 0), single (y), eps("single"))
 %!assert (gpstat (x, single (1), 0), single (y), eps("single"))
-%!assert (gpstat (x, single (1)), single (y), eps("single"))
 %!assert (gpstat (x, 1, single (0)), single (y), eps("single"))
 %!assert (gpstat (single ([x, NaN]), 1, 0), single ([y, NaN]), eps("single"))
 %!assert (gpstat ([x, NaN], single (1), 0), single ([y, NaN]), eps("single"))
-%!assert (gpstat ([x, NaN], single (1)), single ([y, NaN]), eps("single"))
 %!assert (gpstat ([x, NaN], 1, single (0)), single ([y, NaN]), eps("single"))
-
-## Test input validation
-%!error<gpstat: too few input arguments.> gpstat ()
-%!error<gpstat: too few input arguments.> gpstat (1)
-%!error<gpstat: inputs must be of common size or scalars.> ...
-%! gpstat (1, ones (2), ones (3))
-%!error<gpstat: inputs must be of common size or scalars.> ...
-%! gpstat (rand (3), ones (2), ones (3))
-%!error<gpstat: SHAPE, SCALE, and LOCATION must not be complex.> gpstat (i, 2, 2)
-%!error<gpstat: SHAPE, SCALE, and LOCATION must not be complex.> gpstat (2, i, 2)
-%!error<gpstat: SHAPE, SCALE, and LOCATION must not be complex.> gpstat (2, 2, i)
