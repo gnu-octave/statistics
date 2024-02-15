261faf40a4ca6f1934627ea78b1ebf75e831a3b3
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-02-15 02:09:05 +0200

    new fitting functions for t-location-scale distribution

---
+++ inst/dist_fit/tlsfit.m
@@ -0,0 +1,241 @@
+## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
+##
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {statistics} {@var{paramhat} =} tlsfit (@var{x})
+## @deftypefnx {statistics} {[@var{paramhat}, @var{paramci}] =} tlsfit (@var{x})
+## @deftypefnx {statistics} {[@var{paramhat}, @var{paramci}] =} tlsfit (@var{x}, @var{alpha})
+## @deftypefnx {statistics} {[@dots{}] =} tlsfit (@var{x}, @var{alpha}, @var{censor})
+## @deftypefnx {statistics} {[@dots{}] =} tlsfit (@var{x}, @var{alpha}, @var{censor}, @var{freq})
+## @deftypefnx {statistics} {[@dots{}] =} tlsfit (@var{x}, @var{alpha}, @var{censor}, @var{freq}, @var{options})
+##
+## Estimate parameters and confidence intervals for the Location-scale Student's
+## T distribution.
+##
+## @code{@var{muhat} = tlsfit (@var{x})} returns the maximum likelihood
+## estimates of the parameters of the location-scale T distribution
+## given the data in @var{x}.  @qcode{@var{paramhat}(1)} is the location
+## parameter, @math{mu}, @qcode{@var{paramhat}(2)} is the scale parameter,
+## @math{sigma}, and @qcode{@var{paramhat}(3)} is the degrees of freedom,
+## @math{df}.
+##
+## @code{[@var{paramhat}, @var{paramci}] = tlsfit (@var{x})} returns the 95%
+## confidence intervals for the parameter estimates.
+##
+## @code{[@dots{}] = tlsfit (@var{x}, @var{alpha})} also returns the
+## @qcode{100 * (1 - @var{alpha})} percent confidence intervals for the
+## parameter estimates.  By default, the optional argument @var{alpha} is
+## 0.05 corresponding to 95% confidence intervals.  Pass in @qcode{[]} for
+## @var{alpha} to use the default values.
+##
+## @code{[@dots{}] = tlsfit (@var{x}, @var{alpha}, @var{censor})} accepts a
+## boolean vector, @var{censor}, of the same size as @var{x} with @qcode{1}s for
+## observations that are right-censored and @qcode{0}s for observations that are
+## observed exactly.  By default, or if left empty,
+## @qcode{@var{censor} = zeros (size (@var{x}))}.
+##
+## @code{[@dots{}] = tlsfit (@var{x}, @var{alpha}, @var{censor}, @var{freq})}
+## accepts a frequency vector, @var{freq}, of the same size as @var{x}.
+## @var{freq} typically contains integer frequencies for the corresponding
+## elements in @var{x}, but it can contain any non-integer non-negative values.
+## By default, or if left empty, @qcode{@var{freq} = ones (size (@var{x}))}.
+##
+## @code{[@dots{}] = tlsfit (@dots{}, @var{options})} specifies control
+## parameters for the iterative algorithm used to compute ML estimates with the
+## @code{fminsearch} function.  @var{options} is a structure with the following
+## fields and their default values:
+## @itemize
+## @item @qcode{@var{options}.Display = "off"}
+## @item @qcode{@var{options}.TolX = 1e-6}
+## @end itemize
+##
+## Further information about the location-scale Student's T distribution can be
+## found at @url{https://en.wikipedia.org/wiki/Student%27s_t-distribution#Location-scale_t_distribution}
+##
+## @seealso{tlscdf, tlsinv, tlspdf, tlsrnd, tlslike, tlsstat}
+## @end deftypefn
+
+function [paramhat, paramci] = tlsfit (x, alpha, censor, freq, options)
+
+  ## Check input arguments
+  if (! isvector (x))
+    error ("tlsfit: X must be a vector.");
+  endif
+
+  ## Check alpha
+  if (nargin < 2 || isempty (alpha))
+    alpha = 0.05;
+  else
+    if (! isscalar (alpha) || ! isreal (alpha) || alpha <= 0 || alpha >= 1)
+      error ("tlsfit: wrong value for ALPHA.");
+    endif
+  endif
+
+  ## Check censor vector
+  if (nargin < 3 || isempty (censor))
+    censor = zeros (size (x));
+  elseif (! isequal (size (x), size (censor)))
+    error ("tlsfit: X and CENSOR vectors mismatch.");
+  endif
+
+  ## Check frequency vector
+  if (nargin < 4 || isempty (freq))
+    freq = ones (size (x));
+  elseif (! isequal (size (x), size (freq)))
+    error ("tlsfit: X and FREQ vectors mismatch.");
+  endif
+
+  ## Get options structure or add defaults
+  if (nargin < 5)
+    options.Display = "off";
+    options.TolX = 1e-6;
+  else
+    if (! isstruct (options) || ! isfield (options, "Display") || ...
+                                ! isfield (options, "TolX"))
+      error (strcat (["tlsfit: 'options' 5th argument must be a structure"], ...
+                     [" with 'Display' and 'TolX' fields present."]));
+    endif
+  endif
+
+  ## Starting points as robust estimators for MU and SIGMA, and method
+  ## of moments for DF
+  x_uncensored = x(censor==0);
+  mu = median (x_uncensored);
+  sigma = 1.253 * mad (x_uncensored);
+  mom = kurtosis (x_uncensored);
+  mom(mom < 4) = 4;
+  df = 2 * (2 * mom - 3) / (mom - 3);
+  x0 = [mu, sigma, df];
+
+  ## Minimize negative log-likelihood to estimate parameters
+  f = @(params) tlslike (params, x, censor, freq);
+  [paramhat, ~, err, output] = fminsearch (f, x0, options);
+
+  ## Handle errors
+  if (err == 0)
+    if (output.funcCount >= options.MaxFunEvals)
+      warning (strcat (["tlsfit: maximum number of function"], ...
+                       [" evaluations are exceeded."]));
+    elseif (output.iterations >= options.MaxIter)
+      warning ("tlsfit: maximum number of iterations are exceeded.");
+    endif
+  elseif (err < 0)
+    error ("tlsfit: no solution.");
+  endif
+
+  ## Compute CIs using a log normal approximation for parameters.
+  if (nargout > 1)
+    ## Compute asymptotic covariance
+    [~, acov] = tlslike (paramhat, x, censor, freq);
+    ## Get standard errors
+    stderr = sqrt (diag (acov))';
+    mu_se = stderr(1);
+    sigma_se = stderr(2) ./ paramhat(2);
+    df_se = stderr(3) ./ paramhat(3);
+    ## Apply log transform to SIGMA and DF
+    sigma_log = log (paramhat(2));
+    df_log = log (paramhat(3));
+    ## Compute normal quantiles
+    z = norminv (alpha / 2);
+    ## Compute CI
+    muci = [paramhat(1); paramhat(1)] + [mu_se; mu_se] .* [z; -z];
+    sigmaci = [sigma_log; sigma_log] + [sigma_se; sigma_se] .* [z; -z];
+    dfci = [df_log; df_log] + [df_se; df_se] .* [z; -z];
+    ## Inverse log transform
+    paramci = [muci, exp([sigmaci, dfci])];
+ endif
+
+endfunction
+
+%!demo
+%! ## Sample 3 populations from 3 different location-scale T distibutions
+%! randn ("seed", 1);    # for reproducibility
+%! randg ("seed", 2);    # for reproducibility
+%! r1 = tlsrnd (-4, 3, 1, 2000, 1);
+%! randn ("seed", 3);    # for reproducibility
+%! randg ("seed", 4);    # for reproducibility
+%! r2 = tlsrnd (0, 3, 1, 2000, 1);
+%! randn ("seed", 5);    # for reproducibility
+%! randg ("seed", 6);    # for reproducibility
+%! r3 = tlsrnd (5, 5, 4, 2000, 1);
+%! r = [r1, r2, r3];
+%!
+%! ## Plot them normalized and fix their colors
+%! hist (r, [-21:21], [1, 1, 1]);
+%! h = findobj (gca, "Type", "patch");
+%! set (h(1), "facecolor", "c");
+%! set (h(2), "facecolor", "g");
+%! set (h(3), "facecolor", "r");
+%! ylim ([0, 0.25]);
+%! xlim ([-20, 20]);
+%! hold on
+%!
+%! ## Estimate their lambda parameter
+%! mu_sigma_dfA = tlsfit (r(:,1));
+%! mu_sigma_dfB = tlsfit (r(:,2));
+%! mu_sigma_dfC = tlsfit (r(:,3));
+%!
+%! ## Plot their estimated PDFs
+%! x = [-20:0.1:20];
+%! y = tlspdf (x, mu_sigma_dfA(1), mu_sigma_dfA(2), mu_sigma_dfA(3));
+%! plot (x, y, "-pr");
+%! y = tlspdf (x, mu_sigma_dfB(1), mu_sigma_dfB(2), mu_sigma_dfB(3));
+%! plot (x, y, "-sg");
+%! y = tlspdf (x, mu_sigma_dfC(1), mu_sigma_dfC(2), mu_sigma_dfC(3));
+%! plot (x, y, "-^c");
+%! hold off
+%! legend ({"Normalized HIST of sample 1 with μ=0, σ=2 and df=1", ...
+%!          "Normalized HIST of sample 2 with μ=5, σ=2 and df=1", ...
+%!          "Normalized HIST of sample 3 with μ=3, σ=4 and df=3", ...
+%!          sprintf("PDF for sample 1 with estimated μ=%0.2f, σ=%0.2f, and df=%0.2f", ...
+%!                  mu_sigma_dfA(1), mu_sigma_dfA(2), mu_sigma_dfA(3)), ...
+%!          sprintf("PDF for sample 2 with estimated μ=%0.2f, σ=%0.2f, and df=%0.2f", ...
+%!                  mu_sigma_dfB(1), mu_sigma_dfB(2), mu_sigma_dfB(3)), ...
+%!          sprintf("PDF for sample 3 with estimated μ=%0.2f, σ=%0.2f, and df=%0.2f", ...
+%!                  mu_sigma_dfC(1), mu_sigma_dfC(2), mu_sigma_dfC(3))})
+%! title ("Three population samples from different location-scale T distibutions")
+%! hold off
+
+## Test output
+%!test
+%! x = [-1.2352, -0.2741, 0.1726, 7.4356, 1.0392, 16.4165];
+%! [paramhat, paramci] = tlsfit (x);
+%! paramhat_out = [0.035893, 0.862711, 0.649261];
+%! paramci_out = [-0.949034, 0.154655, 0.181080; 1.02082, 4.812443, 2.327914];
+%! assert (paramhat, paramhat_out, 1e-6);
+%! assert (paramci, paramci_out, 1e-6);
+%!test
+%! x = [-1.2352, -0.2741, 0.1726, 7.4356, 1.0392, 16.4165];
+%! [paramhat, paramci] = tlsfit (x, 0.01);
+%! paramci_out = [-1.2585, 0.0901, 0.1212; 1.3303, 8.2591, 3.4771];
+%! assert (paramci, paramci_out, 1e-4);
+
+## Test input validation
+%!error<tlsfit: X must be a vector.> tlsfit (ones (2,5));
+%!error<tlsfit: wrong value for ALPHA.> tlsfit ([1, 2, 3, 4, 5], 1.2);
+%!error<tlsfit: wrong value for ALPHA.> tlsfit ([1, 2, 3, 4, 5], 0);
+%!error<tlsfit: wrong value for ALPHA.> tlsfit ([1, 2, 3, 4, 5], "alpha");
+%!error<tlsfit: X and CENSOR vectors mismatch.> ...
+%! tlsfit ([1, 2, 3, 4, 5], 0.05, [1 1 0]);
+%!error<tlsfit: X and CENSOR vectors mismatch.> ...
+%! tlsfit ([1, 2, 3, 4, 5], [], [1 1 0 1 1]');
+%!error<tlsfit: X and FREQ vectors mismatch.> ...
+%! tlsfit ([1, 2, 3, 4, 5], 0.05, zeros (1,5), [1 1 0]);
+%!error<tlsfit: X and FREQ vectors mismatch.> ...
+%! tlsfit ([1, 2, 3, 4, 5], [], [], [1 1 0 1 1]');
+%!error<tlsfit: 'options' 5th argument must be a structure> ...
+%! tlsfit ([1, 2, 3, 4, 5], 0.05, [], [], 2);
---
+++ inst/dist_fit/tlslike.m
@@ -0,0 +1,183 @@
+## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+##
+## This file is part of the statistics package for GNU Octave.
+##
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR l PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {statistics} {@var{nlogL} =} tlslike (@var{params}, @var{x})
+## @deftypefnx {statistics} {[@var{nlogL}, @var{acov}] =} tlslike (@var{params}, @var{x})
+## @deftypefnx {statistics} {[@dots{}] =} tlslike (@var{params}, @var{x}, @var{alpha}, @var{censor})
+## @deftypefnx {statistics} {[@dots{}] =} tlslike (@var{params}, @var{x}, @var{alpha}, @var{censor}, @var{freq})
+##
+## Negative log-likelihood for the location-scale Student's T distribution.
+##
+## @code{@var{nlogL} = tlslike (@var{params}, @var{x})} returns the negative
+## log-likelihood of the x in @var{x} corresponding to the location-scale T
+## distribution with (1) location parameter @math{mu}, (2) scale parameter
+## @math{sigma} and (3) degrees of freedom @math{df} given in the three-element
+## vector @var{params}.
+##
+## @code{[@var{nlogL}, @var{acov}] = tlslike (@var{params}, @var{x})} also
+## returns the inverse of Fisher's information matrix, @var{acov}.  If the input
+## parameter values in @var{params} are the maximum likelihood estimates, the
+## diagonal elements of @var{acov} are their asymptotic variances.  @var{acov}
+## is based on the observed Fisher's information, not the expected information.
+##
+## @code{[@dots{}] = tlslike (@var{params}, @var{x}, @var{censor})} accepts a
+## boolean vector, @var{censor}, of the same size as @var{x} with @qcode{1}s for
+## observations that are right-censored and @qcode{0}s for observations that are
+## observed exactly.  By default, or if left empty,
+## @qcode{@var{censor} = zeros (size (@var{x}))}.
+##
+## @code{[@dots{}] = tlslike (@var{params}, @var{x}, @var{censor},
+## @var{freq})} accepts a frequency vector, @var{freq}, of the same size as
+## @var{x}.  @var{freq} typically contains integer frequencies for the
+## corresponding elements in @var{x}, but may contain any non-integer
+## non-negative values.  By default, or if left empty,
+## @qcode{@var{freq} = ones (size (@var{x}))}.
+##
+## Further information about the location-scale Student's T distribution can be
+## found at @url{https://en.wikipedia.org/wiki/Student%27s_t-distribution#Location-scale_t_distribution}
+##
+## @seealso{tlscdf, tlsinv, tlspdf, tlsrnd, tlsfit, tlsstat}
+## @end deftypefn
+
+function [nlogL, acov] = tlslike (params, x, censor, freq)
+
+  ## Check input arguments and add defaults
+  if (nargin < 2)
+    error ("tlslike: too few input arguments.");
+  endif
+  if (numel (params) != 3)
+    error ("tlslike: wrong parameters length.");
+  endif
+  if (! isvector (x))
+    error ("tlslike: X must be a vector.");
+  endif
+  if (nargin < 3 || isempty (censor))
+    censor = zeros (size (x));
+  elseif (! isequal (size (x), size (censor)))
+    error ("tlslike: X and CENSOR vectors mismatch.");
+  endif
+  if (nargin < 4 || isempty (freq))
+    freq = ones (size (x));
+  elseif (isequal (size (x), size (freq)))
+    nulls = find (freq == 0);
+    if (numel (nulls) > 0)
+      x(nulls) = [];
+      censor(nulls) = [];
+      freq(nulls) = [];
+    endif
+  else
+    error ("tlslike: X and FREQ vectors mismatch.");
+  endif
+
+  ## Compute the negative log-likelihood
+  nlogL = tlsnll (x, params, censor, freq);
+
+  ## Compute the negative hessian and invert to get the information matrix
+  if (nargout > 1)
+    ei = zeros (1, 3);
+    ej = zeros (1, 3);
+    nH = zeros (3, 3);
+    dp = (eps ^ (1/4)) .* max (abs (params), 1);
+    for i = 1:3
+      ei(i) = dp(i);
+      for j = 1:(i-1)
+        ej(j) = dp(j);
+        ## Four-point central difference for mixed second partials
+        nH(i,j) = tlsnll (x, params+ei+ej, censor, freq) ...
+                - tlsnll (x, params+ei-ej, censor, freq) ...
+                - tlsnll (x, params-ei+ej, censor, freq) ...
+                + tlsnll (x, params-ei-ej, censor, freq);
+        ej(j) = 0;
+      endfor
+      ## Five-point central difference for pure second partial
+      nH(i,i) = -  tlsnll (x, params+2*ei, censor, freq) ...
+                + 16 * tlsnll (x, params+ei, censor, freq) - 30 * nlogL ...
+                + 16 * tlsnll (x, params-ei, censor, freq) ...
+                - tlsnll (x, params-2*ei, censor, freq);
+      ei(i) = 0;
+    endfor
+
+    ## Fill in the upper triangle
+    nH = nH + triu(nH',1);
+
+    ## Normalize the second differences to get derivative estimates
+    nH = nH ./ (4 .* dp(:) * dp(:)' + diag (8 * dp(:) .^ 2));
+
+    ## Check neg Hessian is positive definite
+    [R, p] = chol (nH);
+    if (p > 0)
+      warning ("tlslike: non positive definite Hessian matrix.");
+      acov = NaN (3);
+      return
+    endif
+
+    ## ACOV estimate is the negative inverse of the Hessian
+    Rinv = inv (R);
+    acov = Rinv * Rinv';
+  endif
+
+endfunction
+
+## Internal function to calculate negative log likelihood for tlslike
+function nlogL = tlsnll (x, params, censor, freq)
+
+  mu = params(1);
+  sigma = params(2);
+  sigma(sigma <= 0) = NaN;
+  df = params(3);
+  df(df <= 0) = NaN;
+
+  z = (x - mu) ./ sigma;
+  w = df + (z .^ 2);
+  logw = log (w);
+
+  L = - 0.5 .* (df + 1) .* logw + gammaln (0.5 .* (df + 1)) ...
+      - gammaln (0.5 .* df) + 0.5 .* df .* log (df) ...
+      - log (sigma) - 0.5 .* log (pi);
+
+  n_censored = sum (freq .* censor);
+  if (n_censored > 0)
+    censored = (censor == 1);
+    if (df < 1e7)   # Use incomplete beta function
+      S_censored = betainc (df ./ w(censored), 0.5 .* df, 0.5) ./ 2;
+      S_censored(z(censored) < 0) = 1 - S_censored(z(censored) < 0);
+    else            # Use a normal approximation
+      S_censored = log(0.5 * erfc(z(censored) ./ sqrt(2)));
+    endif
+    L(censored) = log(S_censored);
+  endif
+  nlogL = - sum (freq .* L);
+
+endfunction
+
+## Test output
+%!test
+%! x = [-1.2352, -0.2741, 0.1726, 7.4356, 1.0392, 16.4165];
+%! [nlogL, acov] = tlslike ([0.035893, 0.862711, 0.649261], x);
+%! acov_out = [0.2525, 0.0670, 0.0288; ...
+%!             0.0670, 0.5724, 0.1786; ...
+%!             0.0288, 0.1786, 0.1789];
+%! assert (nlogL, 17.9979636579, 1e-10);
+%! assert (acov, acov_out, 1e-4);
+
+## Test input validation
+%!error<tlslike: too few input arguments.> tlslike ([12, 15, 1]);
+%!error<tlslike: wrong parameters length.> tlslike ([12, 15], [1:50]);
+%!error<tlslike: X must be a vector.> tlslike ([12, 3, 1], ones (10, 2));
+%!error<tlslike: X and CENSOR> tlslike ([12, 15, 1], [1:50], [1, 2, 3]);
+%!error<tlslike: X and FREQ> tlslike ([12, 15, 1], [1:50], [], [1, 2, 3]);
