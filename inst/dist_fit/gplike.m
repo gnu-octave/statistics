50821c83a25838352d84164dc8b4ca0f1bcb806e
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-02-26 01:18:14 +0200

    gplike: fix acov calculation,, added BISTs

--- inst/dist_fit/gplike.m
+++ inst/dist_fit/gplike.m
@@ -25,10 +25,9 @@
 ##
 ## @code{@var{nlogL} = gplike (@var{params}, @var{x})} returns the negative
 ## log-likelihood of the data in @var{x} corresponding to the generalized Pareto
-## distribution with (1) shape parameter @var{k} and (2) scale parameter
-## @var{sigma} given in the two-element vector @var{params}.  @code{gplike}
-## does not allow a location parameter and it must be assumed known, and
-## subtracted from @var{x} before calling @code{gplike}.
+## distribution with (1) shape parameter @var{k}, (2) scale parameter
+## @var{sigma}, and (3) location parameter @var{theta} given in the
+## three-element vector @var{params}.
 ##
 ## @code{[@var{nlogL}, @var{acov}] = gplike (@var{params}, @var{x})} returns
 ## the inverse of Fisher's information matrix, @var{acov}.  If the input
@@ -58,7 +57,7 @@
 ## @seealso{gpcdf, gpinv, gppdf, gprnd, gpfit, gpstat}
 ## @end deftypefn
 
-function [nlogL, acov] = gplike (params, x, freq=[])
+function [nlogL, acov] = gplike (params, x, freq)
 
   ## Check input arguments
   if (nargin < 2)
@@ -69,11 +68,12 @@ function [nlogL, acov] = gplike (params, x, freq=[])
     error ("gplike: X must be a vector.");
   endif
 
-  if (numel (params) != 2)
-    error ("gplike: PARAMS must be a two-element vector.");
+  if (numel (params) != 3)
+    error ("gplike: PARAMS must be a three-element vector.");
   endif
 
-  if (isempty (freq))
+  ## Parse FREQ argument or add default
+  if (nargin < 3 || isempty (freq))
     freq = ones (size (x));
   elseif (! isequal (size (x), size (freq)))
     error ("gplike: X and FREQ vectors mismatch.");
@@ -90,67 +90,81 @@ function [nlogL, acov] = gplike (params, x, freq=[])
     x = xf;
   endif
 
-  ## Get SHAPE and SCALE parameters
-  shape = params(1);
-  scale = params(2);
-  ## Get sample size and scale x
+  ## Get K, SIGMA, and THETA parameters
+  k = params(1);
+  sigma = params(2);
+  theta = params(3);
+  ## Get sample size and sigma x
   sz = numel (x);
-  z = x ./ scale;
+  z = (x - theta) ./ sigma;
 
-  ## For SHAPE > 0
-  if (abs (shape) > eps)
-    if (shape > 0 || max (z) < -1 / shape)
-      sumLn = sum (log1p (shape .* z));
-      nlogL = sz * log (scale) + (1 + 1 / shape) .* sumLn;
+  ## For K > 0
+  if (abs (k) > eps)
+    if (k > 0 || max (z) < -1 / k)
+      sumLn = sum (log1p (k .* z));
+      nlogL = sz * log (sigma) + (1 + 1 / k) .* sumLn;
       if (nargout > 1)
-        v = z ./ (1 + shape .* z);
-        sumv = sum (v);
-        sumvsq = sum (v .^ 2);
-        nH11 = 2 * sumLn ./ shape ^ 3 - ...
-               2 * sumv ./ shape ^ 2 - (1 + 1 / shape) .* sumvsq;
-        nH12 = (-sumv + (shape + 1) .* sumvsq) ./ scale;
-        nH22 = (-sz + 2 * (shape + 1) .* sumv - ...
-                shape * (shape + 1) .* sumvsq) ./ scale ^ 2;
+        z_kz = z ./ (1 + k .* z);
+        sumv = sum (z_kz);
+        sumvsq = sum (z_kz .^ 2);
+        nH11 = 2 * sumLn ./ k ^ 3 - ...
+               2 * sumv ./ k ^ 2 - (1 + 1 / k) .* sumvsq;
+        nH12 = (-sumv + (k + 1) .* sumvsq) ./ sigma;
+        nH22 = (-sz + 2 * (k + 1) .* sumv - ...
+                k * (k + 1) .* sumvsq) ./ sigma ^ 2;
         acov = [nH22, -nH12; -nH12, nH11] / (nH11 * nH22 - nH12 * nH12);
+        acov = [acov, [0; 0]; 0, 0, 0];
       endif
     else
-      ## The support of the GP when shape<0 is 0 < y < abs(scale/shape)
+      ## The support of the GP when k<0 is 0 < y < abs(sigma/k)
       nlogL = Inf;
       if (nargout > 1)
-        acov = [NaN NaN; NaN NaN];
+        acov = [NaN, NaN, 0; NaN, NaN, 0; 0, 0, 0];
       endif
     endif
-  else # For shape = 0
-    ## Handle limit explicitly to prevent (1/0) * log(1) == Inf*0 == NaN.
-    nlogL = sz*log(scale) + sum(z);
+  else # For k = 0
+    nlogL = sz * log (sigma) + sum (z);
     if (nargout > 1)
       sumz = sum (z);
       sumzsq = sum (z .^ 2);
       sumzcb = sum (z .^ 3);
       nH11 = (2 / 3) * sumzcb - sumzsq;
-      nH12 = (-sumz + sumzsq) ./ scale;
-      nH22 = (-sz + 2 * sumz) ./ scale ^ 2;
+      nH12 = (-sumz + sumzsq) ./ sigma;
+      nH22 = (-sz + 2 * sumz) ./ sigma ^ 2;
       acov = [nH22, -nH12; -nH12, nH11] / (nH11 * nH22 - nH12 * nH12);
+      acov = [acov, [0; 0]; 0, 0, 0];
     endif
   endif
 endfunction
 
 ## Test output
-%!assert (gplike ([2, 3], 4), 3.047536764863501, 1e-14)
-%!assert (gplike ([1, 2], 4), 2.890371757896165, 1e-14)
-%!assert (gplike ([2, 3], [1:10]), 32.57864322725392, 1e-14)
-%!assert (gplike ([2, 3], [1:10], ones (1,10)), 32.57864322725392, 1e-14)
-%!assert (gplike ([1, 2], [1:10]), 31.65666282460443, 1e-14)
-%!assert (gplike ([1, 2], [1:10], ones (1,10)), 31.65666282460443, 1e-14)
-%!assert (gplike ([1, NaN], [1:10]), NaN)
+%!test
+%! k = 0.8937; sigma = 1.3230; theta = 1;
+%! x = [2.2196, 11.9301, 4.3673, 1.0949, 6.5626, ...
+%!      1.2109, 1.8576, 1.0039, 12.7917, 2.2590];
+%! [nlogL, acov] = gplike ([k, sigma, theta], x);
+%! assert (nlogL, 21.736, 1e-3);
+%! assert (acov, [0.7249, -0.7351, 0; -0.7351, 1.3040, 0; 0, 0, 0], 1e-4);
+%!assert (gplike ([2, 3, 0], 4), 3.047536764863501, 1e-14)
+%!assert (gplike ([2, 3, 4], 8), 3.047536764863501, 1e-14)
+%!assert (gplike ([1, 2, 0], 4), 2.890371757896165, 1e-14)
+%!assert (gplike ([1, 2, 4], 8), 2.890371757896165, 1e-14)
+%!assert (gplike ([2, 3, 0], [1:10]), 32.57864322725392, 1e-14)
+%!assert (gplike ([2, 3, 2], [1:10] + 2), 32.57864322725392, 1e-14)
+%!assert (gplike ([2, 3, 0], [1:10], ones (1,10)), 32.57864322725392, 1e-14)
+%!assert (gplike ([1, 2, 0], [1:10]), 31.65666282460443, 1e-14)
+%!assert (gplike ([1, 2, 3], [1:10] + 3), 31.65666282460443, 1e-14)
+%!assert (gplike ([1, 2, 0], [1:10], ones (1,10)), 31.65666282460443, 1e-14)
+%!assert (gplike ([1, NaN, 0], [1:10]), NaN)
 
 ## Test input validation
 %!error<gplike: function called with too few input arguments.> gplike ()
 %!error<gplike: function called with too few input arguments.> gplike (1)
-%!error<gplike: X must be a vector.> gplike ([1, 2], [])
-%!error<gplike: X must be a vector.> gplike ([1, 2], ones (2))
-%!error<gplike: PARAMS must be a two-element vector.> gplike (2, [1:10])
+%!error<gplike: X must be a vector.> gplike ([1, 2, 0], [])
+%!error<gplike: X must be a vector.> gplike ([1, 2, 0], ones (2))
+%!error<gplike: PARAMS must be a three-element vector.> gplike (2, [1:10])
+%!error<gplike: PARAMS must be a three-element vector.> gplike ([2, 3], [1:10])
 %!error<gplike: X and FREQ vectors mismatch.> ...
-%! gplike ([1, 2], ones (10, 1), ones (8,1))
+%! gplike ([1, 2, 0], ones (10, 1), ones (8,1))
 %!error<gplike: FREQ must not contain negative values.> ...
-%! gplike ([1, 2], ones (1, 8), [1 1 1 1 1 1 1 -1])
+%! gplike ([1, 2, 0], ones (1, 8), [1 1 1 1 1 1 1 -1])
