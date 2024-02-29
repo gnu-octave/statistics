c560cdd9ed8e1d8d573ae677887d2c12fa61e7ec
ruchiksonagote <ruchiksonagote2003@gmail.com>
2024-03-01 03:08:35 +0530

    updated glmfit

--- inst/glmfit.m
+++ inst/glmfit.m
@@ -17,28 +17,33 @@
 ## see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {statistics} {@var{beta} =} glmfit (@var{X}, @var{y}, @var{distribution}, @var{link})
-## Perform generalized linear model fitting 
+## @deftypefn {statistics} {@var{b} =} glmfit (@var{X}, @var{y}, 
+## @var{distribution}, @var{link})
+## Perform generalized linear model fitting. 
 ## 
-## This function fits a generalized linear model (GLM) to the given data using 
-## the specified link function and distribution of the response variable.
-## The model is fitted using Iteratively Reweighted Least Sqaures (IRLS).
+## This function fits a generalized linear model (GLM) to the given data 
+## using the specified link function and distribution of the response 
+## variable. The model is fitted using Iteratively Reweighted 
+## Least Squares (IRLS).
 ##
-## @var{X} is an @var{n}-by-@var{p} matrix of predictor variables with @var{n} observations and @var{p} predictions. 
-## @var{y} is an @var{n}-by-1 vector of response varibales.
-## @var{distribution} specifies the distribution of the response variable (e.g., 'poisson'). 
+## @var{X} is an @var{n}-by-@var{p} matrix of predictor variables with 
+## @var{n} observations and @var{p} predictors. 
+## @var{y} is an @var{n}-by-1 vector of response variables.
+## @var{distribution} specifies the distribution of the response variable 
+## (e.g., 'poisson'). 
 ## @var{link} specifies the link function to use (e.g., 'log').
 ##
-## The function returns @var{beta}, the estimated coefficients of the GLM, including 
-## the intercept term as the first element of @var{beta}.
+## The function returns @var{b}, the estimated coefficients of the GLM, 
+## including the intercept term as the first element of @var{b}.
 ##
-## Currently, the function supports only the 'poisson' distribution and 'log' link function.
-## Further expansion is required to support additional distributions and link functions.
+## Currently, the function supports only the 'poisson' distribution 
+## and 'log' link function. Further expansion is required to support
+## additional distributions and link functions.
 ## 
 ## @end deftypefn
 
-function beta = glmfit(X, y, distribution, link)
-    ## check input
+function b = glmfit(X, y, distribution, link)
+    ## Check input
     y = round (y(:));
     if (nargin < 2)
         X = zeros (length (y), 0);
@@ -49,88 +54,80 @@ function beta = glmfit(X, y, distribution, link)
     [my, ny] = size (y);
     [mx, nx] = size (X);
     if (mx != my)
-        error ("glm: X and y must have the same number of observations");
+        error ("glm: X and y must have the same number of observations.");
     endif
 
-    ## add column of ones
+    ## Add column of ones
     X    = [ones(size(X, 1), 1), X]; 
-    ## initialize beta
-    beta = zeros(size(X, 2), 1);  
+    ## Initialize b
+    b = zeros(size(X, 2), 1);  
     max_itr  = 1000;
     tolerance = 1e-6;
-    beta_prev = beta;
+    b_prev = b;
   
+    ## Select functions 
+    [inverse_link_func, working_response_func, diag_matrix_func] = select_func(distribution, link);
+
     for i = 1:max_itr
-        linear_predictor = X * beta;
-        ## give inverse function according to link function
-        mu = inverse_link_function(linear_predictor, link);
-        ## weights for IRLS 
-        z = working_response(distribution, y, linear_predictor, mu);
-        W = diag_matrix(distribution, mu);
-        ## Update beta 
-        beta = (X' * W * X) \ (X' * W * z);
-        ## check for convergence
-        if norm(beta - beta_prev, 2) < tolerance
+        linear_predictor = X * b;
+        ## Give inverse function according to link function
+        mu = inverse_link_func(linear_predictor);
+        ## Weights for IRLS 
+        z = working_response_func(y, linear_predictor, mu);
+        W = diag_matrix_func(mu);
+        ## Update b 
+        b = (X' * W * X) \ (X' * W * z);
+        ## Check for convergence
+        if norm(b - b_prev, 2) < tolerance
             break;
         endif
-        beta_prev = beta;
+        b_prev = b;
     endfor
 
     if (i == max_itr)
-        disp('warning: reached limit');
+        warning('glmfit: reached limit');
     endif
 end
 
-function mu = inverse_link_function(linear_predictor, link)
+function [inverse_link_func, working_response_func, diag_matrix_func] = select_func(distribution, link)
     switch link
         case 'log'
-            mu = exp(linear_predictor);
+            inverse_link_func = @(linear_predictor) exp(linear_predictor);
         ## Add cases for other link functions 
         otherwise
-            error('Unsupported link function');
+            error('Unsupported link function.');
+    endswitch
+
+    switch distribution
+        case 'poisson'
+            working_response_func = @(y, linear_predictor, mu) linear_predictor + (y - mu) ./ mu;
+            diag_matrix_func = @(mu) diag(mu);
+        ## Add other cases
+        otherwise
+            error('Unsupported distribution.');
     endswitch
 end
 
-function z = working_response(distribution, y, linear_predictor, mu)
-    switch distribution
-        case 'poisson'
-            z = linear_predictor + (y - mu) ./ mu;
-        ## add other cases
-        otherwise
-            error('Unsupported distribution');
-    endswitch
-end
-
-function W = diag_matrix(distribution, mu)
-    switch distribution
-        case 'poisson'
-            w = mu;
-            W = diag(w);
-        ## add other cases
-        otherwise
-            error('Unsupported distribution');
-    endswitch
-end
 
 %!test
 %! N = 50;
 %! X = rand(N, 1);
-%! beta_true = [0.4; 1.5]; 
-%! mu_true = exp(beta_true(1) + beta_true(2) * X);
+%! b_true = [0.4; 1.5]; 
+%! mu_true = exp(b_true(1) + b_true(2) * X);
 %! y = poissrnd(mu_true);
 %! distribution = 'poisson';
 %! link = 'log';
-%! beta = glmfit([X], y, distribution, link);
-%! assert(beta(1), beta_true(1), 0.1);
-%! assert(beta(2), beta_true(2), 0.1);
+%! b = glmfit([X], y, distribution, link);
+%! assert(b(1), b_true(1), 0.1);
+%! assert(b(2), b_true(2), 0.1);
 
 %!test
 %! X = (1:20)';
-%! beta_true = [1; 0.1];
-%! mu_true = exp(beta_true(1) + beta_true(2) * X);
+%! b_true = [1; 0.1];
+%! mu_true = exp(b_true(1) + b_true(2) * X);
 %! y = exp(mu_true);
 %! distribution = 'poisson';
 %! link = 'log';
-%! beta = glmfit(X, y, distribution, link);
-%! assert(beta(1), beta_true(1), 0.1);
-%! assert(beta(2), beta_true(2), 0.1);
+%! b = glmfit(X, y, distribution, link);
+%! assert(b(1), b_true(1), 0.1);
+%! assert(b(2), b_true(2), 0.1);
