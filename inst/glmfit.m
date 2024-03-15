0134825b0d1707a74fee40890773c9e69b53e7c3
ruchikasonagote <111609654+ruchikasonagote@users.noreply.github.com>
2024-03-15 17:10:11 +0530

    Update glmfit.m

--- inst/glmfit.m
+++ inst/glmfit.m
@@ -17,74 +17,59 @@
 ## see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {@var{b} =} glmfit (@var{X}, @var{y}, @var{distribution})
-## @deftypefnx {statistics} {@var{b} =} glmfit (@var{X}, @var{y}, @var{distribution},
-## @var{Name}, @var{Value})
-## @deftypefnx  {statistics} {[@var{b}, @var{dev}] =} glmfit (@var{X}, @var{y}, 
-## @var{distribution})
-## @deftypefnx  {statistics} {[@var{b}, @var{dev}] =} glmfit (@var{X}, @var{y}, 
-## @var{distribution}, 
-## @var{Name}, @var{Value})
+## @deftypefn {statistics} {@var{b} =} glmfit (@var{X}, @var{y}, @var{distribution})
+## @deftypefnx {statistics} {@var{b} =} glmfit (@var{X}, @var{y}, @var{distribution},@var{Name}, @var{Value})
+## @deftypefnx {statistics} {[@var{b}, @var{dev}] =} glmfit (@dots{})
 ##
 ## Perform generalized linear model fitting.
 ##
-## This function fits a generalized linear model (GLM) to the given data using
-## the specified link function and distribution of the response variable. 
+## @code{@var{b} = glmfit (@var{X}, @var{y}, @var{distribution})} returns a 
+## coefficient estimates vector, @var{b} for a
+## generalized linear regression model of responses in @var{y} and 
+## predictors in @var{X}, using the @var{distribution}.
 ##
 ## @itemize
-## @item @var{X} is an @math{nxp} matrix of predictor variables with
+## @item @var{X} is an @math{nxp} numeric matrix of predictor variables with
 ## @math{n} observations and @math{p} predictors.
-## @item @var{y} can be:
-##   @itemize
-##   @item An @math{n x 1} vector of responses for all the distributions.
-##   @item An @math{n x 2} matrix where the first column contains the number of successes
-##    and the second column contains the number of trials for the 'binomial' distribution.
-##   @end itemize
+## @item @var{y} is a @math{n x 1} vector of responses for all the distributions. 
+## 'binomial' distribution can have @var{y} as a @math{n x 2} matrix 
+## where the first column contains the number of successes and the
+## second column contains the number of trials.
 ## @item @var{distribution} specifies the distribution of the response variable
 ## (e.g., 'poisson').
 ## @end itemize
 ##
-## @code{@var{b} = glmfit (@var{X}, @var{y}, @var{distribution}, @var{Name},
-## @var{Value})} specifies additional options using @qcode{Name-Value} pair
-## arguments.
+## @code{@var{b} = glmfit (@dots{}, @var{Name}, @var{Value})} 
+## specifies additional options using @qcode{Name-Value} pair arguments.
+## 
 ## @multitable @columnfractions 0.18 0.02 0.8
 ## @headitem @tab @var{Name} @tab @var{Value}
 ##
 ## @item @qcode{"link"} @tab @tab A character vector specifying a link
-## function.
-## You can also specify a custom link as a structure with three fields:
-## Link Function, Derivative Function, Inverse Function   
+## function.  
+##
 ## @item @qcode{"constant"} @tab @tab Specifies whether to 
 ## include a constant term in the model. Options are 
-## @qcode{"on"} (default) or @qcode{"off"}.
+## @var{"on"} (default) or @var{"off"}.
 ## @end multitable
-## 
-## The function returns @var{b}, the estimated coefficients of the GLM,
-## including the intercept term as the first element of @var{b}.
 ##
-## @code{@var{[b, dev]} = glmfit (@var{X}, @var{y}, @var{distribution}, @dots{})} 
-## also returns the deviance of the fit.
+## @code{[@var{b}, @var{dev}] = glmfit (@dots{})} 
+## returns the estimated coefficient vector, @var{b}, as well as 
+## the deviance, @var{dev}, of the fit.
 ##
 ## Supported distributions include 'poisson', 'binomial', and 'normal'. 
-## The function also supports custom link functions provided as function handles.
-##
-## Examples:
-##
-## @example
-## @group
-## X = [1, 2, 3; 1, 3, 5; 1, 5, 7];
-## y = [1; 2; 3];
-## distribution = 'poisson';
-## [b, dev] = glmfit(X, y, distribution, 'link', 'log');
-## @end group
-## @end example
-##
+## Supported link functions include 'identity', 'log', 'logit', 'probit', 
+## 'loglog', 'comploglog', 'reciprocal' and a custom link.
+## Custom link function provided as a structure with three fields:
+## Link Function, Derivative Function, Inverse Function. 
 ## @end deftypefn
 
-function [b,varargout] = GLM(X, y, distribution, varargin)
+function [b,varargout] = glmfit (X, y, distribution, varargin)
   ## Check input
   if nargin < 3
     error ("glmfit: at least three input arguments (X, y, distribution) are required.");
+  elseif mod (nargin - 3, 2) != 0
+    error ("glmfit: Name-Value arguments must be in pairs.");
   endif
   xymissing = (isnan (y) | any (isnan (X), 2));
   y(xymissing) = [];
@@ -96,7 +81,7 @@ function [b,varargout] = GLM(X, y, distribution, varargin)
   endif
 
   ## Check dimensions based on distribution
-  if strcmpi(distribution, 'binomial')
+  if strcmpi (distribution, 'binomial')
     if size(y, 2) > 2
       error("glmfit: for a binomial distribution, y must be an n-by-1 or n-by-2 matrix.");
     endif
@@ -130,14 +115,14 @@ function [b,varargout] = GLM(X, y, distribution, varargin)
           derLinkFunc = linkInput{2};
           invLinkFunc = linkInput{3};
           ## Check for function_handle
-          if ~all (cellfun (@(f) isa (f, 'function_handle'), linkInput))
-            error ("glmfit: custom link functions must be function handles.")
+          if ! all (cellfun (@(f) isa (f, 'function_handle'), linkInput))
+            error ("glmfit: custom link functions must be function handles.");
           endif
-          ## Test the custom functions with a small vector
+          ## Test the custom function with a small vector
           try
-            testInput = rand(5, 1);
-            testOutput = invLinkFunc(testInput);
-            if ~isequal (size (testInput), size (testOutput))
+            testInput = [1; 2; 3; 4; 5];
+            testOutput = invLinkFunc (testInput);
+            if ! isequal (size (testInput), size (testOutput))
               error("glmfit: custom inverse link function must return output of the same size as input.");
             endif
           catch
@@ -165,8 +150,8 @@ function [b,varargout] = GLM(X, y, distribution, varargin)
   endwhile
 
   ## Adjust X based on constant
-  if (any (strcmpi (constant, {"on"})))
-    X = [ones(size (X, 1), 1), X];
+  if (strcmpi (constant, 'on'))
+    X = [ones(mx, 1), X];
   endif
 
   ## Initialize b
@@ -218,26 +203,29 @@ function [b,varargout] = GLM(X, y, distribution, varargin)
   endswitch
 
   options = optimset ('MaxFunEvals', 10000, 'MaxIter', 10000);
-  b = fminsearch(negLogLikelihood, b, options);
+  b = fminsearch (negLogLikelihood, b, options);
   
   if (nargout > 1)
       dev = [];
       switch (tolower (distribution))
         case "poisson"
           p = exp (X * b);
-          epsilon = 1e-6; % A small constant
-          dev = sum(2 * (y .* log((y + epsilon) ./ (p + epsilon)) - (y - p)));
+          eps = 1e-6; 
+          dev = sum (2 * (y .* log ((y + eps) ./ (p + eps)) - (y - p)));
         case "binomial"
-          linear_predictor = X * b;
-          p = exp(linear_predictor) ./ (1 + exp(linear_predictor)); % Predicted probability of success
-          epsilon = 1e-6; % Small constant to prevent log(0)
+          eps = 1e-10; 
           if size (y, 2) == 1
+            successes = y;
             trials = ones(size (y));
           elseif size (y, 2) == 2
+            successes = y(:, 1);
             trials = y(:, 2);
           endif
-          dev = ...
-            -2 * sum(y(:,1) .* log(max(p, epsilon)) + (trials - y(:,1)) .* log(max(1 - p, epsilon)));
+          p_hat = max (min (inverse_link_func (X * b), 1 - eps), eps);
+          p = successes ./ trials;
+          p = max (min (p, 1 - eps), eps);
+          dev = 2 * sum (successes .* log (p ./ p_hat) + (trials - successes) ...
+            .* log ((1 - p) ./ (1 - p_hat)));
         case "normal"
           dev = sum ((y - (X * b)) .^ 2);
       endswitch
@@ -246,34 +234,20 @@ function [b,varargout] = GLM(X, y, distribution, varargin)
 endfunction
 
 %!demo
-%! rand("seed", 42); 
-%! X = rand(100, 1); 
+%! rand ("seed", 1); 
+%! X = rand (100, 1); 
 %! b_true = [0.5; -1.2]; 
-%! mu = exp(beta_true(1) + beta_true(2) * X); 
-%! y = poissrnd(mu); 
-%!
-%! ## Fit a GLM model using the Poisson distribution
-%! b = glmfit(X, y, "poisson");
-%!
-%! ## Display the estimated coefficients
-%! disp("Estimated coefficients (Intercept, Slope):");
-%! disp(b);
-%!
-%! ## Visualize the fitted model against the original data
-%! X_plot = linspace(min(X), max(X), 100)';
-%! mu_est = exp([ones(size(X_plot, 1), 1), X_plot] * b); 
-%!
-%! figure;
-%! ## Original data points
-%! plot(X, y, 'o', 'MarkerFaceColor', 'w'); hold on; 
-%! ## Fitted model curve
-%! plot(X_plot, mu_est, 'r-', 'LineWidth', 2); 
-%! xlabel('Predictor (X)');
-%! ylabel('Response (y)');
-%! title('GLM Poisson Regression Fit');
-%! legend({'Data', 'Fitted model'}, 'Location', 'NorthEast');
-%! hold off;
+%! mu = exp (b_true(1) + b_true(2) * X); 
+%! randp ("seed", 1);
+%! y = poissrnd (mu); 
+%! ## Fit a GLM model using the poisson distribution
+%! [b,dev] = glmfit (X, y, 'poisson');
 
+%!demo
+%! x = [2100 2300 2500 2700 2900 3100 3300 3500 3700 3900 4100 4300]';
+%! n = [48 42 31 34 31 21 23 23 21 16 17 21]';
+%! y = [1 2 0 3 8 8 14 17 19 15 17 21]';
+%! [b,dev] = glmfit (x,[y n],'binomial','Link','probit');
 
 ## Test output
 %!test
@@ -283,21 +257,69 @@ endfunction
 %! mu_true = exp (b_true(1) + b_true(2) * X);
 %! randp ("seed", 1);
 %! y = poissrnd (mu_true);
-%! b = glmfit(X, y, "poisson", "link", "log");
-%! assert(b(1), b_true(1), 0.5);
-%! assert(b(2), b_true(2), 0.5);
+%! b = glmfit (X, y, "poisson", "link", "log");
+%! assert (b(1), b_true(1), 0.5);
+%! assert (b(2), b_true(2), 0.5);
+%!test
+%! rand ("seed", 1);
+%! X1 = rand (50, 1);
+%! X2 = rand (50, 1) * 0.5; 
+%! b_true = [0.4; 1.5; -0.7]; 
+%! mu_true = exp (b_true(1) + b_true(2) * X1 + b_true(3) * X2);
+%! randp ("seed", 1);
+%! y = poissrnd(mu_true);
+%! [b, dev] = glmfit ([X1, X2], y, "poisson", "link", "log");
+%! assert (b(1), b_true(1), 1);
+%! assert (b(2), b_true(2), 1);
+%! assert (b(3), b_true(3), 1);
+%! assert (dev < 60, true); 
 
 ## Test input validation
-%!error glmfit()
-%!error glmfit(rand(5,2))
-%!error glmfit(rand(5,2),rand(5,1))
-%!error glmfit(rand(5,2), rand(5,1), 'poisson', 'link')
-%!error <glmfit: X must be a numeric.> glmfit('abc', rand(6,1), 'poisson')
-%!error <glmfit: y must be a numeric.> glmfit(rand(5,2), 'abc', 'poisson')
-%!error <glmfit: invalid distribution.> glmfit(rand(5,2), rand(5,1), 2)
-%!error <glmfit: invalid parameter name.> glmfit(rand(5,2), rand(5,1), 'poisson', 2, 'log')
-%!error <glmfit: invalid link function.> glmfit(rand(5,2), rand(5,1), 'poisson', 'link', 2)
-%!error <glmfit: X and y must have same number of observations.> glmfit(rand(5,2), rand(6,1), 'poisson')
-%!error <glmfit: unsupported link function.> glmfit(rand(10,2), rand(10,1), 'poisson', 'link', 'inverse')
-%!error <glmfit: unsupported parameter name.> glmfit(rand(10,2), rand(10,1), 'poisson', 'notALink', 'log')
-%!error <glmfit: distribution must be a recognized value.> glmfit(rand(10,2), rand(10,1), 'xyz')
+%!error <glmfit: too few arguments.> ...
+%! glmfit ()
+%!error <glmfit: too few arguments.> ...
+%! glmfit (rand(5,2))
+%!error <glmfit: too few arguments.> ...
+%! glmfit (rand(5,2),rand(5,1))
+%!error <glmfit: too few arguments.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link')
+%!error <glmfit: X must be a numeric.> ...
+%! glmfit ('abc', rand(6,1), 'poisson')
+%!error <glmfit: y must be a numeric.> ...
+%! glmfit (rand(5,2), 'abc', 'poisson')
+%!error <glmfit: invalid distribution.> ...
+%! glmfit (rand(5,2), rand(5,1), 2)
+%!error <glmfit: invalid parameter name.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 2, 'log')
+%!error <glmfit: invalid link function.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', 2)
+%!error <glmfit: X and y must have same number of observations.> ...
+%! glmfit (rand(5,2), rand(6,1), 'poisson')
+%!error <glmfit: unsupported link function.> ...
+%! glmfit (rand(10,2), rand(10,1), 'poisson', 'link', 'inverse')
+%!error <glmfit: unsupported parameter name.> ...
+%! glmfit (rand(10,2), rand(10,1), 'poisson', 'notALink', 'log')
+%!error <glmfit: distribution must be a recognized value.> ...
+%! glmfit (rand(10,2), rand(10,1), 'xyz')
+%!error <glmfit: y must be a vector or n-by-2 matrix for binomial distribution.> ...
+%! glmfit (rand(5,2), rand(5,3), 'binomial')
+%!error <glmfit: constant value should be either 'on' or 'off'.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'constant', 'yes')
+%!error <glmfit: y cannot contain negative values for 'binomial' distribution.> ...
+%! glmfit (rand(5,2), [-1, 2; 2, 3; 3, 4; 4, 5; 5, 6], 'binomial')
+%!error <glmfit: number of successes cannot exceed number of trials.> ...
+%! glmfit (rand(5,2), [randi(10,5,1), randi(9,5,1)], 'binomial')
+%!error <glmfit: y cannot contain negative values for 'poisson' distribution.> ...
+%! glmfit (rand(5,2), [-1; 2; 3; 4; 5], 'poisson')
+%!error <glmfit: custom link function must include three function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {'log'})
+%!error <glmfit: custom link function must include three function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {'log', 'hijy'})
+%!error <glmfit: custom link function must include three function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {'log','dfv','dfgvd'})
+%!error <glmfit: for custom link, all inputs must be function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {@log, 'derivative', @exp})
+%!error <glmfit: for custom link, all inputs must be function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {'derivative', @log, @exp})
+%!error <glmfit: for custom link, all inputs must be function handles.> ...
+%! glmfit (rand(5,2), rand(5,1), 'poisson', 'link', {@log, @exp, 'derivative'})
