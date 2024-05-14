dc5bb8bc5511f889fa70a36f7d0a6d1288988f50
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-05-15 00:57:05 +0300

    histfit: change input handling so that gca is called after input validation, extra functionality to return cellstr of supported distributions similarily to fitdist, update help texinfo

--- inst/histfit.m
+++ inst/histfit.m
@@ -17,39 +17,65 @@
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn  {statistics} {} histfit (@var{x}, @var{nbins})
-## @deftypefnx {statistics} {@var{h} =} histfit (@var{x}, @var{nbins})
+## @deftypefn  {statistics} {} histfit (@var{x})
+## @deftypefnx {statistics} {} histfit (@var{x}, @var{nbins})
+## @deftypefnx {statistics} {} histfit (@var{x}, @var{nbins}, @var{distname})
+## @deftypefnx {statistics} {} histfit (@var{ax}, @dots{})
+## @deftypefnx {statistics} {@var{h} =} histfit (@dots{})
 ##
-## Plot histogram with superimposed fitted normal density.
+## Plot histogram with superimposed distribution fit.
 ##
-## @code{histfit (@var{x}, @var{nbins})} plots a histogram of the values in
-## the vector @var{x} using @var{nbins} bars in the histogram.  With one input
-## argument, @var{nbins} is set to the square root of the number of elements in
-## @var{x}.
+## @code{histfit (@var{x)} plots a histogram of the values in the vector @var{x}
+## using the number of bins equal to the square root of the number of nonmissing
+## elements in @var{x} and superimposes a fitted normal density function.
 ##
-## @code{@var{h} = histfit (@var{x}, @var{nbins})} returns the bins and fitted
-## line handles of the plot in @var{h}.
+## @code{histfit (@var{x}, @var{nbins})} plots a histogram of the values in the
+## vector @var{x} using @var{nbins} number of bins in the histogram and
+## superimposes a fitted normal density function.
 ##
-## Example
+## @code{histfit (@var{x}, @var{nbins}, @var{distname})} plots a histogram of
+## the values in the vector @var{x} using @var{nbins} number of bins in the
+## histogram and superimposes a fitted density function from the distribution
+## specified by @var{distname}.
 ##
-## @example
-## histfit (randn (100, 1))
-## @end example
+## @code{histfit (@var{ax}, @dots{})} uses the axes handle @var{ax} to plot the
+## histogram and the fitted density function onto followed by any of the input
+## argument combinations specified in the previous syntaxes.
 ##
-## @seealso{bar, hist, pareto}
+## @code{@var{h} = histfit (@dots{})} returns a vector of handles @var{h}, where
+## @qcode{@var{h}(1)} is the handle to the histogram and @qcode{@var{h}(1)} is
+## the handle to the density curve.
+##
+## Note: calling @code{fitdist} without any input arguments will return a cell
+## array of character vectors listing all supported distributions.
+##
+## @seealso{bar, hist, normplot, fitdist}
 ## @end deftypefn
 
 function [varargout] = histfit (varargin)
 
-  ## Check for axes handle
+  ## Add list of supported probability distribution objects
+  PDO = {'Beta'; 'BirnbaumSaunders'; 'Burr'; 'Exponential'; 'ExtremeValue'; ...
+         'Gamma'; 'GeneralizedExtremeValue'; 'GeneralizedPareto'; ...
+         'InverseGaussian'; 'Logistic'; 'Loglogistic'; 'Lognormal'; ...
+         'Nakagami'; 'NegativeBinomial'; 'Normal'; 'Poisson'; 'Rayleigh'; ...
+         'Rician'; 'tLocationScale'; 'Weibull'};
+
+  ABBR = {"bisa"; "ev"; "gev"; "gp"; "invg"; "nbin"; "tls"; "wbl"};
+
+  ## Check for zero input arguments
   if (numel (varargin) < 1)
-    error ("histfit: too few input arguments.");
+    varargout{1} = PDO;
+    return
   endif
+
+  ## Check for axes handle
   if (isaxes (varargin{1}))
     ax = varargin{1};
     varargin(1) = [];
+    get_current_axes = false;
   else
-    ax = gca ();
+    get_current_axes = true;
   endif
 
   ## Get data
@@ -82,24 +108,21 @@ function [varargout] = histfit (varargin)
   ## Get distribution
   if (numel (varargin) > 2)
     distname = varargin{3};
-    ## Add list of supported probability distribution objects
-    PDO = {'Beta'; 'BirnbaumSaunders'; 'Burr'; 'Exponential'; ...
-           'ExtremeValue'; 'Gamma'; 'GeneralizedExtremeValue'; ...
-           'GeneralizedPareto'; 'InverseGaussian'; 'Kernel'; 'Logistic'; ...
-           'Loglogistic'; 'Lognormal'; 'Nakagami'; 'NegativeBinomial'; ...
-           'Normal'; 'Poisson'; 'Rayleigh'; 'Rician'; 'tLocationScale'; ...
-           'Weibull'; "bisa"; "ev"; "gev"; "gp"; "invg"; "nbin"; "tls"; "wbl"};
-
     ## Check distribution name
     if (! (ischar (distname) && size (distname, 1) == 1))
       error ("histfit: DISTNAME must be a character vector.");
-    elseif (! any (strcmpi (distname, PDO)))
-      error ("histfit: unrecognized distribution name.");
     elseif (strcmpi (distname, "kernel"))
       error ("histfit: 'Kernel' distribution is not supported yet.");
+    elseif (! (any (strcmpi (distname, PDO)) || any (strcmpi (distname, ABBR))))
+      error ("histfit: unrecognized distribution name.");
     endif
   else
     distname = "normal";
+  endif
+
+  ## Create axes handle (if necessary)
+  if (get_current_axes)
+    ax = gca ();
   endif
 
   ## Plot the histogram
@@ -226,17 +249,24 @@ endfunction
 %! end_unwind_protect
 
 ## Test input validation
-%!error<histfit: too few input arguments.> histfit ();
-%!error<histfit: too few input arguments.> histfit (gca);
-%!error<histfit: X must be a numeric vector of real numbers.> histfit ("wer");
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   ax = axes ("parent", hf);
+%!   fail ("histfit (ax)", "histfit: too few input arguments.");
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!error<histfit: X must be a numeric vector of real numbers.> ...
+%! histfit ('wer')
 %!error<histfit: no data in X.> histfit ([NaN, NaN, NaN]);
 %!error<histfit: NBINS must be a real scalar integer value.> ...
-%! histfit (randn (100, 1), 5.6);
+%! histfit (randn (100, 1), 5.6)
 %!error<histfit: DISTNAME must be a character vector.> ...
-%! histfit (randn (100, 1), 8, 5);
+%! histfit (randn (100, 1), 8, 5)
 %!error<histfit: DISTNAME must be a character vector.> ...
-%! histfit (randn (100, 1), 8, {"normal"});
-%!error<histfit: unrecognized distribution name.> ...
-%! histfit (randn (100, 1), 8, "ASDASDASD");
+%! histfit (randn (100, 1), 8, {'normal'})
 %!error<histfit: 'Kernel' distribution is not supported yet.> ...
-%! histfit (randn (100, 1), 8, "Kernel");
+%! histfit (randn (100, 1), 8, 'Kernel')
+%!error<histfit: unrecognized distribution name.> ...
+%! histfit (randn (100, 1), 8, 'ASDASDASD')
