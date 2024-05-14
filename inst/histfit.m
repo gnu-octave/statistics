fcaa140ee279411a3bb2ec14ee6dcf142bc7b62c
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-05-14 18:53:16 +0300

    histfit: update functionality to handle other distributions

--- inst/histfit.m
+++ inst/histfit.m
@@ -1,5 +1,5 @@
 ## Copyright (C) 2003 Alberto Terruzzi <t-albert@libero.it>
-## Copyright (C) 2022-2023 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2022-2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -39,39 +39,105 @@
 ## @seealso{bar, hist, pareto}
 ## @end deftypefn
 
-function [varargout] = histfit (x, nbins)
+function [varargout] = histfit (varargin)
 
-  if (nargin < 1 || nargin > 2)
-    print_usage;
+  ## Check for axes handle
+  if (numel (varargin) < 1)
+    error ("histfit: too few input arguments.");
+  endif
+  if (isaxes (varargin{1}))
+    ax = varargin{1};
+    varargin(1) = [];
+  else
+    ax = gca ();
   endif
 
-  if (! isnumeric (x) || ! isreal (x) || ! isvector (x) || isscalar (x))
-    error ("histfit: X must be a numeric vector of real numbers.");
+  ## Get data
+  if (numel (varargin) < 1)
+    error ("histfit: too few input arguments.");
+  else
+    x = varargin{1};
+    if (! isnumeric (x) || ! isreal (x) || ! isvector (x) || isscalar (x))
+      error ("histfit: X must be a numeric vector of real numbers.");
+    endif
+    ## Remove missing values
+    x(isnan (x)) = [];
+    xsize = numel (x);
+    ## Check for valid data
+    if (xsize < 1)
+      error ("histfit: no data in X.");
+    endif
   endif
 
-  row = sum (! isnan (x));
-
-  if (nargin < 2)
-    nbins = ceil (sqrt (row));
+  ## Get nbins
+  if (numel (varargin) > 1)
+    nbins = varargin{2};
+    if (! (isreal (nbins) && isscalar (nbins) && fix (nbins) == nbins))
+      error ("histfit: NBINS must be a real scalar integer value.");
+    endif
+  else
+    nbins = ceil (sqrt (xsize));
   endif
 
-  [n, xbin] = hist (x, nbins);
-  if (any (abs (diff (xbin, 2)) > 10 * max (abs (xbin)) * eps))
-    error ("histfit: bins must have uniform width.");
+  ## Get distribution
+  if (numel (varargin) > 2)
+    distname = varargin{3};
+    ## Add list of supported probability distribution objects
+    PDO = {'Beta'; 'BirnbaumSaunders'; 'Burr'; 'Exponential'; ...
+           'ExtremeValue'; 'Gamma'; 'GeneralizedExtremeValue'; ...
+           'GeneralizedPareto'; 'InverseGaussian'; 'Kernel'; 'Logistic'; ...
+           'Loglogistic'; 'Lognormal'; 'Nakagami'; 'NegativeBinomial'; ...
+           'Normal'; 'Poisson'; 'Rayleigh'; 'Rician'; 'tLocationScale'; ...
+           'Weibull'; "bisa"; "ev"; "gev"; "gp"; "invg"; "nbin"; "tls"; "wbl"};
+
+    ## Check distribution name
+    if (! (ischar (distname) && size (distname, 1) == 1))
+      error ("histfit: DISTNAME must be a character vector.");
+    elseif (! any (strcmpi (distname, PDO)))
+      error ("histfit: unrecognized distribution name.");
+    elseif (strcmpi (distname, "kernel"))
+      error ("histfit: 'Kernel' distribution is not supported yet.");
+    endif
+  else
+    distname = "normal";
   endif
 
-  ## Compute mu and sigma parameters
-  mr = mean (x, "omitnan");
-  sr = std (x);
-  ## Evenly spaced samples of the expected range in X
-  x = (-3*sr+mr:0.1*sr:3*sr+mr)';
-  [xb, yb] = bar (xbin, n);
-  y = normpdf (x, mr, sr);
-  binwidth = xbin(2) - xbin(1);
-  ## Necessary normalization to overplot the histogram
-  y = row * y * binwidth;
-  ## Plot density line over histogram.
-  h = plot (xb, yb, ";;b", x, y, ";;r-");
+  ## Plot the histogram
+  if (strcmpi (distname, "poisson"))
+    binwidth = 1;
+    xmin = min (x) - 1;
+    xmax = max (x) + 1;
+    [binsize, bincenter] = hist (x, [xmin:xmax]);
+  else
+    [binsize, bincenter] = hist (x, nbins);
+    binwidth = max (diff (bincenter));
+    xmin = min (x) - binwidth / 2;
+    xmax = max (x) + binwidth / 2;
+  endif
+  h = bar (ax, bincenter, binsize, 1, "facecolor", "b");
+
+  ## Fit distibution to data
+  pd = fitdist (x, distname);
+
+  ## Compute density function
+  if (strcmpi (distname, "poisson"))
+    x = [min(x):max(x)]';
+    y = pdf (pd, x);
+  else
+    x = [xmin:(xmax-xmin)/100:xmax]';
+    y = pdf (pd, x);
+  endif
+
+  ## Normalize density line and overplot the histogram
+  y = xsize * y * binwidth;
+  hold on;
+  if (strcmpi (distname, "poisson"))
+    h(2) = plot (ax, x, y, ";;r-o");
+  else
+    h(2) = plot (ax, x, y, ";;r-");
+  endif
+  xlim ([xmin, xmax]);
+  hold off;
 
   ## Return the plot's handle if requested
   if (nargout == 1)
@@ -81,6 +147,12 @@ endfunction
 
 %!demo
 %! histfit (randn (100, 1))
+
+%!demo
+%! histfit (poissrnd (2, 1000, 1), 10, "Poisson")
+
+%!demo
+%! histfit (betarnd (3, 10, 1000, 1), 10, "beta")
 
 ## Test plotting
 %!test
@@ -107,7 +179,64 @@ endfunction
 %! unwind_protect_cleanup
 %!   close (hf);
 %! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   histfit (randn (100, 1));
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   histfit (poissrnd (2, 1000, 1), 10, "Poisson");
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   histfit (betarnd (3, 10, 1000, 1), 10, "beta");
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   ax = gca ();
+%!   histfit (ax, randn (100, 1));
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   ax = gca ();
+%!   histfit (ax, poissrnd (2, 1000, 1), 10, "Poisson");
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
+%!test
+%! hf = figure ("visible", "off");
+%! unwind_protect
+%!   ax = gca ();
+%!   histfit (ax, betarnd (3, 10, 1000, 1), 10, "beta");
+%! unwind_protect_cleanup
+%!   close (hf);
+%! end_unwind_protect
 
 ## Test input validation
-%!error histfit ();
-%!error histfit ([x',x']);
+%!error<histfit: too few input arguments.> histfit ();
+%!error<histfit: too few input arguments.> histfit (gca);
+%!error<histfit: X must be a numeric vector of real numbers.> histfit ("wer");
+%!error<histfit: no data in X.> histfit ([NaN, NaN, NaN]);
+%!error<histfit: NBINS must be a real scalar integer value.> ...
+%! histfit (randn (100, 1), 5.6);
+%!error<histfit: DISTNAME must be a character vector.> ...
+%! histfit (randn (100, 1), 8, 5);
+%!error<histfit: DISTNAME must be a character vector.> ...
+%! histfit (randn (100, 1), 8, {"normal"});
+%!error<histfit: unrecognized distribution name.> ...
+%! histfit (randn (100, 1), 8, "ASDASDASD");
+%!error<histfit: 'Kernel' distribution is not supported yet.> ...
+%! histfit (randn (100, 1), 8, "Kernel");
