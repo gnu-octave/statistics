1a88e1d068733e87971f9111ff1dab8607243fee
pr0m1th3as <abertsatos@biol.uoa.gr>
2024-08-11 23:32:15 +0300

    __plot__: update functionality to handle 'cdf' PlotType for the plot method in distribution objects. Related issue #129

--- inst/dist_obj/private/__plot__.m
+++ inst/dist_obj/private/__plot__.m
@@ -81,7 +81,7 @@ function h = __plot__ (pd, DistType, varargin)
     case "pdf"
       h = plot_pdf (pd, ax, DistType, Discrete);
     case "cdf"
-
+      h = plot_cdf (pd, ax, DistType, Discrete);
     case "probability"
 
   endswitch
@@ -93,6 +93,34 @@ function x = expand_freq (data, freq)
   for i = 1:numel (freq)
     x = [x, repmat(data(i), 1, freq(i))];
   endfor
+endfunction
+
+function [lb, ub, xmin, xmax] = compute_boundaries (pd)
+  ## Compute moments to determine plot boundaries
+  m = mean (pd);
+  s = std (pd);
+  lb = m - 3 * s;
+  ub = m + 3 * s;
+  xmin = m - 3.5 * s;
+  xmax = m + 3.5 * s;
+
+  ## Fix boundaries for specific distributions
+  PD = {"bino", "bisa", "burr", "exp", "gam", "invg", "logl", ...
+        "logn", "naka", "nbin", "poiss", "rayl", "rice", "wbl"};
+  if (strcmpi (pd.DistributionCode, "beta"))
+    lb = xmin = 0;
+    ub = xmax = 1;
+  elseif (any (strcmpi (pd.DistributionCode, PD)))
+    lb = max (m - 3 * s, 0);
+    xmin = max (m - 3.5 * s, 0);
+  elseif (strcmpi (pd.DistributionCode, "gev"))
+
+  elseif (strcmpi (pd.DistributionCode, "gp"))
+
+  elseif (strcmpi (pd.DistributionCode, "hn"))
+    lb = max (m - 3 * s, m);
+    xmin = max (m - 3.5 * s, m);
+  endif
 endfunction
 
 function h = plot_pdf (pd, ax, DistType, Discrete)
@@ -157,31 +185,8 @@ function h = plot_pdf (pd, ax, DistType, Discrete)
   ## Check for fitted distribution
   if (isempty (pd.InputData)) # fixed parameters, no data
 
-    ## Compute moments to determine plot boundaries
-    m = mean (pd);
-    s = std (pd);
-    lb = m - 3 * s;
-    ub = m + 3 * s;
-    xmin = m - 3.5 * s;
-    xmax = m + 3.5 * s;
-
-    ## Fix boundaries for specific distributions
-    PD = {"bino", "bisa", "burr", "exp", "gam", "invg", "logl", ...
-          "logn", "naka", "nbin", "poiss", "rayl", "rice", "wbl"};
-    if (strcmpi (pd.DistributionCode, "beta"))
-      lb = xmin = 0;
-      ub = xmax = 1;
-    elseif (any (strcmpi (pd.DistributionCode, PD)))
-      lb = max (m - 3 * s, 0);
-      xmin = max (m - 3.5 * s, 0);
-    elseif (strcmpi (pd.DistributionCode, "gev"))
-
-    elseif (strcmpi (pd.DistributionCode, "gp"))
-
-    elseif (strcmpi (pd.DistributionCode, "hn"))
-      lb = max (m - 3 * s, m);
-      xmin = max (m - 3.5 * s, m);
-    endif
+    ## Compute plot boundaries
+    [lb, ub, xmin, xmax] = compute_boundaries (pd);
 
     ## Compute stem or line for PDF
     if (DistType)
@@ -215,13 +220,13 @@ function h = plot_pdf (pd, ax, DistType, Discrete)
     endif
 
     ## Compute the patch or histogram for data
+    xsize = numel (x);
     if (DistType)
       binwidth = 1;
       xmin = min (x) - 1;
       xmax = max (x) + 1;
       [binsize, bincenter] = hist (x, [xmin:xmax]);
     else
-      xsize = numel (x);
       nbins = ceil (sqrt (xsize));
       [binsize, bincenter] = hist (x, nbins);
       binwidth = max (diff (bincenter));
@@ -261,6 +266,148 @@ function h = plot_pdf (pd, ax, DistType, Discrete)
       xlim ([xmin, xmax]);
       xlabel ("Data");
       ylabel ("PDF");
+      hold off;
+    endif
+  endif
+
+endfunction
+
+function h = plot_cdf (pd, ax, DistType, Discrete)
+
+  ## Handle special case of multinomial distribution
+  if (strcmpi (pd.DistributionCode, "mn"))
+    y = pd.ParameterValues';
+    x = [1:numel(y)]';
+    if (Discrete)
+      h = stem (ax, x, y, "color", "b");
+    else
+      h = plot (ax, x, y, ";;b-");
+    endif
+    xlim ([0.5, max(x)+0.5]);
+    xlabel ("Data");
+    ylabel ("Probability");
+    return
+  endif
+
+  ## Handle special case of piecewise linear distribution
+  if (strcmpi (pd.DistributionCode, "pl"))
+    x = pd.ParameterValues(:,1);
+    y = pd.ParameterValues(:,2);
+    h = plot (ax, x, y, ";;b-");
+    xgap = (x(end) - x(1)) * 0.1;
+    xlim ([x(1)-xgap, x(end)+xgap]);
+    xlabel ("Data");
+    ylabel ("Probability");
+    return
+  endif
+
+  ## Handle special case of triangular distribution
+  if (strcmpi (pd.DistributionCode, "tri"))
+    lb = pd.A;
+    ub = pd.C;
+    xmin = lb - (ub - lb) * 0.1;
+    xmax = ub + (ub - lb) * 0.1;
+    x = [lb:(ub-lb)/100:ub]';
+    y = pdf (pd, x);
+    h = plot (ax, x, y, ";;r-", "linewidth", 2);
+    xlim ([xmin, xmax]);
+    xlabel ("Data");
+    ylabel ("PDF");
+    return
+  endif
+
+  ## Handle special case of log-uniform and uniform distributions
+  if (any (strcmpi (pd.DistributionCode, {"logu", "unif"})))
+    lb = pd.Lower;
+    ub = pd.Upper;
+    xmin = lb - (ub - lb) * 0.1;
+    xmax = ub + (ub - lb) * 0.1;
+    x = [lb:(ub-lb)/100:ub]';
+    y = pdf (pd, x);
+    h = plot (ax, x, y, ";;r-", "linewidth", 2);
+    xlim ([xmin, xmax]);
+    xlabel ("Data");
+    ylabel ("PDF");
+    return
+  endif
+
+  ## Compute plot boundaries
+  [lb, ub, xmin, xmax] = compute_boundaries (pd);
+
+  ## Check for fitted distribution
+  if (isempty (pd.InputData)) # fixed parameters, no data
+
+    ## Compute stem or line for PDF
+    if (DistType)
+      x = [floor(lb):ceil(ub)]';
+      p = cdf (pd, x);
+    else
+      x = [lb:(ub-lb)/100:ub]';
+      p = cdf (pd, x);
+    endif
+
+    ## Plot
+    if (Discrete)
+      h = stairs (ax, x, p, "color", "r");
+      xlim ([lb-0.5, ub+0.5]);
+      ylim ([0, 1]);
+      xlabel ("Data");
+      ylabel ("CDF");
+    else
+      h = plot (ax, x, p, ";;r-", "linewidth", 2);
+      xlim ([xmin, xmax]);
+      ylim ([0, 1]);
+      xlabel ("Data");
+      ylabel ("CDF");
+    endif
+
+  else # fitted distribution, data available
+
+    ## Expand frequency vector (if necessary)
+    if (any (pd.InputData.freq != 1))
+      x = expand_freq (pd.InputData.data, pd.InputData.freq);
+    else
+      x = pd.InputData.data;
+    endif
+
+    ## Compute the stairs for data
+    [yy, xx, n, ~, eid] = cdfcalc (x);
+    ## Create vectors for plotting
+    nidx = reshape (repmat (1:n, 2, 1), 2*n, 1);
+    xCDF = [-Inf; xx(nidx); Inf];
+    yCDF = [0; 0; yy(1+nidx)];
+
+    ## Compute stairs or line for CDF
+    if (DistType)
+      x = [min(x):max(x)]';
+      p = cdf (pd, x);
+    else
+      x = [xmin:(xmax-xmin)/100:xmax]';
+      p = cdf (pd, x);
+    endif
+
+    ## Plot
+    if (DistType)
+      h(2) = plot (ax, xCDF, yCDF, ";;b-");
+      hold on;
+      if (Discrete)
+        h(1) = stem (ax, x, p, "color", "r");
+      else
+        h(1) = plot (ax, x, p, ";;r-");
+      endif
+      xlim ([xmin, xmax]);
+      ylim ([0, 1]);
+      xlabel ("Data");
+      ylabel ("CDF");
+      hold off;
+    else
+      h(2) = plot (ax, xCDF, yCDF, ";;b-");
+      hold on;
+      h(1) = plot (ax, x, p, ";;r-", "linewidth", 2);
+      xlim ([xmin, xmax]);
+      ylim ([0, 1]);
+      xlabel ("Data");
+      ylabel ("CDF");
       hold off;
     endif
   endif
