ca3c4eb0b8cd9e062545ae5752c785272a2a5b9c
Swayam Shah <swayamshah66@gmail.com>
2025-04-29 21:37:39 +0530

    fix formatting

--- inst/Clustering/KDTreeSearcher.m
+++ inst/Clustering/KDTreeSearcher.m
@@ -622,7 +622,8 @@ classdef KDTreeSearcher
         farthest = KDTreeSearcher.kdtree_cand_farthest (X, p, nn, dist, ...
                                                          distparam);
         radius = pdist2 (X(farthest,:), p, dist, distparam);
-        if (isfield (node, "right") && (length (nn) < k || p(d) + radius > X(point,d)))
+        if (isfield (node, "right") && ...
+            (length (nn) < k || p(d) + radius > X(point,d)))
           nn = KDTreeSearcher.findkdtree_recur (X, node.right, p, nn, k, ...
                                                  dist, distparam);
         endif
