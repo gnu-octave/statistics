47e7f729426fbf945a05ae58d4ddb0a7feaeeef0
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-09-17 03:19:20 +0300

    HalfNormalDistribution: fix error in negloglik method and update documentation

--- inst/dist_obj/HalfNormalDistribution.m
+++ inst/dist_obj/HalfNormalDistribution.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2024-2025 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -24,14 +24,8 @@ classdef HalfNormalDistribution
   ## A @code{HalfNormalDistribution} object consists of parameters, a model
   ## description, and sample data for a half-normal probability distribution.
   ##
-  ## The half-normal distribution uses the following parameters.
-  ##
-  ## @multitable @columnfractions 0.25 0.48 0.27
-  ## @headitem @var{Parameter} @tab @var{Description} @tab @var{Support}
-  ##
-  ## @item @qcode{mu} @tab Location @tab @math{-Inf < mu < Inf}
-  ## @item @qcode{sigma} @tab Scale @tab @math{sigma > 0}
-  ## @end multitable
+  ## The half-normal distribution is a special case of the folded normal
+  ## distribution.
   ##
   ## There are several ways to create a @code{HalfNormalDistribution} object.
   ##
@@ -39,8 +33,9 @@ classdef HalfNormalDistribution
   ## @item Fit a distribution to data using the @code{fitdist} function.
   ## @item Create a distribution with specified parameter values using the
   ## @code{makedist} function.
-  ## @item Use the constructor @qcode{HalfNormalDistribution (@var{mu}, @var{sigma})}
-  ## to create a half-normal distribution with specified parameter values.
+  ## @item Use the constructor @qcode{HalfNormalDistribution (@var{mu},
+  ## @var{sigma})} to create a half-normal distribution with specified parameter
+  ## values.
   ## @item Use the static method @qcode{HalfNormalDistribution.fit (@var{x},
   ## @var{mu}, @var{freq})} to a distribution to data @var{x}.
   ## @end itemize
@@ -48,23 +43,6 @@ classdef HalfNormalDistribution
   ## It is highly recommended to use @code{fitdist} and @code{makedist}
   ## functions to create probability distribution objects, instead of the
   ## constructor and the aforementioned static method.
-  ##
-  ## A @code{HalfNormalDistribution} object contains the following properties,
-  ## which can be accessed using dot notation.
-  ##
-  ## @multitable @columnfractions 0.25 0.25 0.25 0.25
-  ## @item @qcode{DistributionName} @tab @qcode{DistributionCode} @tab
-  ## @qcode{NumParameters} @tab @qcode{ParameterNames}
-  ## @item @qcode{ParameterDescription} @tab @qcode{ParameterValues} @tab
-  ## @qcode{ParameterValues} @tab @qcode{ParameterCI}
-  ## @item @qcode{ParameterIsFixed} @tab @qcode{Truncation} @tab
-  ## @qcode{IsTruncated} @tab @qcode{InputData}
-  ## @end multitable
-  ##
-  ## A @code{HalfNormalDistribution} object contains the following methods:
-  ## @code{cdf}, @code{icdf}, @code{iqr}, @code{mean}, @code{median},
-  ## @code{negloglik}, @code{paramci}, @code{pdf}, @code{plot}, @code{proflik},
-  ## @code{random}, @code{std}, @code{truncate}, @code{var}.
   ##
   ## Further information about the half-normal distribution can be found at
   ## @url{https://en.wikipedia.org/wiki/Half-normal_distribution}
@@ -74,32 +52,177 @@ classdef HalfNormalDistribution
   ## @end deftypefn
 
   properties (Dependent = true)
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} mu
+    ##
+    ## Location parameter
+    ##
+    ## A real scalar value characterizing the location of the half-normal
+    ## distribution.  You can access the @qcode{mu} property using dot name
+    ## assignment.
+    ##
+    ## @end deftp
     mu
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} sigma
+    ##
+    ## Scale parameter
+    ##
+    ## A positive scalar value characterizing the scale of the half-normal
+    ## distribution. You can access the @qcode{sigma} property using dot name
+    ## assignment.
+    ##
+    ## @end deftp
     sigma
   endproperties
 
   properties (GetAccess = public, Constant = true)
-    CensoringAllowed = false;
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} DistributionName
+    ##
+    ## Probability distribution name
+    ##
+    ## A character vector specifying the name of the probability distribution
+    ## object.  This property is read-only.
+    ##
+    ## @end deftp
     DistributionName = "HalfNormalDistribution";
-    DistributionCode = "hn";
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} NumParameters
+    ##
+    ## Number of parameters
+    ##
+    ## A scalar integer value specifying the number of parameters characterizing
+    ## the probability distribution.  This property is read-only.
+    ##
+    ## @end deftp
     NumParameters = 2;
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} ParameterNames
+    ##
+    ## Names of parameters
+    ##
+    ## A @math{2x1} cell array of character vectors with each element containing
+    ## the name of a distribution parameter.  This property is read-only.
+    ##
+    ## @end deftp
     ParameterNames = {"mu", "sigma"};
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} ParameterDescription
+    ##
+    ## Description of parameters
+    ##
+    ## A @math{2x1} cell array of character vectors with each element containing
+    ## a short description of a distribution parameter.  This property is
+    ## read-only.
+    ##
+    ## @end deftp
     ParameterDescription = {"Location", "Scale"};
   endproperties
 
-  properties (GetAccess = public, Constant = true)
+  properties (GetAccess = public, Constant = true, Hidden)
+    CensoringAllowed = false;
+    DistributionCode = "hn";
     ParameterRange = [-Inf, realmin; Inf, Inf];
     ParameterLogCI = [true, true];
   endproperties
 
-  properties (GetAccess = public , SetAccess = protected)
+  properties (GetAccess = public, SetAccess = protected)
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} ParameterValues
+    ##
+    ## Distribution parameter values
+    ##
+    ## A @math{2x1} numeric vector containing the values of the distribution
+    ## parameters.  This property is read-only.  You can change the distribution
+    ## parameters by assigning new values to the @qcode{a} and @qcode{b}
+    ## properties.
+    ##
+    ## @end deftp
     ParameterValues
-    ParameterCI
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} ParameterCovariance
+    ##
+    ## Covariance matrix of the parameter estimates
+    ##
+    ## A @math{2x2} numeric matrix containing the variance-covariance of the
+    ## parameter estimates.  Diagonal elements contain the variance of each
+    ## estimated parameter and non-diagonal elements contain the covariance
+    ## between the parameter estimates.  The covariance matrix is only
+    ## meaningful when the distribution was fitted to data.  If the distribution
+    ## object was created with fixed parameters, or a parameter of a fitted
+    ## distribution is modified, then all elements of the variance-covariance
+    ## are zero.  This property is read-only.
+    ##
+    ## @end deftp
     ParameterCovariance
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} ParameterIsFixed
+    ##
+    ## Flag for fixed parameters
+    ##
+    ## A @math{1x2} logical vector specifying which parameters are fixed and
+    ## which are estimated.  @qcode{true} values correspond to fixed parameters,
+    ## @qcode{false} values correspond to parameter estimates.  This property is
+    ## read-only.
+    ##
+    ## @end deftp
     ParameterIsFixed
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} Truncation
+    ##
+    ## Truncation interval
+    ##
+    ## A @math{1x2} numeric vector specifying the truncation interval for the
+    ## probability distribution.  First element contains the lower boundary,
+    ## second element contains the upper boundary.  This property is read-only.
+    ## You can only truncate a probability distribution with the
+    ## @qcode{truncate} method.
+    ##
+    ## @end deftp
     Truncation
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} IsTruncated
+    ##
+    ## Flag for truncated probability distribution
+    ##
+    ## A logical scalar value specifying whether a probability distribution is
+    ## truncated or not.  This property is read-only.
+    ##
+    ## @end deftp
     IsTruncated
+
+    ## -*- texinfo -*-
+    ## @deftp {HalfNormalDistribution} {property} InputData
+    ##
+    ## Data used for fitting a probability distribution
+    ##
+    ## A scalar structure containing the following fields:
+    ## @itemize
+    ## @item @qcode{data} : a numeric vector containing the data used for
+    ## distribution fitting.
+    ## @item @qcode{cens} : an empty array, since @qcode{HalfNormalDistribution}
+    ## does not allow censoring.
+    ## @item @qcode{frequency} : a numeric vector of non-negative integer values
+    ## containing the frequency information corresponding to the elements of the
+    ## data used for distribution fitting.  If no frequency vector was used for
+    ## distribution fitting, then this field defaults to an empty array.
+    ## @end itemize
+    ##
+    ## @end deftp
     InputData
+  endproperties
+
+  properties (GetAccess = public, SetAccess = protected, Hidden)
+    ParameterCI
   endproperties
 
   methods (Hidden)
@@ -305,8 +428,8 @@ classdef HalfNormalDistribution
         nlogL = [];
         return
       endif
-      nlogL = - hnlike ([this.mu, this.sigma], this.InputData.data, ...
-                        this.InputData.cens, this.InputData.freq);
+      nlogL = - hnlike ([this.mu, this.sigma], ...
+                        this.InputData.data, this.InputData.freq);
     endfunction
 
     ## -*- texinfo -*-
