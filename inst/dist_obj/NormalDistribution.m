b76dbaad954728639258de969cdc9cc7027dcd3f
Swayam Shah <swayamshah66@gmail.com>
2025-10-08 20:40:38 +0530

    1

--- inst/dist_obj/NormalDistribution.m
+++ inst/dist_obj/NormalDistribution.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2024 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2025 Swayam Shah <swayamshah66@gmail.com>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
@@ -17,89 +18,216 @@
 
 classdef NormalDistribution
   ## -*- texinfo -*-
-  ## @deftypefn {statistics} NormalDistribution
+  ## @deftp {statistics} NormalDistribution
   ##
   ## Normal probability distribution object.
   ##
   ## A @code{NormalDistribution} object consists of parameters, a model
   ## description, and sample data for a normal probability distribution.
   ##
-  ## The normal distribution uses the following parameters.
-  ##
-  ## @multitable @columnfractions 0.25 0.48 0.27
-  ## @headitem @var{Parameter} @tab @var{Description} @tab @var{Support}
-  ##
-  ## @item @qcode{mu} @tab Mean @tab @math{-Inf < mu < Inf}
-  ## @item @qcode{sigma} @tab Standard deviation @tab @math{sigma > 0}
-  ## @end multitable
+  ## The normal distribution is a continuous probability distribution that is
+  ## symmetric about the mean, @var{mu}, showing that data near the mean are
+  ## more frequent in occurrence than data far from the mean. It is defined by
+  ## location parameter @var{mu} and scale parameter @var{sigma}.
   ##
   ## There are several ways to create a @code{NormalDistribution} object.
   ##
   ## @itemize
   ## @item Fit a distribution to data using the @code{fitdist} function.
-  ## @item Create a distribution with specified parameter values using the
+  ## @item Create a distribution with fixed parameter values using the
   ## @code{makedist} function.
   ## @item Use the constructor @qcode{NormalDistribution (@var{mu}, @var{sigma})}
-  ## to create a normal distribution with specified parameter values.
+  ## to create a normal distribution with fixed parameter values @var{mu} and
+  ## @var{sigma}.
   ## @item Use the static method @qcode{NormalDistribution.fit (@var{x},
-  ## @var{censor}, @var{freq}, @var{options})} to a distribution to data @var{x}.
+  ## @var{censor}, @var{freq}, @var{options})} to fit a distribution to data
+  ## @var{x}.
   ## @end itemize
   ##
   ## It is highly recommended to use @code{fitdist} and @code{makedist}
-  ## functions to create probability distribution objects, instead of the
-  ## constructor and the aforementioned static method.
-  ##
-  ## A @code{NormalDistribution} object contains the following properties,
-  ## which can be accessed using dot notation.
-  ##
-  ## @multitable @columnfractions 0.25 0.25 0.25 0.25
-  ## @item @qcode{DistributionName} @tab @qcode{DistributionCode} @tab
-  ## @qcode{NumParameters} @tab @qcode{ParameterNames}
-  ## @item @qcode{ParameterDescription} @tab @qcode{ParameterValues} @tab
-  ## @qcode{ParameterValues} @tab @qcode{ParameterCI}
-  ## @item @qcode{ParameterIsFixed} @tab @qcode{Truncation} @tab
-  ## @qcode{IsTruncated} @tab @qcode{InputData}
-  ## @end multitable
-  ##
-  ## A @code{NormalDistribution} object contains the following methods:
-  ## @code{cdf}, @code{icdf}, @code{iqr}, @code{mean}, @code{median},
-  ## @code{negloglik}, @code{paramci}, @code{pdf}, @code{plot}, @code{proflik},
-  ## @code{random}, @code{std}, @code{truncate}, @code{var}.
+  ## functions to create probability distribution objects, instead of the class
+  ## constructor or the aforementioned static method.
   ##
   ## Further information about the normal distribution can be found at
   ## @url{https://en.wikipedia.org/wiki/Normal_distribution}
   ##
   ## @seealso{fitdist, makedist, normcdf, norminv, normpdf, normrnd, normfit,
   ## normlike, normstat}
-  ## @end deftypefn
+  ## @end deftp
 
   properties (Dependent = true)
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} mu
+    ##
+    ## Location parameter
+    ##
+    ## A scalar value characterizing the location of the normal distribution.
+    ## You can access the @qcode{mu} property using dot name assignment.
+    ##
+    ## @end deftp
     mu
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} sigma
+    ##
+    ## Scale parameter
+    ##
+    ## A positive scalar value characterizing the scale of the normal
+    ## distribution. You can access the @qcode{sigma} property using dot name
+    ## assignment.
+    ##
+    ## @end deftp
     sigma
   endproperties
 
   properties (GetAccess = public, Constant = true)
-    CensoringAllowed = true;
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} DistributionName
+    ##
+    ## Probability distribution name
+    ##
+    ## A character vector specifying the name of the probability distribution
+    ## object. This property is read-only.
+    ##
+    ## @end deftp
     DistributionName = "NormalDistribution";
-    DistributionCode = "norm";
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} NumParameters
+    ##
+    ## Number of parameters
+    ##
+    ## A scalar integer value specifying the number of parameters characterizing
+    ## the probability distribution. This property is read-only.
+    ##
+    ## @end deftp
     NumParameters = 2;
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} ParameterNames
+    ##
+    ## Names of parameters
+    ##
+    ## A @math{2x1} cell array of character vectors with each element containing
+    ## the name of a distribution parameter. This property is read-only.
+    ##
+    ## @end deftp
     ParameterNames = {"mu", "sigma"};
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} ParameterDescription
+    ##
+    ## Description of parameters
+    ##
+    ## A @math{2x1} cell array of character vectors with each element containing
+    ## a short description of a distribution parameter. This property is
+    ## read-only.
+    ##
+    ## @end deftp
     ParameterDescription = {"Mean", "Standard Deviation"};
   endproperties
 
-  properties (GetAccess = public, Constant = true)
+  properties (GetAccess = public, Constant = true, Hidden)
+    CensoringAllowed = true;
+    DistributionCode = "norm";
     ParameterRange = [-Inf, realmin; Inf, Inf];
     ParameterLogCI = [true, true];
   endproperties
 
-  properties (GetAccess = public , SetAccess = protected)
+  properties (GetAccess = public, SetAccess = protected)
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} ParameterValues
+    ##
+    ## Distribution parameter values
+    ##
+    ## A @math{2x1} numeric vector containing the values of the distribution
+    ## parameters. This property is read-only. You can change the distribution
+    ## parameters by assigning new values to the @qcode{mu} and @qcode{sigma}
+    ## properties.
+    ##
+    ## @end deftp
     ParameterValues
-    ParameterCI
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} ParameterCovariance
+    ##
+    ## Covariance matrix of the parameter estimates
+    ##
+    ## A @math{2x2} numeric matrix containing the variance-covariance of the
+    ## parameter estimates. Diagonal elements contain the variance of each
+    ## estimated parameter, and non-diagonal elements contain the covariance
+    ## between the parameter estimates. The covariance matrix is only meaningful
+    ## when the distribution was fitted to data. If the distribution object was
+    ## created with fixed parameters, or a parameter of a fitted distribution is
+    ## modified, then all elements of the variance-covariance are zero. This
+    ## property is read-only.
+    ##
+    ## @end deftp
     ParameterCovariance
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} ParameterIsFixed
+    ##
+    ## Flag for fixed parameters
+    ##
+    ## A @math{1x2} logical vector specifying which parameters are fixed and
+    ## which are estimated. @qcode{true} values correspond to fixed parameters,
+    ## @qcode{false} values correspond to parameter estimates. This property is
+    ## read-only.
+    ##
+    ## @end deftp
     ParameterIsFixed
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} Truncation
+    ##
+    ## Truncation interval
+    ##
+    ## A @math{1x2} numeric vector specifying the truncation interval for the
+    ## probability distribution. First element contains the lower boundary,
+    ## second element contains the upper boundary. This property is read-only.
+    ## You can only truncate a probability distribution with the
+    ## @qcode{truncate} method.
+    ##
+    ## @end deftp
     Truncation
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} IsTruncated
+    ##
+    ## Flag for truncated probability distribution
+    ##
+    ## A logical scalar value specifying whether a probability distribution is
+    ## truncated or not. This property is read-only.
+    ##
+    ## @end deftp
     IsTruncated
+
+    ## -*- texinfo -*-
+    ## @deftp {NormalDistribution} {property} InputData
+    ##
+    ## Data used for fitting a probability distribution
+    ##
+    ## A scalar structure containing the following fields:
+    ## @itemize
+    ## @item @qcode{data}: a numeric vector containing the data used for
+    ## distribution fitting.
+    ## @item @qcode{cens}: a numeric vector of logical values indicating
+    ## censoring information corresponding to the elements of the data used for
+    ## distribution fitting. If no censoring vector was used for distribution
+    ## fitting, then this field defaults to an empty array.
+    ## @item @qcode{freq}: a numeric vector of non-negative integer values
+    ## containing the frequency information corresponding to the elements of the
+    ## data used for distribution fitting. If no frequency vector was used for
+    ## distribution fitting, then this field defaults to an empty array.
+    ## @end itemize
+    ##
+    ## @end deftp
     InputData
+  endproperties
+
+  properties (GetAccess = public, SetAccess = protected, Hidden)
+    ParameterCI
   endproperties
 
   methods (Hidden)
@@ -200,13 +328,13 @@ classdef NormalDistribution
     endfunction
 
     ## -*- texinfo -*-
-    ## @deftypefn  {NormalDistribution} {@var{p} =} icdf (@var{pd}, @var{p})
+    ## @deftypefn  {NormalDistribution} {@var{x} =} icdf (@var{pd}, @var{p})
     ##
     ## Compute the inverse cumulative distribution function (iCDF).
     ##
-    ## @code{@var{p} = icdf (@var{pd}, @var{x})} computes the quantile (the
+    ## @code{@var{x} = icdf (@var{pd}, @var{p})} computes the quantile (the
     ## inverse of the CDF) of the probability distribution object, @var{pd},
-    ## evaluated at the values in @var{x}.
+    ## evaluated at the values in @var{p}.
     ##
     ## @end deftypefn
     function x = icdf (this, p)
@@ -293,8 +421,8 @@ classdef NormalDistribution
     ##
     ## Compute the negative loglikelihood of a probability distribution.
     ##
-    ## @code{@var{m} = negloglik (@var{pd})} computes the negative loglikelihood
-    ## of the probability distribution object, @var{pd}.
+    ## @code{@var{nlogL} = negloglik (@var{pd})} computes the negative
+    ## loglikelihood of the probability distribution object, @var{pd}.
     ##
     ## @end deftypefn
     function nlogL = negloglik (this)
@@ -319,20 +447,20 @@ classdef NormalDistribution
     ## boundaries of the 95% confidence interval for each parameter of the
     ## probability distribution object, @var{pd}.
     ##
-    ## @code{@var{ci} = paramci (@var{pd}, @var{Name}, @var{Value})} computes the
-    ## confidence intervals with additional options specified specified by
+    ## @code{@var{ci} = paramci (@var{pd}, @var{Name}, @var{Value})} computes
+    ## the confidence intervals with additional options specified by
     ## @qcode{Name-Value} pair arguments listed below.
     ##
     ## @multitable @columnfractions 0.18 0.02 0.8
     ## @headitem @var{Name} @tab @tab @var{Value}
     ##
     ## @item @qcode{"Alpha"} @tab @tab A scalar value in the range @math{(0,1)}
-    ## specifying the significance level for the confidence interval.  The
+    ## specifying the significance level for the confidence interval. The
     ## default value 0.05 corresponds to a 95% confidence interval.
     ##
     ## @item @qcode{"Parameter"} @tab @tab A character vector or a cell array of
     ## character vectors specifying the parameter names for which to compute
-    ## confidence intervals.  By default, @code{paramci} computes confidence
+    ## confidence intervals. By default, @code{paramci} computes confidence
     ## intervals for all distribution parameters.
     ## @end multitable
     ##
@@ -383,8 +511,8 @@ classdef NormalDistribution
     ##
     ## Plot a probability distribution object.
     ##
-    ## @code{plot (@var{pd}} plots a probability density function (PDF) of the
-    ## probability distribution object @var{pd}.  If @var{pd} contains data,
+    ## @code{plot (@var{pd})} plots a probability density function (PDF) of the
+    ## probability distribution object @var{pd}. If @var{pd} contains data,
     ## which have been fitted by @code{fitdist}, the PDF is superimposed over a
     ## histogram of the data.
     ##
@@ -392,25 +520,25 @@ classdef NormalDistribution
     ## options with the @qcode{Name-Value} pair arguments listed below.
     ##
     ## @multitable @columnfractions 0.18 0.02 0.8
-    ## @headitem @tab @var{Name} @tab @var{Value}
+    ## @headitem @var{Name} @tab @tab @var{Value}
     ##
     ## @item @qcode{"PlotType"} @tab @tab A character vector specifying the plot
-    ## type.  @qcode{"pdf"} plots the probability density function (PDF).  When
+    ## type. @qcode{"pdf"} plots the probability density function (PDF). When
     ## @var{pd} is fit to data, the PDF is superimposed on a histogram of the
-    ## data.  @qcode{"cdf"} plots the cumulative density function (CDF).  When
+    ## data. @qcode{"cdf"} plots the cumulative density function (CDF). When
     ## @var{pd} is fit to data, the CDF is superimposed over an empirical CDF.
     ## @qcode{"probability"} plots a probability plot using a CDF of the data
-    ## and a CDF of the fitted probability distribution.  This option is
+    ## and a CDF of the fitted probability distribution. This option is
     ## available only when @var{pd} is fitted to data.
     ##
     ## @item @qcode{"Discrete"} @tab @tab A logical scalar to specify whether to
     ## plot the PDF or CDF of a discrete distribution object as a line plot or a
-    ## stem plot, by specifying @qcode{false} or @qcode{true}, respectively.  By
+    ## stem plot, by specifying @qcode{false} or @qcode{true}, respectively. By
     ## default, it is @qcode{true} for discrete distributions and @qcode{false}
-    ## for continuous distributions.  When @var{pd} is a continuous distribution
+    ## for continuous distributions. When @var{pd} is a continuous distribution
     ## object, option is ignored.
     ##
-    ## @item @qcode{"Parent"} @tab @tab An axes graphics object for plot.  If
+    ## @item @qcode{"Parent"} @tab @tab An axes graphics object for plot. If
     ## not specified, the @code{plot} function plots into the current axes or
     ## creates a new axes object if one does not exist.
     ## @end multitable
@@ -440,9 +568,9 @@ classdef NormalDistribution
     ## @code{[@var{nlogL}, @var{param}] = proflik (@var{pd}, @var{pnum})}
     ## returns a vector @var{nlogL} of negative loglikelihood values and a
     ## vector @var{param} of corresponding parameter values for the parameter in
-    ## the position indicated by @var{pnum}.  By default, @code{proflik} uses
+    ## the position indicated by @var{pnum}. By default, @code{proflik} uses
     ## the lower and upper bounds of the 95% confidence interval and computes
-    ## 100 equispaced values for the selected parameter.  @var{pd} must be
+    ## 100 equispaced values for the selected parameter. @var{pd} must be
     ## fitted to data.
     ##
     ## @code{[@var{nlogL}, @var{param}] = proflik (@var{pd}, @var{pnum},
@@ -458,11 +586,11 @@ classdef NormalDistribution
     ##
     ## For the normal distribution, @qcode{@var{pnum} = 1} selects the
     ## parameter @qcode{mu} and @qcode{@var{pnum} = 2} selects the parameter
-    ## @var{sigma}.
+    ## @qcode{sigma}.
     ##
     ## When opted to display the profile likelihood plot, @code{proflik} also
     ## plots the baseline loglikelihood computed at the lower bound of the 95%
-    ## confidence interval and estimated maximum likelihood.  The latter might
+    ## confidence interval and estimated maximum likelihood. The latter might
     ## not be observable if it is outside of the used-defined range of parameter
     ## values.
     ##
@@ -478,21 +606,21 @@ classdef NormalDistribution
     endfunction
 
     ## -*- texinfo -*-
-    ## @deftypefn  {NormalDistribution} {@var{y} =} random (@var{pd})
-    ## @deftypefnx {NormalDistribution} {@var{y} =} random (@var{pd}, @var{rows})
-    ## @deftypefnx {NormalDistribution} {@var{y} =} random (@var{pd}, @var{rows}, @var{cols}, @dots{})
-    ## @deftypefnx {NormalDistribution} {@var{y} =} random (@var{pd}, [@var{sz}])
+    ## @deftypefn  {NormalDistribution} {@var{r} =} random (@var{pd})
+    ## @deftypefnx {NormalDistribution} {@var{r} =} random (@var{pd}, @var{rows})
+    ## @deftypefnx {NormalDistribution} {@var{r} =} random (@var{pd}, @var{rows}, @var{cols}, @dots{})
+    ## @deftypefnx {NormalDistribution} {@var{r} =} random (@var{pd}, [@var{sz}])
     ##
     ## Generate random arrays from the probability distribution object.
     ##
     ## @code{@var{r} = random (@var{pd})} returns a random number from the
     ## distribution object @var{pd}.
     ##
-    ## When called with a single size argument, @code{betarnd} returns a square
-    ## matrix with the dimension specified.  When called with more than one
+    ## When called with a single size argument, @code{normrnd} returns a square
+    ## matrix with the dimension specified. When called with more than one
     ## scalar argument, the first two arguments are taken as the number of rows
     ## and columns and any further arguments specify additional matrix
-    ## dimensions.  The size may also be specified with a row vector of
+    ## dimensions. The size may also be specified with a row vector of
     ## dimensions, @var{sz}.
     ##
     ## @end deftypefn
@@ -532,13 +660,14 @@ classdef NormalDistribution
     ##
     ## Truncate a probability distribution.
     ##
-    ## @code{@var{t} = truncate (@var{pd})} returns a probability distribution
-    ## @var{t}, which is the probability distribution @var{pd} truncated to the
-    ## specified interval with lower limit, @var{lower}, and upper limit,
-    ## @var{upper}.  If @var{pd} is fitted to data with @code{fitdist}, the
-    ## returned probability distribution @var{t} is not fitted, does not contain
-    ## any data or estimated values, and it is as it has been created with the
-    ## @var{makedist} function, but it includes the truncation interval.
+    ## @code{@var{t} = truncate (@var{pd}, @var{lower}, @var{upper})} returns a
+    ## probability distribution @var{t}, which is the probability distribution
+    ## @var{pd} truncated to the specified interval with lower limit,
+    ## @var{lower}, and upper limit, @var{upper}. If @var{pd} is fitted to data
+    ## with @code{fitdist}, the returned probability distribution @var{t} is not
+    ## fitted, does not contain any data or estimated values, and it is as it
+    ## has been created with the @var{makedist} function, but it includes the
+    ## truncation interval.
     ##
     ## @end deftypefn
     function this = truncate (this, lower, upper)
@@ -562,7 +691,7 @@ classdef NormalDistribution
     ##
     ## Compute the variance of a probability distribution.
     ##
-    ## @code{@var{v} = var (@var{pd})} computes the standard deviation of the
+    ## @code{@var{v} = var (@var{pd})} computes the variance of the
     ## probability distribution object, @var{pd}.
     ##
     ## @end deftypefn
