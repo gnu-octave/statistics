058833e1d5e2f0943a1e1c94d7a416772e770c33
Swayam Shah <swayamshah66@gmail.com>
2025-12-06 02:22:04 +0530

    2

--- inst/x2fx.m
+++ inst/x2fx.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2025 Swayam Shah <swayamshah66@gmail.com>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
