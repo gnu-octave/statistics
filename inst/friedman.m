49d4eeecaf992cbfb672beae461400abede7619a
Swayam Shah <swayamshah66@gmail.com>
2025-12-05 18:45:09 +0530

    7

--- inst/friedman.m
+++ inst/friedman.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2022 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2025 Swayam Shah <swayamshah66@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
