444485fb216e84783508f5dead9b4b3de52025a7
AvanishSalunke <avanishsalunke16@gmail.com>
2026-01-11 15:38:51 +0000

    Fixes in datasample.m file

--- inst/datasample.m
+++ inst/datasample.m
@@ -152,6 +152,16 @@ function [y, idcs] = datasample (data, k, varargin)
       endfor
     else
       ## complex case
+      if (k > imax)
+        error (strcat ("datasample: Sample size K must be less than or", ...
+                       " equal to the size of the dimension from which to", ...
+                       " sample for sampling without replacement."));
+      endif
+      if (k > sum (weights > 0))
+        error (strcat ("datasample: There must be at least K positive", ...
+                       " weights when sampling without replacement."));
+      endif
+
       ## choose k numbers uniformly between 0 and 1
       samples = rand (k, 1);
 
@@ -215,6 +225,12 @@ endfunction
 %!error <weights must be defined> datasample([1 2], 1, 1, "Weights", [1 -2 3]);
 %!error <weights must be defined> datasample([1 2], 1, 1, "Weights", ones (2));
 %!error <weights must be equal> datasample([1 2], 1, 1, "Weights", [1 2 3]);
+%!error <datasample: Sample size K must be less than or equal to the size of the dimension from which to sample for sampling without replacement.> ...
+%! data = 1:5; weights = [0.077846, 0.103765, 0.703748, 0.840937, 0.422901];
+%! sampled = datasample (data, 8, 'Weights', weights, 'Replace', false);
+%!error <datasample: There must be at least K positive weights when sampling without replacement.> ...
+%! data = 1:5; weights = [1, 0, 1, 0, 0];
+%! sampled = datasample (data, 3, 'Weights', weights, 'Replace', false);
 
 %!test
 %! dat = randn (10, 4);
