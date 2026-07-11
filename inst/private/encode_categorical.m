d530c91e0722b26b0e0cc3a15ae33464820a8442
Andreas Bertsatos <abertsatos@biol.uoa.gr>
2026-07-11 18:03:26 +0300

    Append Avanish to the copyright header

--- inst/private/encode_categorical.m
+++ inst/private/encode_categorical.m
@@ -1,4 +1,5 @@
 ## Copyright (C) 2026 Andreas Bertsatos <abertsatos@biol.uoa.gr>
+## Copyright (C) 2026 Avanish Salunke <avanishsalunke16@gmail.com>
 ##
 ## This file is part of the statistics package for GNU Octave.
 ##
