6a9663518476886ea99170996e92a9dcbb1b4bf5
Sonu0305 <swayamshah66@gmail.com>
2026-09-15 21:02:03 +0400

    Update empty array error message for mnrfit

--- inst/Regression/mnrfit.m
+++ inst/Regression/mnrfit.m
@@ -199,7 +199,7 @@ function [B, DEV, STATS] = mnrfit (X, Y, varargin)
     n = numel (UY);            ## number of unique response categories
   endif
   if (isempty (Y))
-    error ("mnrfit: X and Y must contain at least one valid observation.");
+    error ("mnrfit: x and y must contain at least one observation.");
   endif
   if (isnumeric (Y))
     if (! (all (Y > 0) && all (fix (Y) == Y)))
@@ -663,10 +663,8 @@ endfunction
 %!error<mnrfit: 'estdisp' must be 'on' or 'off'.> ...
 %! mnrfit (ones (5, 4), [1; 2; 1; 2; 1], 'estdisp', 'maybe')
 
-%!test
-%! ## Edge cases with empty arrays
-%!error<mnrfit: X and Y must contain at least one valid observation.> ...
+## Edge cases with empty arrays
+%!error <mnrfit: x and y must contain at least one observation.> ...
 %! mnrfit ([], [])
-%!error<mnrfit: X and Y must contain at least one valid observation.> ...
+%!error <mnrfit: x and y must contain at least one observation.> ...
 %! mnrfit (zeros(0,3), zeros(0,1))
-
