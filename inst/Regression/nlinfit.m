cee32985d3d31743b1901d6f61809f6a5febe49a
Sonu0305 <swayamshah66@gmail.com>
2026-09-16 06:53:40 +0400

    Fix broadcasting array issue in lm_fit and relax isvector check

--- inst/Regression/nlinfit.m
+++ inst/Regression/nlinfit.m
@@ -245,9 +245,10 @@ function [beta, R, J] = lm_fit (X, y, modelfun, beta0, w, opts)
 
   beta   = beta0;
   yhat   = modelfun (beta, X);
-  if (! isequal (size (yhat), size (y)))
+  if (! isvector (yhat) || numel (yhat) != numel (y))
     error ("nlinfit: MODELFUN must return a vector of the same size as Y.");
   endif
+  yhat = yhat(:);
   R      = y - yhat;
   sse    = sum (w .* R .^ 2);
   lambda = 1e-2;                           # Marquardt damping
@@ -266,7 +267,8 @@ function [beta, R, J] = lm_fit (X, y, modelfun, beta0, w, opts)
       A     = JtJ + lambda * diagJtJ;
       delta = pinv (A) * Jtr;
       bnew  = beta + delta;
-      rnew  = y - modelfun (bnew, X);
+      ynew  = modelfun (bnew, X);
+      rnew  = y - ynew(:);
       ssenew = sum (w .* rnew .^ 2);
       if (isfinite (ssenew) && ssenew < sse)
         stepok = true;
